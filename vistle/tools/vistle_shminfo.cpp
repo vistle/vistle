480f43082e1f557b5e9f67e18514ee016655674f
Martin Aumüller <aumuell@reserv.at>
2012-10-24 20:25:14 +0200

    shm debug tool

---
+++ vistle/tools/vistle_shminfo.cpp
@@ -0,0 +1,66 @@
+/*
+ * Visualization Testing Laboratory for Exascale Computing (VISTLE)
+ */
+
+
+#include <iostream>
+#include <cstdlib>
+
+#include <mpi.h>
+
+#include <core/object.h>
+
+using namespace vistle;
+
+int main(int argc, char ** argv) {
+
+   MPI_Init(&argc, &argv);
+
+   if (argc < 2) {
+      std::cerr << "vistle session id is required" << std::endl;
+      exit(1);
+   }
+
+   Shm &shm = Shm::attach(argv[1], -1, -1);
+
+
+   for (size_t i=0; i<Shm::s_shmdebug->size(); ++i) {
+      const ShmDebugInfo &info = (*Shm::s_shmdebug)[i];
+      std::cerr << (info.type ? info.type : '0') << " " << (info.deleted ? "D" : "A") << " " << info.name;
+      if (!info.deleted) {
+         switch(info.type) {
+            case 'O':
+               if (info.handle) {
+                  boost::shared_ptr<const Object> obj = shm.getObjectFromHandle(info.handle);
+                  if (obj) {
+                     std::cerr << " " << obj->refcount() << std::endl;
+                  } else {
+                     std::cerr << "ERR" << std::endl;
+                  }
+               } else {
+                  std::cerr << "no handle" << std::endl;
+               }
+               break;
+            case 'V':
+               if (info.handle) {
+                  const void *p = Shm::instance().getShm().get_address_from_handle(info.handle);
+                  const ShmVector<int> *v = static_cast<const ShmVector<int> *>(p);
+                  std::cerr << " " << v->refcount() << std::endl;
+               } else {
+                  std::cerr << " no handle" << std::endl;
+               }
+               break;
+            case '\0':
+               std::cerr << std::endl;
+               break;
+            default:
+               std::cerr << " unknown type" << std::endl;
+               break;
+         }
+      }
+   }
+
+   MPI_Finalize();
+
+   return 0;
+}
