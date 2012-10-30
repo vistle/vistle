00a1954938410c6c135ed3aa88a23332776f2c10
Martin Aumüller <aumuell@reserv.at>
2012-10-30 18:48:55 +0100

    better grepability

--- vistle/tools/vistle_shminfo.cpp
+++ vistle/tools/vistle_shminfo.cpp
@@ -45,38 +45,38 @@ int main(int argc, char ** argv) {
 
    for (size_t i=0; i<Shm::s_shmdebug->size(); ++i) {
       const ShmDebugInfo &info = (*Shm::s_shmdebug)[i];
-      std::cerr << (info.type ? info.type : '0') << " " << (info.deleted ? "D" : "A") << " " << info.name;
+      std::cout << (info.type ? info.type : '0') << " " << (info.deleted ? "D" : "A") << " " << info.name;
       if (info.deleted) {
-         std::cerr << std::endl;
+         std::cout << std::endl;
       } else {
          switch(info.type) {
             case 'O':
                if (info.handle) {
                   boost::shared_ptr<const Object> obj = shm.getObjectFromHandle(info.handle);
                   if (obj) {
-                     std::cerr << " type " << obj->getType();
-                     std::cerr << " " << obj->refcount() << std::endl;
+                     std::cout << " type " << obj->getType();
+                     std::cout << " " << obj->refcount() << std::endl;
                   } else {
-                     std::cerr << "ERR" << std::endl;
+                     std::cout << "ERR" << std::endl;
                   }
                } else {
-                  std::cerr << "no handle" << std::endl;
+                  std::cout << "no handle" << std::endl;
                }
                break;
             case 'V':
                if (info.handle) {
                   const void *p = Shm::instance().getShm().get_address_from_handle(info.handle);
                   const ShmVector<int> *v = static_cast<const ShmVector<int> *>(p);
-                  std::cerr << " " << v->refcount() << std::endl;
+                  std::cout << " " << v->refcount() << std::endl;
                } else {
-                  std::cerr << " no handle" << std::endl;
+                  std::cout << " no handle" << std::endl;
                }
                break;
             case '\0':
-               std::cerr << std::endl;
+               std::cout << std::endl;
                break;
             default:
-               std::cerr << " unknown type" << std::endl;
+               std::cout << " unknown type" << std::endl;
                break;
          }
       }
