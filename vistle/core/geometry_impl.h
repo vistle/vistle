633582e66be7a270719a8b5093e5fd61a9436d7b
Martin Aumüller <aumuell@reserv.at>
2012-11-17 08:32:50 +0100

    fix writing of Geometry

--- vistle/core/geometry_impl.h
+++ vistle/core/geometry_impl.h
@@ -6,10 +6,26 @@ namespace vistle {
 template<class Archive>
 void Geometry::Data::serialize(Archive &ar, const unsigned int version) {
    ar & V_NAME("base", boost::serialization::base_object<Base::Data>(*this));
-   ar & V_NAME("geometry", *geometry);
-   ar & V_NAME("colors", *colors);
-   ar & V_NAME("normals", *normals);
-   ar & V_NAME("texture", *texture);
+
+   bool haveGeometry = geometry;
+   ar & V_NAME("haveGeometry", haveGeometry);
+   if (haveGeometry)
+      ar & V_NAME("geometry", *geometry);
+
+   bool haveColors = colors;
+   ar & V_NAME("haveColors", haveColors);
+   if (haveColors)
+      ar & V_NAME("colors", *colors);
+
+   bool haveNormals = normals;
+   ar & V_NAME("haveNormals", haveNormals);
+   if (haveNormals)
+      ar & V_NAME("normals", *normals);
+
+   bool haveTexture = texture;
+   ar & V_NAME("haveTexture", haveTexture);
+   if (haveTexture)
+      ar & V_NAME("texture", *texture);
 }
 
 } // namespace vistle
