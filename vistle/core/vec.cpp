59138e3e98d23664a0d12463627d41a3126e1a15
Martin Aumüller <aumuell@reserv.at>
2012-12-13 20:21:25 +0100

    don't use 2- and 4-dim Vecs for now

--- vistle/core/vec.cpp
+++ vistle/core/vec.cpp
@@ -13,20 +13,24 @@ VEC(double, 1, VECDOUBLE)
 VEC(char, 1, VECCHAR)
 VEC(int, 1, VECINT)
 
+#if 0
 VEC(float, 2, VEC2FLOAT)
 VEC(double, 2, VEC2DOUBLE)
 VEC(char, 2, VEC2CHAR)
 VEC(int, 2, VEC2INT)
+#endif
 
 VEC(float, 3, VEC3FLOAT)
 VEC(double, 3, VEC3DOUBLE)
 VEC(char, 3, VEC3CHAR)
 VEC(int, 3, VEC3INT)
 
+#if 0
 VEC(float, 4, VEC4FLOAT)
 VEC(double, 4, VEC4DOUBLE)
 VEC(char, 4, VEC4CHAR)
 VEC(int, 4, VEC4INT)
+#endif
 
 template<class V>
 static void inst_vec() {
@@ -42,9 +46,9 @@ static void inst_vec() {
 
 void inst_vecs() {
    INST_VECS(1)
-   INST_VECS(2)
+   //INST_VECS(2)
    INST_VECS(3)
-   INST_VECS(4)
+   //INST_VECS(4)
 }
 
 } // namespace vistle
