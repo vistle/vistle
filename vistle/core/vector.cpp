10990f86eab065db39f52a4fc74b301d91e864d6
Martin Aumüller <aumuell@reserv.at>
2013-02-07 20:43:21 +0100

    enable inlining for old vector class

--- vistle/core/vector.cpp
+++ vistle/core/vector.cpp
@@ -2,39 +2,4 @@
 
 namespace vistle {
 
-Vector::Vector(const Scalar _x, const Scalar _y, const Scalar _z)
-   : x(_x), y(_y), z(_z) {
-
-}
-
-Vector::Vector(): x(0), y(0), z(0) {
-
-}
-
-Vector Vector::operator - () const {
-
-   return Vector(-x, -y, -z);
-}
-
-Vector Vector::operator * (Scalar const & rhs) const {
-
-   return Vector(rhs * x, rhs * y, rhs * z);
-}
-
-
-Vector Vector::operator + (Vector const & rhs) const {
-
-   return Vector(x + rhs.x, y + rhs.y, z + rhs.z);
-}
-
-Vector Vector::operator - (Vector const & rhs) const {
-
-   return Vector(x - rhs.x, y - rhs.y, z - rhs.z);
-}
-
-Scalar Vector::operator * (Vector const & rhs) const {
-
-      return x * rhs.x + y * rhs.y + z * rhs.z;
-}
-
 } // namespace vistle
--- vistle/core/vector.h
+++ vistle/core/vector.h
@@ -7,23 +7,34 @@ namespace vistle {
 
 class Vector {
 public:
-   Vector(const Scalar x, const Scalar y, const Scalar z);
-   Vector();
+   Vector(const Scalar x, const Scalar y, const Scalar z)
+   : x(x), y(y), z(z) {}
+   Vector() : x(Scalar()), y(Scalar()), z(Scalar()) {}
 
    // negate
-   Vector operator - () const;
+   Vector operator-() const {
+      return Vector(-x, -y, -z);
+   }
 
-   // scalar multiplication
-   Vector operator * (Scalar const & rhs) const;
+   // scaling
+   Vector operator*(Scalar const & rhs) const {
+      return Vector(rhs * x, rhs * y, rhs * z);
+   }
 
    // vector addition
-   Vector operator + (Vector const & rhs) const;
+   Vector operator+(Vector const & rhs) const {
+      return Vector(x + rhs.x, y + rhs.y, z + rhs.z);
+   }
 
-   // vector supstraction
-   Vector operator - (Vector const & rhs) const;
+   // vector substraction
+   Vector operator-(Vector const & rhs) const {
+      return Vector(x - rhs.x, y - rhs.y, z - rhs.z);
+   }
 
    // scalar product
-   Scalar operator * (Vector const & rhs) const;
+   Scalar operator*(Vector const & rhs) const {
+      return x * rhs.x + y * rhs.y + z * rhs.z;
+   }
 
    Scalar x, y, z;
 };
