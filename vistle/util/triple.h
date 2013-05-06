df79a9f0981d3327e3748ef332e72e6b897ae055
Martin Aumüller <aumuell@reserv.at>
2013-05-06 14:16:24 +0200

    use boost::fusion::vector3 as triple

---
+++ vistle/util/triple.h
@@ -0,0 +1,14 @@
+#ifndef TRIPLE_H
+#define TRIPLE_H
+
+#define vector3 triple
+#include <boost/fusion/container.hpp>
+#undef triple
+
+namespace vistle {
+
+using boost::fusion::triple;
+
+} // namespace vistle
+
+#endif
