c56d84433c0b2a32155f1168ca2f7bc8120a9181
Martin Aumüller <aumuell@reserv.at>
2013-06-03 16:36:26 +0200

    provide make_triple analogous to std::make_pair

--- vistle/util/triple.h
+++ vistle/util/triple.h
@@ -9,6 +9,11 @@ namespace vistle {
 
 using boost::fusion::triple;
 
+template<typename T1, typename T2, typename T3>
+triple<T1, T2, T3> make_triple(const T1 &v1, const T2 &v2, const T3 &v3) {
+   return triple<T1, T2, T3>(v1, v2, v3);
+}
+
 } // namespace vistle
 
 #endif
