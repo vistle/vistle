73553363a570766e8d622e615b5eced631d1dbb9
Martin Aumüller <aumuell@reserv.at>
2013-06-10 15:13:13 +0200

    only use broadcasting for object distribution

--- vistle/core/renderer.cpp
+++ vistle/core/renderer.cpp
@@ -63,47 +63,35 @@ bool Renderer::dispatch() {
          switch (message->type()) {
             case vistle::message::Message::OBJECTRECEIVED: {
                const message::ObjectReceived *recv = static_cast<const message::ObjectReceived *>(message);
-               if (recv->rank() == rank()) {
-                  Object::const_ptr obj = Shm::the().getObjectFromName(recv->objectName());
-                  vecstreambuf<char> memstr;
-                  ba::binary_oarchive memar(memstr);
-                  obj->save(memar);
-                  const std::vector<char> &mem = memstr.get_vector();
-                  uint64_t len = mem.size();
-                  std::cerr << "Rank " << rank() << ": Broadcasting " << len << " bytes, type=" << obj->getType() << " (" << obj->getName() << ")" << std::endl;
-                  const char *data = mem.data();
-                  std::vector<MPI_Request> request(size());
-                  for (int r=0; r<size(); ++r) {
-                     if (r == rank())
-                        continue;
-                     MPI_Isend(&len, 1, MPI_UINT64_T, r, 3453, MPI_COMM_WORLD, &request[r]);
-                  }
-                  //MPI_Bcast(&len, 1, MPI_UINT64_T, rank(), MPI_COMM_WORLD);
-                  MPI_Bcast(const_cast<char *>(data), len, MPI_BYTE, rank(), MPI_COMM_WORLD);
-                  for (int r=0; r<size(); ++r) {
-                     if (r == rank())
-                        continue;
-                     MPI_Wait(&request[r], MPI_STATUS_IGNORE);
-                  }
-               } else {
-                  uint64_t len = 0;
-                  std::cerr << "Rank " << rank() << ": Waiting to receive" << std::endl;
-                  //MPI_Bcast(&len, 1, MPI_UINT64_T, recv->rank(), MPI_COMM_WORLD);
-                  MPI_Request r;
-                  MPI_Irecv(&len, 1, MPI_UINT64_T, recv->rank(), 3453, MPI_COMM_WORLD, &r);
-                  MPI_Wait(&r, MPI_STATUS_IGNORE);
-                  std::cerr << "Rank " << rank() << ": Waiting to receive " << len << " bytes" << std::endl;
-                  std::vector<char> mem(len);
-                  char *data = mem.data();
-                  MPI_Bcast(data, mem.size(), MPI_BYTE, recv->rank(), MPI_COMM_WORLD);
-                  std::cerr << "Rank " << rank() << ": Received " << len << " bytes for " << recv->objectName() << std::endl;
-                  vecstreambuf<char> membuf(mem);
-                  ba::binary_iarchive memar(membuf);
-                  Object::ptr obj = Object::load(memar);
-                  if (obj) {
-                     std::cerr << "Rank " << rank() << ": Restored " << recv->objectName() << " as " << obj->getName() << ", type: " << obj->getType() << std::endl;
-                     assert(obj->check());
-                     addInputObject(recv->getPortName(), obj);
+               if (size() > 1) {
+                  if (recv->rank() == rank()) {
+                     Object::const_ptr obj = Shm::the().getObjectFromName(recv->objectName());
+                     vecstreambuf<char> memstr;
+                     ba::binary_oarchive memar(memstr);
+                     obj->save(memar);
+                     const std::vector<char> &mem = memstr.get_vector();
+                     uint64_t len = mem.size();
+                     std::cerr << "Rank " << rank() << ": Broadcasting " << len << " bytes, type=" << obj->getType() << " (" << obj->getName() << ")" << std::endl;
+                     const char *data = mem.data();
+                     MPI_Bcast(&len, 1, MPI_UINT64_T, rank(), MPI_COMM_WORLD);
+                     MPI_Bcast(const_cast<char *>(data), len, MPI_BYTE, rank(), MPI_COMM_WORLD);
+                  } else {
+                     uint64_t len = 0;
+                     std::cerr << "Rank " << rank() << ": Waiting to receive" << std::endl;
+                     MPI_Bcast(&len, 1, MPI_UINT64_T, recv->rank(), MPI_COMM_WORLD);
+                     std::cerr << "Rank " << rank() << ": Waiting to receive " << len << " bytes" << std::endl;
+                     std::vector<char> mem(len);
+                     char *data = mem.data();
+                     MPI_Bcast(data, mem.size(), MPI_BYTE, recv->rank(), MPI_COMM_WORLD);
+                     std::cerr << "Rank " << rank() << ": Received " << len << " bytes for " << recv->objectName() << std::endl;
+                     vecstreambuf<char> membuf(mem);
+                     ba::binary_iarchive memar(membuf);
+                     Object::ptr obj = Object::load(memar);
+                     if (obj) {
+                        std::cerr << "Rank " << rank() << ": Restored " << recv->objectName() << " as " << obj->getName() << ", type: " << obj->getType() << std::endl;
+                        assert(obj->check());
+                        addInputObject(recv->getPortName(), obj);
+                     }
                   }
                }
                break;
