4941b3b69fc8099bfb67d8744b0316645acea430
Martin Aumüller <aumuell@reserv.at>
2013-06-13 17:24:00 +0200

    move getbindir to util library

---
+++ vistle/util/findself.cpp
@@ -0,0 +1,132 @@
+#include "findself.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unistd.h>
+#include <dirent.h>
+
+namespace vistle {
+
+static void splitpath(const std::string &value, std::vector<std::string> *components)
+{
+#ifdef _WIN32
+   const char *sep = ";";
+#else
+   const char *sep = ":";
+#endif
+
+   std::string::size_type begin = 0;
+   do {
+      std::string::size_type end = value.find(sep, begin);
+
+      std::string c;
+      if (end != std::string::npos) {
+         c = value.substr(begin, end-begin);
+         ++end;
+      } else {
+         c = value.substr(begin);
+      }
+      begin = end;
+
+      if (!c.empty())
+         components->push_back(c);
+   } while(begin != std::string::npos);
+}
+
+std::string getbindir(int argc, char *argv[]) {
+
+   char *wd = getcwd(NULL, 0);
+   if (!wd) {
+
+      std::cerr << "Communicator: failed to determine working directory: " << strerror(errno) << std::endl;
+      exit(1);
+   }
+   std::string cwd(wd);
+   free(wd);
+
+   // determine complete path to executable
+   std::string executable;
+#ifdef _WIN32
+   char buf[2000];
+   DWORD sz = GetModuleFileName(NULL, buf, sizeof(buf));
+   if (sz != 0) {
+
+      executable = buf;
+   } else {
+
+      std::cerr << "Communicator: getbindir(): GetModuleFileName failed - error: " << GetLastError() << std::endl;
+   }
+#else
+   char buf[PATH_MAX];
+   ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf));
+   if(len != -1) {
+
+      executable = std::string(buf, len);
+   } else if (argc >= 1) {
+
+      bool found = false;
+      if (!strchr(argv[0], '/')) {
+
+         if(const char *path = getenv("PATH")) {
+            std::vector<std::string> components;
+            splitpath(path, &components);
+            for(size_t i=0; i<components.size(); ++i) {
+
+               std::string component = components[i];
+               if (component[0] != '/')
+                  component = cwd + "/" + component;
+
+               DIR *dir = opendir(component.c_str());
+               if (!dir) {
+                  std::cerr << "Communicator: failed to open directory " << component << ": " << strerror(errno) << std::endl;
+                  continue;
+               }
+
+               while (struct dirent *ent = readdir(dir)) {
+                  if (!strcmp(ent->d_name, argv[0])) {
+                     found = true;
+                     break;
+                  }
+               }
+
+               if (found) {
+                  executable = component + "/" + argv[0];
+                  break;
+               }
+            }
+         }
+      } else if (argv[0][0] != '/') {
+         executable = cwd + "/" + argv[0];
+      } else {
+         executable = argv[0];
+      }
+   }
+#endif
+
+   // guess vistle bin directory
+   if (!executable.empty()) {
+
+      std::string dir = executable;
+#ifdef _WIN32
+      std::string::size_type idx = dir.find_last_of("\\/");
+#else
+      std::string::size_type idx = dir.rfind('/');
+#endif
+      if (idx == std::string::npos) {
+
+         dir = cwd;
+      } else {
+
+         dir = executable.substr(0, idx);
+      }
+
+#ifdef DEBUG
+      std::cerr << "Communicator: vistle bin directory determined to be " << dir << std::endl;
+#endif
+      return dir;
+   }
+
+   return std::string();
+}
+
+} // namespace vistle
---
+++ vistle/util/findself.h
@@ -0,0 +1,11 @@
+#ifndef FINDSELF_H
+#define FINDSELF_H
+
+#include <string>
+
+namespace vistle {
+
+std::string getbindir(int argc, char *argv[]);
+
+} // namespace vistle
+#endif
