33a439b1d971d4fee7ef840f1be67a9d309da7c9
Martin Aumüller <aumuell@reserv.at>
2014-04-09 16:25:39 +0200

    module for showing placement of ranks onto nodes

---
+++ vistle/module/MpiInfo/MpiInfo.cpp
@@ -0,0 +1,65 @@
+#include <sstream>
+#include <limits>
+#include <algorithm>
+
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpi/collectives/all_reduce.hpp>
+
+#include <module/module.h>
+#include <core/vec.h>
+#include <core/scalars.h>
+#include <core/message.h>
+#include <core/coords.h>
+#include <core/lines.h>
+#include <core/triangles.h>
+#include <core/indexed.h>
+
+using namespace vistle;
+
+class MpiInfo: public vistle::Module {
+
+ public:
+   MpiInfo(const std::string &shmname, int rank, int size, int moduleID);
+   ~MpiInfo();
+
+ private:
+
+   virtual bool compute();
+   virtual bool reduce(int timestep);
+
+   bool prepare();
+};
+
+using namespace vistle;
+
+MpiInfo::MpiInfo(const std::string &shmname, int rank, int size, int moduleID)
+   : Module("MPI info", shmname, rank, size, moduleID)
+{
+}
+
+MpiInfo::~MpiInfo() {
+
+}
+
+bool MpiInfo::compute() {
+
+   std::vector<char> hostname(1024);
+   gethostname(hostname.data(), hostname.size());
+
+   std::stringstream str;
+   str << "rank " << rank() << "/" << size() << " on host " << hostname.data() << std::endl;
+   sendInfo(str.str());
+
+   return true;
+}
+
+bool MpiInfo::prepare() {
+
+   return true;
+}
+
+bool MpiInfo::reduce(int timestep) {
+}
+
+MODULE_MAIN(MpiInfo)
+
