04eb7fc2bfb053e7f8f708058afe976450086dd5
Martin Aumüller <aumuell@reserv.at>
2014-05-08 15:17:51 +0200

    fix sending of parameters from UI to modules
    UIs have to have ids < 0, otherwise message might not be forwarded to modules with the same id

--- vistle/userinterface/userinterface.cpp
+++ vistle/userinterface/userinterface.cpp
@@ -168,7 +168,8 @@ bool UserInterface::handleMessage(const vistle::message::Message *message) {
       case message::Message::SETID: {
          const message::SetId *id = static_cast<const message::SetId *>(message);
          m_id = id->getId();
-         message::DefaultSender::init(m_id, 0);
+         assert(m_id > 0);
+         message::DefaultSender::init(-m_id, 0);
          //std::cerr << "received new UI id: " << m_id << std::endl;
          break;
       }
