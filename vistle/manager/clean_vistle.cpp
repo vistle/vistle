656be7726660c4eabb3a0e5ef2fbd6aa89eda542
Martin Aumüller <aumuell@reserv.at>
2014-10-02 12:27:42 +0200

    clean shmem for specific vistle instance

--- vistle/manager/clean_vistle.cpp
+++ vistle/manager/clean_vistle.cpp
@@ -17,7 +17,17 @@ int main(int argc, char ** argv) {
 
       MPI_Init(&argc, &argv);
 
-      ret = vistle::Shm::cleanAll() ? 0 : 1;
+      if (argc == 2) {
+
+         int rank = 0;
+         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+         vistle::Shm::attach(argv[1], 0, rank);
+         vistle::Shm::the().setRemoveOnDetach();
+         vistle::Shm::the().detach();
+      } else {
+
+         ret = vistle::Shm::cleanAll() ? 0 : 1;
+      }
 
       MPI_Finalize();
    } catch(std::exception &e) {
