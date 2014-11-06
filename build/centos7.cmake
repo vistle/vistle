b1d209bb2e2c6ff93916076fc0faa5d7822c8032
Martin Aumüller <aumuell@reserv.at>
2014-11-06 16:54:31 +0100

    move system specific build settings to separate files

---
+++ build/centos7.cmake
@@ -0,0 +1,2 @@
+# it's easier to find all libraries when running with MPI
+set(VISTLE_BUILD_SHARED OFF)
---
+++ build/hornet.cmake
@@ -0,0 +1,4 @@
+# we want an entirely static build
+set(VISTLE_BUILD_SHARED OFF)
+set(Boost_USE_STATIC_LIBS TRUE)
+
