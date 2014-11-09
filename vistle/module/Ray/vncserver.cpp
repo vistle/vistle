169cdf811a94bab29d94df596492dc098aabe8d9
Martin Aumüller <aumuell@reserv.at>
2014-11-09 11:48:41 +0100

    dead code

--- vistle/module/Ray/vncserver.cpp
+++ vistle/module/Ray/vncserver.cpp
@@ -885,132 +885,6 @@ VncServer::preFrame()
    rfbReleaseClientIterator(i);
 }
 
-template<typename T>
-int clz (T xx)
-{
-   vassert(sizeof(T)<=8);
-   uint64_t x = xx;
-
-   if (xx==0)
-      return 8*sizeof(T);
-
-   int n=0;
-   if ((x & 0xffffffff00000000UL) == 0) { x <<= 32; if (sizeof(T) >= 8) n +=32; }
-   if ((x & 0xffff000000000000UL) == 0) { x <<= 16; if (sizeof(T) >= 4) n +=16; }
-   if ((x & 0xff00000000000000UL) == 0) { x <<= 8;  if (sizeof(T) >= 2) n += 8; }
-   if ((x & 0xf000000000000000UL) == 0) { x <<= 4;  n += 4; }
-   if ((x & 0xc000000000000000UL) == 0) { x <<= 2;  n += 2; }
-   if ((x & 0x8000000000000000UL) == 0) { x <<= 1;  n += 1; }
-   return n;
-}
-
-template<typename T, class Quant>
-void depthcompare_t(const T *ref, const T *check, unsigned w, unsigned h) {
-
-#ifdef max
-#undef max
-#endif
-
-   size_t numlow=0, numhigh=0;
-   unsigned maxx=0, maxy=0;
-   size_t numblack=0, numblackwrong=0;
-   size_t totalvalidbits = 0;
-   int minvalidbits=8*sizeof(T), maxvalidbits=0;
-   double totalerror = 0.;
-   double squarederror = 0.;
-   T Max = std::numeric_limits<T>::max();
-   if (sizeof(T)==4)
-      Max >>= 8;
-   T maxerr = 0;
-   T refminval = Max, minval = Max;
-   T refmaxval = 0, maxval = 0;
-   for (unsigned y=0; y<h; ++y) {
-      for (unsigned x=0; x<w; ++x) {
-         size_t idx = y*w+x;
-         T e = 0;
-
-         T r = ref[idx];
-         T c = check[idx];
-         if (sizeof(T)==4) {
-            r >>= 8;
-            c >>= 8;
-         }
-         if (r == Max) {
-            ++numblack;
-            if (c != r) {
-               ++numblackwrong;
-               fprintf(stderr, "!B: %u %u (%lu)\n", x, y, (unsigned long)c);
-            }
-         } else {
-            int validbits = clz<T>(r^c);
-            if (validbits < minvalidbits)
-               minvalidbits = validbits;
-            if (validbits > maxvalidbits)
-               maxvalidbits = validbits;
-            totalvalidbits += validbits;
-
-            if (r < refminval)
-               refminval = r;
-            if (r > refmaxval)
-               refmaxval = r;
-            if (c < minval)
-               minval = c;
-            if (c > maxval)
-               maxval = c;
-         }
-         if (r > c) {
-            ++numlow;
-            e = r - c;
-         } else if (r < c) {
-            ++numhigh;
-            e = c - r;
-         }
-         if (e > maxerr) {
-            maxx = x;
-            maxy = y;
-            maxerr = e;
-         }
-         double err = e;
-         totalerror += err;
-         squarederror += err*err;
-      }
-   }
-
-   size_t nonblack = w*h-numblack;
-   double MSE = squarederror/w/h;
-   double MSE_nonblack = squarederror/nonblack;
-   double PSNR = -1., PSNR_nonblack=-1.;
-   if (MSE > 0.) {
-      PSNR = 10. * (2.*log10(Max) - log10(MSE));
-      PSNR_nonblack = 10. * (2.*log10(Max) - log10(MSE_nonblack));
-   }
-
-   fprintf(stderr, "ERROR: #high=%ld, #low=%ld, max=%lu (%u %u), total=%f, mean non-black=%f (%f %%)\n",
-         (long)numhigh, (long)numlow, (unsigned long)maxerr, maxx, maxy, totalerror,
-         totalerror/nonblack,
-         totalerror/nonblack*100./(double)Max
-         );
-   fprintf(stderr, "BITS: totalvalid=%lu, min=%d, max=%d, mean=%f\n",
-         totalvalidbits, minvalidbits, maxvalidbits, (double)totalvalidbits/(double)nonblack);
-
-   fprintf(stderr, "PSNR (2x%d+16x%d): %f dB (non-black: %f)\n",
-         Quant::precision, Quant::bits_per_pixel,
-         PSNR, PSNR_nonblack);
-
-   fprintf(stderr, "RANGE: ref %lu - %lu, act %lu - %lu\n",
-         (unsigned long)refminval, (unsigned long)refmaxval,
-         (unsigned long)minval, (unsigned long)maxval);
-}
-
-static void depthcompare(const char *ref, const char *check, int precision, unsigned w, unsigned h) {
-
-   if (precision == 16) {
-      depthcompare_t<uint16_t, DepthQuantize16>((const uint16_t *)ref, (const uint16_t *)check, w, h);
-   } else if (precision == 32) {
-      depthcompare_t<uint32_t, DepthQuantize24>((const uint32_t *)ref, (const uint32_t *)check, w, h);
-   }
-}
-
 //! called after back-buffer has been swapped to front-buffer
 void
 VncServer::postFrame()
@@ -1280,6 +1154,7 @@ void VncServer::encodeAndSend(int viewNum, int x0, int y0, int w, int h, const V
     for (int y=y0; y<y0+h; y+=tileHeight) {
         for (int x=x0; x<x0+w; x+=tileWidth) {
 
+            // depth
             auto dt = new(tbb::task::allocate_root()) EncodeTask(m_resultQueue,
                     viewNum,
                     x, y,
@@ -1289,6 +1164,7 @@ void VncServer::encodeAndSend(int viewNum, int x0, int y0, int w, int h, const V
             tbb::task::enqueue(*dt);
             ++m_queuedTiles;
 
+            // color
             auto ct = new(tbb::task::allocate_root()) EncodeTask(m_resultQueue,
                     viewNum,
                     x, y,
