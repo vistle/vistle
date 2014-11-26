41de6ab9d129130840adeb7d10d9a7ae06992d94
Martin Aumüller <aumuell@reserv.at>
2014-11-26 18:15:58 +0100

    disable warning

--- vistle/module/module.h
+++ vistle/module/module.h
@@ -255,13 +255,20 @@ V_MODULEEXPORT Object::const_ptr Module::expect<Object>(Port *port);
 
 } // namespace vistle
 
+// MPI_THREAD_SINGLE seems to be ok for OpenMP with MPICH
+#ifdef MPICH_VERSION
+#define V_HAVE_MPICH 1
+#else
+#define V_HAVE_MPICH 0
+#endif
+
 #define MODULE_MAIN(X) \
    int main(int argc, char **argv) { \
       int provided = MPI_THREAD_SINGLE; \
       MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided); \
-      if (provided == MPI_THREAD_SINGLE) { \
+      if (provided == MPI_THREAD_SINGLE && !V_HAVE_MPICH) { \
          std::cerr << "no thread support in MPI" << std::endl; \
-          \
+         exit(1); \
       } \
       vistle::registerTypes(); \
       int rank=-1, size=-1; \
