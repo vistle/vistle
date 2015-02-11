d43c418d12f41f0402510dbf7c81bb3a9bf4e00d
Martin Aumüller <aumuell@reserv.at>
2015-02-11 15:05:41 +0100

    also show rank and size of MPI_COMM_WORLD

--- vistle/test/mpitest/vistle_mpitest.cpp
+++ vistle/test/mpitest/vistle_mpitest.cpp
@@ -7,6 +7,12 @@ int main(int argc, char *argv[]) {
    MPI_Init(&argc, &argv);
    std::cerr << "after MPI_Init" << std::endl;
 
+   int rank=-1, size=-1;
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+   std::cerr << "rank " << rank << "/" << size << std::endl;
+
    std::cerr << "trying MPI_Finalize..." << std::endl;
    MPI_Finalize();
    std::cerr << "after MPI_Finalize" << std::endl;
