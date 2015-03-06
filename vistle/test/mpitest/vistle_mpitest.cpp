eecd23b8723da805f0ca1cfdc74b1eb029449e18
Martin Aumüller <aumuell@reserv.at>
2015-03-06 17:22:09 +0100

    show cpu affinity

--- vistle/test/mpitest/vistle_mpitest.cpp
+++ vistle/test/mpitest/vistle_mpitest.cpp
@@ -1,5 +1,13 @@
 #include <mpi.h>
 #include <iostream>
+#include <sstream>
+
+#ifdef __linux__
+#define _GNU_SOURCE
+#include <sched.h>
+#include <unistd.h>
+#define AFFINITY
+#endif
 
 int main(int argc, char *argv[]) {
 
@@ -12,6 +20,28 @@ int main(int argc, char *argv[]) {
    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
    std::cerr << "rank " << rank << "/" << size << std::endl;
+#ifdef AFFINITY
+   const int ncpu = 1024;
+   cpu_set_t *mask = CPU_ALLOC(ncpu);
+   if (!mask) {
+      std::cerr << "CPU_ALLOC failed" << std::endl;
+   }  else {
+      size_t size = CPU_ALLOC_SIZE(ncpu);
+      std::cerr << "cpu size " << size << std::endl;
+      CPU_ZERO_S(size, mask);
+      if(sched_getaffinity(getpid(), size, mask) == -1) {
+         perror("sched_getaffinity");
+      } else {
+         std::stringstream str;
+         str << "affinity mask on rank " << rank << ": ";
+         for (int i=0; i<16; ++i) {
+            str << (CPU_ISSET_S(i, size, mask) ? "1" : "0");
+         }
+         std::cerr << str.str() << std::endl;
+      }
+      CPU_FREE(mask);
+   }
+#endif
 
    std::cerr << "trying MPI_Finalize..." << std::endl;
    MPI_Finalize();
