d96834ba60d2b3997580fadc22800ccb59a4b603
Martin Aumüller <aumuell@reserv.at>
2015-03-16 11:38:56 +0100

    Boost.MPI broadcast test program

---
+++ vistle/test/mpibcast/mpibcast.cpp
@@ -0,0 +1,28 @@
+#include <boost/mpi.hpp>
+
+namespace mpi = boost::mpi;
+
+//! state shared among all views
+struct GlobalState {
+   unsigned timestep;
+
+      GlobalState()
+         : timestep(0)
+      {
+      }
+
+      template<class Archive>
+      void serialize(Archive &ar, const unsigned int version) {
+         ar & timestep;
+      }
+   };
+
+struct GlobalState g_state;
+
+int main() {
+
+     mpi::environment env;
+
+   mpi::broadcast(mpi::communicator(), g_state, 0);
+
+}
