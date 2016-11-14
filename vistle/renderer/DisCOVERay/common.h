42e7c3526c8c8408ddd523c553fb4a4bcecc1507
Martin Aumüller <aumuell@reserv.at>
2016-11-14 21:09:43 +0100

    reduce number of tile messages

--- vistle/renderer/DisCOVERay/common.h
+++ vistle/renderer/DisCOVERay/common.h
@@ -4,8 +4,8 @@
 // Embree does not yet work with user defined geometry consisting of multiple items
 //#define USE_STREAM // use Embree stream interface
 
-static const int TileSize = 64;
-#define TILESIZE 64
+#define TILESIZE 256
+static const int TileSize = TILESIZE;
 static const RTCSceneFlags sceneFlags = RTC_SCENE_COHERENT;
 
 static const unsigned int RayEnabled = 0xffffffff;
