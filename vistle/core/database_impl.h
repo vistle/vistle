61dfa640440d34b0e9eb6dbcf4db65de7b3645b1
Martin Aumüller <aumuell@reserv.at>
2017-01-26 15:49:12 +0100

    serialize mapping

--- vistle/core/database_impl.h
+++ vistle/core/database_impl.h
@@ -14,6 +14,7 @@ template<class Archive>
 void DataBase::Data::serialize(Archive &ar, const unsigned int version) {
    ar & V_NAME("base_object", boost::serialization::base_object<Base::Data>(*this));
    ar & V_NAME("grid", grid);
+   ar & V_NAME("mapping", mapping);
 }
 
 } // namespace vistle
