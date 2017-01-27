0f6544b1d945a4b39bb57a9a96519c5800fcc980
Martin Aumüller <aumuell@reserv.at>
2017-01-27 11:52:07 +0100

    add attributes to object

---
+++ vistle/module/general/AddAttribute/AddAttribute.cpp
@@ -0,0 +1,67 @@
+#include <module/module.h>
+#include <core/object.h>
+#include <sstream>
+
+const int NumAttributes = 3;
+
+using namespace vistle;
+
+class AddAttribute: public vistle::Module {
+
+ public:
+   AddAttribute(const std::string &shmname, const std::string &name, int moduleID);
+   ~AddAttribute();
+
+ private:
+   virtual bool compute();
+
+   StringParameter *p_name[NumAttributes];
+   StringParameter *p_value[NumAttributes];
+};
+
+using namespace vistle;
+
+AddAttribute::AddAttribute(const std::string &shmname, const std::string &name, int moduleID)
+: Module("add attributes", shmname, name, moduleID)
+{
+
+   Port *din = createInputPort("data_in", "input data", Port::MULTI);
+   Port *dout = createOutputPort("data_out", "output data", Port::MULTI);
+   din->link(dout);
+
+   for (int i=0; i<NumAttributes; ++i) {
+      std::stringstream name;
+      name << "name" << i;
+      p_name[i] = addStringParameter(name.str(), "attribute name", "");
+
+      std::stringstream value;
+      value << "value" << i;
+      p_value[i] = addStringParameter(value.str(), "attribute value", "");
+   }
+}
+
+AddAttribute::~AddAttribute() {
+
+}
+
+bool AddAttribute::compute() {
+
+   Object::const_ptr obj = expect<Object>("data_in");
+   if (!obj)
+      return true;
+
+   Object::ptr out = obj->clone();
+
+   for (int i=0; i<NumAttributes; ++i) {
+      if (!p_name[i]->getValue().empty()) {
+         out->addAttribute(p_name[i]->getValue(), p_value[i]->getValue());
+      }
+   }
+
+   addObject("data_out", out);
+
+   return true;
+}
+
+MODULE_MAIN(AddAttribute)
+
