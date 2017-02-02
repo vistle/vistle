b7cc8764106266e60f90f7326e5ba1eba2c2f21a
Martin Aumüller <aumuell@reserv.at>
2017-02-02 11:03:34 +0100

    module for broadcasting input objects to all ranks

---
+++ vistle/module/general/Distribute/Distribute.cpp
@@ -0,0 +1,52 @@
+#include <boost/lexical_cast.hpp>
+#include <module/module.h>
+
+using namespace vistle;
+namespace mpi = boost::mpi;
+
+class Distribute: public vistle::Module {
+
+ public:
+   Distribute(const std::string &shmname, const std::string &name, int moduleID);
+   ~Distribute();
+
+ private:
+   virtual bool compute() override;
+};
+
+using namespace vistle;
+
+Distribute::Distribute(const std::string &shmname, const std::string &name, int moduleID)
+: Module("broadcast input objects", shmname, name, moduleID)
+{
+   setSchedulingPolicy(message::SchedulingPolicy::Gang);
+
+   createInputPort("data_in", "input data");
+   createOutputPort("data_out", "output data");
+}
+
+Distribute::~Distribute() {
+
+}
+
+bool Distribute::compute() {
+
+   auto obj = accept<Object>("data_in");
+   int have = obj ? 1 : 0;
+   std::vector<int> haveObject;
+   mpi::all_gather(comm(), have, haveObject);
+   for (int r=0; r<size(); ++r) {
+      if (haveObject[r]) {
+         Object::const_ptr xmit;
+         if (r == rank())
+            xmit = obj;
+         broadcastObject(xmit, r);
+         passThroughObject("data_out", xmit);
+      }
+   }
+
+   return true;
+}
+
+MODULE_MAIN(Distribute)
+
