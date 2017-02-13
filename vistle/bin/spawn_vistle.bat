14a0e4e2cc58a955915e9929ac64ff7bc3c652ac
Martin Aumüller <aumuell@reserv.at>
2017-02-13 02:10:38 +0100

    default to just one mpi rank

--- vistle/bin/spawn_vistle.bat
+++ vistle/bin/spawn_vistle.bat
@@ -2,4 +2,6 @@ REM @echo off
 echo spawn_vistle.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
 echo MSMPI_BIN=%MSMPI_BIN%
 
-"%MSMPI_BIN%\mpiexec" %1 %2 %3 %4 %5 %6 %7 %8 %9
+if not defined MPISIZE set MPISIZE=1
+
+"%MSMPI_BIN%\mpiexec" -n %MPISIZE% %1 %2 %3 %4 %5 %6 %7 %8 %9
