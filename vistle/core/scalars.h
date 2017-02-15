43be700efaf06e6732aa777039e6b82925a5233a
Martin Aumüller <aumuell@reserv.at>
2017-02-15 15:07:39 +0100

    generate serialization code for std::vector<ssize_t>

--- vistle/core/scalars.h
+++ vistle/core/scalars.h
@@ -2,12 +2,14 @@
 #define SCALARS_H
 
 #include <boost/mpl/vector.hpp>
+#include <boost/mpl/size.hpp>
 #include <array>
 
 namespace vistle {
 
-typedef boost::mpl::vector<unsigned char, int, unsigned int, size_t, float, double> Scalars;
-const std::array<const char*, 6> ScalarTypeNames = {{"unsigned char", "int", "unsigned int", "size_t", "float", "double"}};
+// list of data types usable in Vistle's shm arrays
+typedef boost::mpl::vector<unsigned char, int, unsigned int, size_t, ssize_t, float, double> Scalars;
+const std::array<const char*, boost::mpl::size<Scalars>::value> ScalarTypeNames = {{"unsigned char", "int", "unsigned int", "size_t", "ssize_t", "float", "double"}};
 
 }
 
