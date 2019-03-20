842a2195f25a89fede838b6c0ed24773f899ad97
Uwe Woessner <woessner@hlrs.de>
2019-03-20 11:46:07 +0100

    windows compile fixes

--- vistle/bin/spawn_vistle.bat
+++ vistle/bin/spawn_vistle.bat
@@ -4,4 +4,4 @@ echo MSMPI_BIN=%MSMPI_BIN%
 
 if not defined MPISIZE set MPISIZE=1
 
-"%MSMPI_BIN%\mpiexec" -n %MPISIZE% %1 %2 %3 %4 %5 %6 %7 %8 %9
+start /WAIT "mpi" "%MSMPI_BIN%\mpiexec" -n %MPISIZE% %1 %2 %3 %4 %5 %6 %7 %8 %9
