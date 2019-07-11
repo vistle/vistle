0ee683b0f79e7132fc325b85d0ad005e251cc095
Martin Aumüller <aumuell@reserv.at>
2019-07-11 18:09:53 +0200

    test harder

--- vistle/test/mpitest/vistle_mpitest.cpp
+++ vistle/test/mpitest/vistle_mpitest.cpp
@@ -36,6 +36,14 @@ int main(int argc, char *argv[]) {
    }
    MPI_Barrier(MPI_COMM_WORLD);
 
+   int sum = 0;
+   MPI_Allreduce(&rank, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+   if (rank == size-1) {
+       std::cerr << "rank " << rank << "/" << size << ": sum of all ranks: " << sum << std::endl;
+   }
+
+   MPI_Barrier(MPI_COMM_WORLD);
+
    std::cerr << "trying MPI_Finalize..." << std::endl;
    MPI_Finalize();
    std::cerr << "after MPI_Finalize" << std::endl;
