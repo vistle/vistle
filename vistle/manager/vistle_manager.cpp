796caf992a017f14fb75666bec740a52a277e8d6
Martin Aumüller <aumuell@reserv.at>
2019-07-12 14:53:12 +0200

    no MPI_Init_thread needed for multi-process Vistle

--- vistle/manager/vistle_manager.cpp
+++ vistle/manager/vistle_manager.cpp
@@ -83,12 +83,16 @@ void run_on_main_thread(std::function<void()> &func) {
 
 int main(int argc, char *argv[])
 {
+#ifdef MODULE_THREAD
    int provided = MPI_THREAD_SINGLE;
    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
    if (provided != MPI_THREAD_MULTIPLE) {
       std::cerr << "insufficient thread support in MPI: MPI_THREAD_MULTIPLE is required (maybe set MPICH_MAX_THREAD_SAFETY=multiple?)" << std::endl;
       exit(1);
    }
+#else
+   MPI_Init(&argc, &argv);
+#endif
    int rank = -1;
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
