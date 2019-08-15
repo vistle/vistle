d7916a2fd1a32509d23ec8c6c0c234d5c0c99b85
Martin Aumüller <aumuell@reserv.at>
2019-08-15 12:37:26 +0200

    work around DYLD_LIBRARY_PATH not being passed on by osx

--- vistle/bin/vistle_tui.sh
+++ vistle/bin/vistle_tui.sh
@@ -11,4 +11,7 @@ if [ -n "$2" ]; then
     port="$2"
 fi
 
+if [ -n "$VISTLE_DYLD_LIBRARY_PATH" ]; then
+    export DYLD_LIBRARY_PATH="$VISTLE_DYLD_LIBRARY_PATH"
+fi
 exec ipython -i -c "import vistle; vistle._vistle.sessionConnect(None, \"$host\", $port); from vistle import *;" -- "$@"
