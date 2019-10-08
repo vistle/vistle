a542785f6764ef644baceb53292111d4d9b816bd
Martin Aumüller <aumuell@reserv.at>
2019-10-08 14:19:12 +0200

    managed shared memory would require objects of known size

--- vistle/core/shm.cpp
+++ vistle/core/shm.cpp
@@ -538,7 +538,7 @@ Object::const_ptr Shm::getObjectFromHandle(const shm_handle_t & handle) const {
 
 ObjectData *Shm::getObjectDataFromName(const std::string &name) const {
 
-    return static_cast<Object::Data *>(vistle::shm<ShmData>::find(name));
+    return static_cast<Object::Data *>(static_cast<void *>(vistle::shm<char>::find(name)));
 }
 
 ObjectData *Shm::getObjectDataFromHandle(const shm_handle_t &handle) const
