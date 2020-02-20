35bfeb26a8226a2be4b664a7de19b91d84f494ea
Dennis Grieger <dennis.grieger@hlrs.de>
2020-02-20 16:32:10 +0100

    Module to play around with the axis orientation and order of rectilinear grids

---
+++ vistle/module/test/TransformGrid/TransformGrid.cpp
@@ -0,0 +1,129 @@
+#include "TransformGrid.h"
+
+#include <sstream>
+#include <limits>
+#include <algorithm>
+
+
+#include <core/vec.h>
+#include <core/scalars.h>
+#include <core/message.h>
+#include <core/coords.h>
+#include <core/lines.h>
+#include <core/triangles.h>
+#include <core/quads.h>
+#include <core/indexed.h>
+#include<core/rectilineargrid.h>
+
+
+using namespace vistle;
+constexpr std::array<char, 3> axisNames{ 'X', 'Y', 'Z' };
+
+TransformGrid::TransformGrid(const std::string &name, int moduleID, mpi::communicator comm)
+   : Module("transform grid", name, moduleID, comm)
+{
+    data_in = createInputPort("data_in", "input data");
+    data_out = createOutputPort("data_out", "output data");
+
+    for (size_t i = 0; i < 3; i++) {
+        m_reverse[i] = addIntParameter(std::string("reverse ") + axisNames[i] + "-axix", "", false, Parameter::Presentation::Boolean);
+    }
+
+    m_order = addIntParameter("orderOfGrodAxes", "", XYZ, Parameter::Presentation::Choice);
+    V_ENUM_SET_CHOICES(m_order, DimensionOrder);
+}
+
+
+bool TransformGrid::compute() {
+
+    Object::const_ptr obj = expect<Object>(data_in);
+    if (!obj)
+        return true;
+
+    Object::const_ptr geo = obj;
+    auto data = DataBase::as(obj);
+    if (data && data->grid()) {
+        geo = data->grid();
+    } else {
+        return true;
+    }
+
+    auto rgrid = RectilinearGrid::as(geo);
+    if (!rgrid) {
+        return true;
+    }
+    std::array<Index, 3>  dims;
+    std::array<const Scalar*, 3>  axes;
+    std::array<bool, 3> reverse;
+    for (size_t i = 0; i < 3; i++) {
+        dims[i] = rgrid->getNumDivisions(i);
+        axes[i] = rgrid->coords(i);
+        reverse[i] = m_reverse[i]->getValue();
+    }
+    //reorder
+    auto newOrder = dimensionOrder(static_cast<DimensionOrder>(m_order->getValue()));
+
+    dims = reorder(dims, newOrder);
+    axes = reorder(axes, newOrder);
+    reverse = reorder(reverse, newOrder);
+
+    RectilinearGrid::ptr gridOut(new RectilinearGrid(dims[0], dims[1], dims[2]));
+
+    for (size_t i = 0; i < 3; i++) {
+        if(reverse[i] ) {
+            for (size_t j = 0; j < dims[i]; j++) {
+                gridOut->coords(i).data()[j] = axes[i][dims[i] - j - 1];
+            }
+        }
+        else {
+            std::copy(axes[i], axes[i] + dims[i], gridOut->coords(i).data());
+        }
+    }
+    DataBase::ptr objOut = data->clone();
+    objOut->setGrid(gridOut);
+    addObject(data_out, objOut);
+   return true;
+}
+
+bool TransformGrid::prepare() {
+
+  
+
+   return true;
+}
+
+bool TransformGrid::reduce(int timestep) {
+
+
+
+   return Module::reduce(timestep);
+}
+
+MODULE_MAIN(TransformGrid)
+
+constexpr std::array<int, 3> dimensionOrder(DimensionOrder order) {
+    std::array<int, 3> o{};
+    switch (order) {
+    case XYZ:
+        o = { 0, 1, 2 };
+        break;
+    case XZY:
+        o = { 0, 2, 1 };
+        break;
+    case YXZ:
+        o = { 1, 0, 2 };
+        break;
+    case YZX:
+        o = { 1, 2, 0 };
+        break;
+    case ZXY:
+        o = { 2, 0, 1 };
+        break;
+    case ZYX:
+        o = { 2, 1, 0 };
+        break;
+    default:
+        break;
+    }
+    return o;
+}
---
+++ vistle/module/test/TransformGrid/TransformGrid.h
@@ -0,0 +1,41 @@
+#ifndef TRANSFORMGRID_H
+#define TRANSFORMGRID_H
+
+#include <module/module.h>
+
+DEFINE_ENUM_WITH_STRING_CONVERSIONS(DimensionOrder, (XYZ)(XZY)(YXZ)(YZX)(ZXY)(ZYX))
+
+constexpr std::array<int, 3> dimensionOrder(DimensionOrder order);
+
+template<typename T>
+constexpr T reorder(const T& val, const std::array<int, std::tuple_size<T>::value>& newOrder) {
+    T newVal{};
+    for (size_t i = 0; i < val.size(); i++) {
+        newVal[i] = val[newOrder[i]];
+    }
+    return newVal;
+}
+
+
+class TransformGrid : public vistle::Module {
+public:
+
+
+
+    TransformGrid(const std::string& name, int moduleID, mpi::communicator comm);
+
+    bool compute();
+
+    bool prepare();
+
+    bool reduce(int timestep);
+
+private:
+    vistle::Port *data_in, * data_out;
+    std::array<vistle::IntParameter*, 3> m_reverse;
+    vistle::IntParameter* m_order  = nullptr;
+};
+
+
+
+#endif // !TRANSFORMGRID_H
