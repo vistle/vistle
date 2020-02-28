2aead3ab87c32e8139d7709bbd8063b308d0d971
Dennis Grieger <dennis.grieger@hlrs.de>
2020-02-28 09:47:26 +0100

    added utility funktions for printing

---
+++ vistle/util/print.h
@@ -0,0 +1,62 @@
+#ifndef VISTLE_UTIL_PRINT_H
+#define VISTLE_UTIL_PRINT_H
+#include <iostream>
+namespace vistle {
+/*use this to easily switch off prints e.g the following code will do nothing:
+
+DoNotPrintInstance << "hello world" << std::endl;
+
+This is usefull in functions that only want to print under certain circumstances e.g:
+#ifdef X
+#define CERR std::cerr
+#else
+#define CERR DoNotPrintInstance
+#endif
+
+*/
+struct DoNotPrint {
+public:
+    template<typename T>
+    DoNotPrint& operator<<(const T& t) {
+        return *this;
+    }
+
+    DoNotPrint& operator<<(std::ostream& (*pManip)(std::ostream&)) {
+        return *this;
+    }
+};
+static DoNotPrint DoNotPrintInstance;
+
+/*
+    prints to stream if condition is true
+*/
+
+struct Print_if {
+public:
+    Print_if(bool b, std::ostream& stream = std::cerr)
+        :m_b(b)
+        , m_stream(stream) {
+    }
+    template<typename T>
+    Print_if& operator<<(const T& t) {
+        if (m_b) {
+            m_stream << t;
+        }
+        return *this;
+    }
+
+    Print_if& operator<<(std::ostream& (*pManip)(std::ostream&)) {
+        if (m_b) {
+            m_stream << pManip;
+        }
+        return *this;
+    }
+private:
+    bool m_b;
+    std::ostream& m_stream;
+};
+
+}
+
+
+#endif // !DO_NOT_PRINT_H
