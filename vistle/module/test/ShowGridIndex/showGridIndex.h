c804aa9a6f0e3170fd6d245484cbc1cb0b6f16f7
Dennis Grieger <dennis.grieger@hlrs.de>
2020-03-09 10:25:47 +0100

    module to show the index of vertices of unstructured grids

---
+++ vistle/module/test/ShowGridIndex/showGridIndex.h
@@ -0,0 +1,20 @@
+#ifndef SHOW_GRID_INDEX_H
+#define SHOW_GRID_INDEX_H
+
+#include <module/module.h>
+
+class ShowGridIndex : public vistle::Module {
+public:
+    ShowGridIndex(const std::string& name, int moduleID, boost::mpi::communicator comm);
+
+private:
+    vistle::Port* m_gridIn, * m_grid_out, *m_indexOut;
+
+
+
+    virtual bool compute() override;
+
+
+};
+
+#endif // !SHOW_GRID_INDEX_H
