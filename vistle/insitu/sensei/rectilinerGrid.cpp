4d39129315c95426fd124647ab6547aef292df69
hpcdgrie <grieger.hlrs.de>
2020-05-12 15:04:57 +0200

    worked in sense adapter

--- vistle/insitu/sensei/rectilinerGrid.cpp
+++ vistle/insitu/sensei/rectilinerGrid.cpp
@@ -1,19 +1,23 @@
 #include "rectilinerGrid.h"
+
 #include <insitu/core/transformArray.h>
-#include <core/structuredgrid.h>
+
+
 
 using namespace insitu;
 
-vistle::Object::const_ptr insitu::makeRectilinearGrid(const Mesh& mesh, message::SyncShmIDs& syncIDs)
+vistle::RectilinearGrid::const_ptr insitu::makeRectilinearGrid(const Mesh& mesh, message::SyncShmIDs& syncIDs, size_t timestep)
 {
 	assert(mesh.type == vistle::Object::RECTILINEARGRID);
+	
+	vistle::RectilinearGrid::ptr grid;
 	if (mesh.interleaved)
 	{
-		auto grid = syncIDs.createVistleObject<vistle::RectilinearGrid>(mesh.sizes[0], mesh.sizes[1], mesh.sizes[2]);
+		grid = syncIDs.createVistleObject<vistle::RectilinearGrid>(mesh.sizes[0], mesh.sizes[1], mesh.sizes[2]);
 		transformInterleavedArray(mesh.data[0].data, std::array<vistle::Scalar* ,3>{ grid->coords(0).data(), grid->coords(1).data(), grid->coords(2).data() }, mesh.data[0].size, mesh.data[0].dataType, static_cast<int>(mesh.dim)); //not tested
 	}
 	else {
-		auto grid = syncIDs.createVistleObject<vistle::RectilinearGrid>(mesh.data[0].size, mesh.data[1].size, mesh.data[2].size);
+		grid = syncIDs.createVistleObject<vistle::RectilinearGrid>(mesh.data[0].size, mesh.data[1].size, mesh.data[2].size);
 		
 		
 		for (size_t i = 0; i < static_cast<int>(mesh.dim); i++)
@@ -25,13 +29,14 @@ vistle::Object::const_ptr insitu::makeRectilinearGrid(const Mesh& mesh, message:
 			assert(mesh.min[i] >= 0);
 			int numTop = mesh.data[i].size - 1 - mesh.max[i];
 			assert(numTop >= 0);
-			grid->setNumGhostLayers(i, vistle::StructuredGrid::GhostLayerPosition::Bottom, mesh.min[i]);
-			grid->setNumGhostLayers(i, vistle::StructuredGrid::GhostLayerPosition::Top, numTop);
+			grid->setNumGhostLayers(i, vistle::RectilinearGrid::GhostLayerPosition::Bottom, mesh.min[i]);
+			grid->setNumGhostLayers(i, vistle::RectilinearGrid::GhostLayerPosition::Top, numTop);
 		}
-		return grid;
-	}
 
-	
+	}
+	grid->setBlock(mesh.blockNumber);
+	grid->setTimestep(timestep);
+	return grid;
 
 
 }
--- vistle/insitu/sensei/structuredGrid.cpp
+++ vistle/insitu/sensei/structuredGrid.cpp
@@ -1,1 +1,34 @@
 #include "structuredGrid.h"		
+#include <insitu/core/transformArray.h>
+
+vistle::StructuredGrid::const_ptr insitu::makeStructuredGrid(const Mesh& mesh, message::SyncShmIDs& syncIDs, size_t timestep)
+{
+	assert(mesh.type == vistle::Object::STRUCTUREDGRID);
+
+	vistle::StructuredGrid::ptr grid;
+	if (mesh.interleaved)
+	{
+		grid = syncIDs.createVistleObject<vistle::StructuredGrid>(mesh.sizes[0], mesh.sizes[1], mesh.sizes[2]);
+		transformInterleavedArray(mesh.data[0].data, std::array<vistle::Scalar*, 3>{ grid->x().data(), grid->y().data(), grid->z().data() }, mesh.data[0].size, mesh.data[0].dataType, static_cast<int>(mesh.dim)); //not tested
+	}
+	else {
+		grid = syncIDs.createVistleObject<vistle::StructuredGrid>(mesh.data[0].size, mesh.data[1].size, mesh.data[2].size);
+
+
+		transformArray(mesh.data[0], grid->x().begin());
+		transformArray(mesh.data[1], grid->y().begin());
+		transformArray(mesh.data[2], grid->z().begin());
+
+		for (size_t i = 0; i < 3; i++) {
+			assert(mesh.min[i] >= 0);
+			int numTop = mesh.data[i].size - 1 - mesh.max[i];
+			assert(numTop >= 0);
+			grid->setNumGhostLayers(i, vistle::StructuredGrid::GhostLayerPosition::Bottom, mesh.min[i]);
+			grid->setNumGhostLayers(i, vistle::StructuredGrid::GhostLayerPosition::Top, numTop);
+		}
+
+	}
+	grid->setBlock(mesh.blockNumber);
+	grid->setTimestep(timestep);
+	return grid;
+}
