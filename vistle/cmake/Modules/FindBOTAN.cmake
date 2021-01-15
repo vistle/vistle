0f0a656ac0cb7cea1cfe0b862a6c48ef701650da
Martin Aumüller <aumuell@reserv.at>
2021-01-15 20:52:09 +0100

    find botan debug library

--- vistle/cmake/Modules/FindBOTAN.cmake
+++ vistle/cmake/Modules/FindBOTAN.cmake
@@ -5,33 +5,36 @@
 #   BOTAN_FOUND  -  True if library and include directory are found
 # If set to TRUE, the following are also defined:
 #   BOTAN_INCLUDE_DIRS  -  The directory where to find the header file
-#   BOTAN_LIBRARIES  -  Where to find the library file
-#
-# For conveniance, these variables are also set. They have the same values
-# than the variables above.  The user can thus choose his/her prefered way
-# to write them.
-#   BOTAN_LIBRARY
-#   BOTAN_INCLUDE_DIR
+#   BOTAN_LIBRARIES  -  Where to find the library files
 #
 # This file is in the public domain
 
-set(BOTAN_NAMES botan botan-2 botan-2.13 botan-1 botan-1.11 botan-1.10)
+include(FindPackageHandleStandardArgs)
 
-find_path(BOTAN_INCLUDE_DIRS NAMES botan/botan.h
+set(BOTAN_NAMES botan botan-2 botan-1)
+set(BOTAN_NAMES_DEBUG botand botan-2d botan-1d)
+
+find_path(BOTAN_INCLUDE_DIR NAMES botan/botan.h
   PATH_SUFFIXES ${BOTAN_NAMES}
   DOC "The Botan include directory")
 
-find_library(BOTAN_LIBRARIES NAMES ${BOTAN_NAMES}
-  DOC "The Botan library")
-
-# Use some standard module to handle the QUIETLY and REQUIRED arguments, and
-# set BOTAN_FOUND to TRUE if these two variables are set.
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(BOTAN REQUIRED_VARS BOTAN_LIBRARIES BOTAN_INCLUDE_DIRS)
-
-if(BOTAN_FOUND)
-  set(BOTAN_LIBRARY ${BOTAN_LIBRARIES})
-  set(BOTAN_INCLUDE_DIR ${BOTAN_INCLUDE_DIRS})
+find_library(BOTAN_LIBRARY NAMES ${BOTAN_NAMES}
+  DOC "The Botan (release) library")
+if (MSVC)
+  find_library(BOTAN_LIBRARY_DEBUG NAMES ${BOTAN_NAMES_DEBUG}
+    DOC "The Botan debug library")
+  find_package_handle_standard_args(BOTAN REQUIRED_VARS BOTAN_LIBRARY BOTAN_LIBRARY_DEBUG BOTAN_INCLUDE_DIR)
+else()
+  find_package_handle_standard_args(BOTAN REQUIRED_VARS BOTAN_LIBRARY BOTAN_INCLUDE_DIR)
 endif()
 
-mark_as_advanced(BOTAN_INCLUDE_DIRS BOTAN_LIBRARIES)
+if(BOTAN_FOUND)
+  set(BOTAN_INCLUDE_DIRS ${BOTAN_INCLUDE_DIR})
+  if (MSVC)
+    set(BOTAN_LIBRARIES optimized ${BOTAN_LIBRARY} debug ${BOTAN_LIBRARY_DEBUG})
+  else()
+    set(BOTAN_LIBRARIES ${BOTAN_LIBRARY})
+  endif()
+endif()
+
+mark_as_advanced(BOTAN_INCLUDE_DIR BOTAN_LIBRARY BOTAN_LIBRARY_DEBUG)
