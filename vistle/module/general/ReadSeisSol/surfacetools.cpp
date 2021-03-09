df19ab16d9925408d6576179d1529fdca377f631
mdjur <marko.djuric@gmx.de>
2021-03-09 16:01:05 +0100

    ReadSeisSol: DomainSurface for internal use.

---
+++ vistle/module/general/ReadSeisSol/surfacetools.cpp
@@ -0,0 +1,33 @@
+#include "surfacetools.h"
+
+namespace vistle {
+void addSurfaceFaces(const Polygons::ptr poly, const UnstructuredGrid::ptr &ugrid)
+{
+    const auto &nf = ugrid->getNeighborFinder();
+    const auto &tl = ugrid->tl().data();
+    const auto &el = ugrid->el().data();
+    const auto &cl = ugrid->cl().data();
+    auto &pl = poly->el();
+    auto &pcl = poly->cl();
+    for (Index i = 0; i < ugrid->getSize(); ++i) {
+        Byte t = tl[i] & UnstructuredGrid::TYPE_MASK;
+        const Index elStart = el[i];
+        const auto numFaces = UnstructuredGrid::NumFaces[t];
+        const auto &faces = UnstructuredGrid::FaceVertices[t];
+        for (int f = 0; f < numFaces; ++f) {
+            const auto &face = faces[f];
+            Index neighbour = 0;
+            if (UnstructuredGrid::Dimensionality[t] == 3)
+                neighbour =
+                    nf.getNeighborElement(i, cl[elStart + face[0]], cl[elStart + face[1]], cl[elStart + face[2]]);
+            if (UnstructuredGrid::Dimensionality[t] == 2 || neighbour == InvalidIndex) {
+                const auto facesize = UnstructuredGrid::FaceSizes[t][f];
+                for (unsigned j = 0; j < facesize; ++j) {
+                    pcl.push_back(cl[elStart + face[j]]);
+                }
+                pl.push_back(pcl.size());
+            }
+        }
+    }
+}
+} // namespace vistle
---
+++ vistle/module/general/ReadSeisSol/surfacetools.h
@@ -0,0 +1,52 @@
+#ifndef VISTLE_MODULE_UTIL_SURFACETOOLS_H
+#define VISTLE_MODULE_UTIL_SURFACETOOLS_H
+
+#include <vistle/core/celltypes.h>
+#include <vistle/core/indexed.h>
+#include <vistle/core/unstr.h>
+#include <vistle/core/polygons.h>
+
+#include <iterator>
+
+namespace vistle {
+namespace {
+template<class T>
+struct IndexedVars {
+    T numElem;
+    T numVertices;
+    T numCorners;
+
+    IndexedVars(const T &elem, const T &vert, const T &corners): numElem(elem), numVertices(vert), numCorners(corners)
+    {}
+};
+
+} // namespace
+
+void addSurfaceFaces(const Polygons::ptr poly, const UnstructuredGrid::ptr &ugrid);
+
+/**
+  * DomainSurface-Code for internal use. src: /vistle/module/generald/DomainSurface/DomainSurface.cpp
+  */
+template<class ContainerIt, class ContainerItType, class T>
+Polygons::ptr extractSurface(ContainerIt beginElemList, ContainerIt beginConnectList, ContainerItType beginTypeList,
+                             const IndexedVars<T> &vars, const cell::CellType &cellType)
+{
+    //surface
+    Polygons::ptr poly(new Polygons(0, 0, 0));
+
+    //create unstructured grid
+    UnstructuredGrid::ptr ugrid(new UnstructuredGrid(vars.numElem, vars.numCorners, vars.numVertices));
+    std::copy(beginElemList, beginElemList + vars.numElem, ugrid->el());
+    std::copy(beginConnectList, beginConnectList + vars.numElem, ugrid->cl());
+    std::copy(beginTypeList, beginTypeList + vars.numElem, ugrid->tl());
+
+    addSurfaceFaces(poly, ugrid, vars);
+
+    if (poly->getNumElements() == 0)
+        return Polygons::ptr();
+
+    return poly;
+}
+} // namespace vistle
+#endif
+
