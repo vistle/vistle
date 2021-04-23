b7122e06c380f9eb38a83c2d8daea2f13317f8d3
Martin Aumüller <aumuell@reserv.at>
2021-04-23 14:07:48 +0200

    speed up linking
    use gold or lld

---
+++ vistle/cmake/Modules/ConfigureLinker.cmake
@@ -0,0 +1,11 @@
+if(NOT VISTLE_LINKER)
+    set(VISTLE_LINKER "Default" CACHE STRING "Configure linker" FORCE)
+endif()
+# Set the possible values for cmake-gui
+set_property(CACHE VISTLE_LINKER PROPERTY STRINGS "Default" "bfd" "gold" "lld")
+if("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
+    if (VISTLE_LINKER AND NOT VISTLE_LINKER STREQUAL "Default")
+        add_link_options(-fuse-ld=${VISTLE_LINKER})
+    endif()
+    add_link_options(-flto)
+endif()
