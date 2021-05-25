e3f4c9705aefa64656526c444858091f0fd7fb26
Martin Aumüller <aumuell@reserv.at>
2021-05-25 14:06:04 +0200

    add_link_options requires at least cmake 3.13

--- vistle/cmake/Modules/ConfigureLinker.cmake
+++ vistle/cmake/Modules/ConfigureLinker.cmake
@@ -1,11 +1,14 @@
-if(NOT VISTLE_LINKER)
-    set(VISTLE_LINKER "Default" CACHE STRING "Configure linker" FORCE)
-endif()
-# Set the possible values for cmake-gui
-set_property(CACHE VISTLE_LINKER PROPERTY STRINGS "Default" "bfd" "gold" "lld")
-if("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
-    if (VISTLE_LINKER AND NOT VISTLE_LINKER STREQUAL "Default")
-        add_link_options(-fuse-ld=${VISTLE_LINKER})
+if(NOT ${CMAKE_VERSION} VERSION_LESS 3.13)
+    # add_link_options requires at least cmake 3.13
+    if(NOT VISTLE_LINKER)
+        set(VISTLE_LINKER "Default" CACHE STRING "Configure linker" FORCE)
     endif()
-    add_link_options(-flto)
+    # Set the possible values for cmake-gui
+    set_property(CACHE VISTLE_LINKER PROPERTY STRINGS "Default" "bfd" "gold" "lld")
+    if("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
+        if (VISTLE_LINKER AND NOT VISTLE_LINKER STREQUAL "Default")
+            add_link_options(-fuse-ld=${VISTLE_LINKER})
+        endif()
+        add_link_options(-flto)
+    endif()
 endif()
