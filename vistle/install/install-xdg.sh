cc5a8cb972b753f0a6cae84b13d0ab8b392cb14c
Martin Aumüller <aumuell@reserv.at>
2021-08-25 15:16:49 +0200

    open vistle:// urls on linux

---
+++ vistle/install/install-xdg.sh
@@ -0,0 +1,10 @@
+xdg-mime install hlrs-vsl.desktop
+
+xdg-desktop-menu install --noupdate hlrs-vistle.desktop
+xdg-desktop-menu install --noupdate hlrs-vistle_opener.desktop
+xdg-desktop-menu forceupdate
+xdg-mime default hlrs-vistle_opener.desktop x-scheme-handler/vistle
+
+xdg-icon-resource install --noupdate --size 48 hlrs-vistle.png
+xdg-icon-resource install --noupdate --context mimetypes --size 48 hlrs-vistle.png text-x-vsl
+xdg-icon-resource forceupdate
