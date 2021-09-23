f796eaff5be7589dffa69c883f2ce2bcf52eebcb
Martin Aumüller <aumuell@reserv.at>
2021-09-23 17:40:13 +0200

    reformat ObjC

--- vistle/gui/macosutils.m
+++ vistle/gui/macosutils.m
@@ -2,7 +2,8 @@
 
 #include <AppKit/AppKit.h>
 
-void macos_disable_tabs() {
+void macos_disable_tabs()
+{
 #ifdef AVAILABLE_MAC_OS_X_VERSION_10_12_AND_LATER
     // Remove (don't allow) the "Show Tab Bar" menu item from the "View" menu, if supported
 
