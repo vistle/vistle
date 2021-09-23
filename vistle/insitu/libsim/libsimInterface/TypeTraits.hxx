1b032c844429effc91696642e10a86498efac7e5
Martin Aumüller <aumuell@reserv.at>
2021-09-23 17:12:56 +0200

    somehow these files seem to not have been formatted yesterday

--- vistle/insitu/libsim/libsimInterface/TypeTraits.hxx
+++ vistle/insitu/libsim/libsimInterface/TypeTraits.hxx
@@ -6,7 +6,7 @@
 #define simV2_TypeTraits_hxx
 
 
-// template type traits for conversions between VisIt's datatype 
+// template type traits for conversions between VisIt's datatype
 // enumerations (eg VISIT_DATATYPE_FLOAT), VTK arrays and enumerations
 // (eg vtkFloatArray, VTK_FLOAT), and C++ types (eg. float).
 // Given a VisIt C++ type enum, the class provides the following
@@ -14,20 +14,19 @@
 //      ::cppType -> C++ type (eg float)
 //      ::vtkType -> a vtkDataArray subclass (eg vtkFloatArray)
 //      ::vtkEnum -> the VTK C++ type enum (eg VTK_FLOAT)
-template<int E> class simV2_TypeTraits {};
+template<int E>
+class simV2_TypeTraits {};
 
-#define simV2_TypeTraits_Specialize(visitE, cppT)   \
-template<> class simV2_TypeTraits<visitE>                       \
-{                                                               \
-public:                                                         \
-    typedef cppT cppType;                                       \
-};
+#define simV2_TypeTraits_Specialize(visitE, cppT) \
+    template<> \
+    class simV2_TypeTraits<visitE> { \
+    public: \
+        typedef cppT cppType; \
+    };
 
-simV2_TypeTraits_Specialize(VISIT_DATATYPE_FLOAT, float)
-simV2_TypeTraits_Specialize(VISIT_DATATYPE_DOUBLE, double)
-simV2_TypeTraits_Specialize(VISIT_DATATYPE_INT, int)
-simV2_TypeTraits_Specialize(VISIT_DATATYPE_CHAR, unsigned char)
-simV2_TypeTraits_Specialize(VISIT_DATATYPE_LONG, long)
+simV2_TypeTraits_Specialize(VISIT_DATATYPE_FLOAT, float) simV2_TypeTraits_Specialize(VISIT_DATATYPE_DOUBLE, double)
+    simV2_TypeTraits_Specialize(VISIT_DATATYPE_INT, int) simV2_TypeTraits_Specialize(VISIT_DATATYPE_CHAR, unsigned char)
+        simV2_TypeTraits_Specialize(VISIT_DATATYPE_LONG, long)
 
 // The simV2TemplateMacro avoids duplication of long switch statement
 // case lists. It enumerates the "case" body for each datatype in
@@ -40,45 +39,40 @@ simV2_TypeTraits_Specialize(VISIT_DATATYPE_LONG, long)
 //   simV2TemplateMacro(
 //       myFunc(static_cast<simV2_TT::cppType>(data), arg2));
 //   }
-#define simV2TemplateMacroCase(visitE, call)                                \
-  case visitE: { typedef simV2_TypeTraits<visitE> simV2_TT; call; }; break
-#define simV2TemplateMacro(call)                                            \
-  simV2TemplateMacroCase(VISIT_DATATYPE_FLOAT, call);                       \
-  simV2TemplateMacroCase(VISIT_DATATYPE_DOUBLE, call);                      \
-  simV2TemplateMacroCase(VISIT_DATATYPE_INT, call);                         \
-  simV2TemplateMacroCase(VISIT_DATATYPE_CHAR, call);                        \
-  simV2TemplateMacroCase(VISIT_DATATYPE_LONG, call);
+#define simV2TemplateMacroCase(visitE, call) \
+    case visitE: { \
+        typedef simV2_TypeTraits<visitE> simV2_TT; \
+        call; \
+    }; break
+#define simV2TemplateMacro(call) \
+    simV2TemplateMacroCase(VISIT_DATATYPE_FLOAT, call); \
+    simV2TemplateMacroCase(VISIT_DATATYPE_DOUBLE, call); \
+    simV2TemplateMacroCase(VISIT_DATATYPE_INT, call); \
+    simV2TemplateMacroCase(VISIT_DATATYPE_CHAR, call); \
+    simV2TemplateMacroCase(VISIT_DATATYPE_LONG, call);
 
 // The simV2FloatTemplateMacro does the same things as simV2TemplateMacro
 // however it only expands the cases for float and double. This addresses
 // common situations in numerical computation where integers don't make sense
 // and for use with vtkPoints which only suppport float and double.
-#define simV2FloatTemplateMacro(call)                                            \
-  simV2TemplateMacroCase(VISIT_DATATYPE_FLOAT, call);                       \
-  simV2TemplateMacroCase(VISIT_DATATYPE_DOUBLE, call);
+#define simV2FloatTemplateMacro(call) \
+    simV2TemplateMacroCase(VISIT_DATATYPE_FLOAT, call); \
+    simV2TemplateMacroCase(VISIT_DATATYPE_DOUBLE, call);
 
-// verify that the type enum names one of the supported types
-inline
-bool simV2_ValidDataType(int dataType)
+    // verify that the type enum names one of the supported types
+    inline bool simV2_ValidDataType(int dataType)
 {
-    if ( (dataType == VISIT_DATATYPE_CHAR)
-      || (dataType == VISIT_DATATYPE_INT)
-      || (dataType == VISIT_DATATYPE_LONG)
-      || (dataType == VISIT_DATATYPE_FLOAT)
-      || (dataType == VISIT_DATATYPE_DOUBLE) )
-    {
+    if ((dataType == VISIT_DATATYPE_CHAR) || (dataType == VISIT_DATATYPE_INT) || (dataType == VISIT_DATATYPE_LONG) ||
+        (dataType == VISIT_DATATYPE_FLOAT) || (dataType == VISIT_DATATYPE_DOUBLE)) {
         return true;
     }
     return false;
 }
 
 // verify that the type enum names one of the supported types
-inline
-bool simV2_ValidFloatDataType(int dataType)
+inline bool simV2_ValidFloatDataType(int dataType)
 {
-    if ( (dataType == VISIT_DATATYPE_FLOAT)
-      || (dataType == VISIT_DATATYPE_DOUBLE) )
-    {
+    if ((dataType == VISIT_DATATYPE_FLOAT) || (dataType == VISIT_DATATYPE_DOUBLE)) {
         return true;
     }
     return false;
