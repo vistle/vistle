0365ffed5b75e367b54e366ccc68ed5cd1266974
mdjur <marko.djuric@gmx.de>
2021-10-13 10:35:11 +0200

    extract templates for structured ghostcell boundaries to utils

--- vistle/module/general/utils/ghost.h
+++ vistle/module/general/utils/ghost.h
@@ -1,19 +1,22 @@
-#ifndef _READTSUNAMI_IMPL_H
-#define _READTSUNAMI_IMPL_H
+#ifndef GHOST_H
+#define GHOST_H
+
 #include <algorithm>
 #include <iterator>
 #include <vector>
 
+namespace vistle {
+
 /**
-  * Adds ghost cells to given startvalue and current count of data based on given dimension.
+  * @brief Adds ghost cells to given startvalue and current count of data based on given dimension.
   *
   * @start: start value for reading data.
   * @count: count of values for current block.
   * @dimData: dimension of data for corresponding start and count.
   * @numGhost: number of ghost cells to be added.
   */
-template<typename ForwardIt>
-void addGhostStructured_tmpl(ForwardIt &start, ForwardIt &count, const ForwardIt &dimData, const ForwardIt &numGhost)
+template<typename NumType>
+void structured_ghost_addition(NumType &start, NumType &count, const NumType &dimData, const NumType &numGhost)
 {
     auto partitionSize = start + count;
     if (start == 0) {
@@ -28,39 +31,17 @@ void addGhostStructured_tmpl(ForwardIt &start, ForwardIt &count, const ForwardIt
 }
 
 /**
-  * Adds ghost cells to given startvalue and current count of data based on given dimension.
-  *
-  * @start: Begin iterator.
-  * @count: End iterator.
-  * @dimData: dimension of whole dataset (e.g. latitude = 773).
-  * @numGhost: number of ghost cells to be added.
-  */
-/* template<class InputIt, class NumericType> */
-/* void addGhostStructured_tmpl(InputIt &start, InputIt &end, const NumericType &dimData, const NumericType &numGhost) */
-/* { */
-/*     auto size = std::distance(start, end); */
-/*     if (size == dimData) { */
-/*         start -= numGhost; */
-/*         end += numGhost; */
-/*     } else if (size > 0) { */
-/*         start -= numGhost; */
-/*         end += 2 * numGhost; */
-/*     } else */
-/*         end += numGhost; */
-/* } */
-
-/**
-  * Generates a vector<T> for structured data which contains block values for each dim partitioned for current blocknumber.
+  * @brief Generates partion for structured grid which contains block values for each dim partitioned for current blocknumber.
   *
   * @first: Begin pointer of block container.
   * @last: End pointer of block container.
-  * @d_first: Begin pointer of container that results will be written to.
+  * @d_first: Begin pointer of container where results will be written to.
   * @blockNum: Current blocknumber.
   * @return: Begin iterator of container which contains block values specified for each direction and current blocknumber.
   */
 template<class ForwardIt, class OutputBlockIt, class NumericType>
-OutputBlockIt blockPartitionStructured_tmpl(ForwardIt first, ForwardIt last, OutputBlockIt d_first,
-                                            const NumericType &blockNum)
+OutputBlockIt structured_block_partition(ForwardIt first, ForwardIt last, OutputBlockIt d_first,
+                                         const NumericType &blockNum)
 {
     const auto numBlocks{std::distance(first, last)};
     std::transform(first, last, d_first, [it = 0, &numBlocks, b = blockNum](const NumericType &bS) mutable {
@@ -76,24 +57,23 @@ OutputBlockIt blockPartitionStructured_tmpl(ForwardIt first, ForwardIt last, Out
 }
 
 /**
-  * Generates a vector<T> for structured data which contains block values for each dim partitioned for current blocknumber.
+  * @brief Generates a vector<T> for structured grid which contains block values for each dim partitioned for current blocknumber.
   *
   * @blockSize: Blocksize specified for each dim.
   * @bockNum: Current active blocknumber.
   * @return: Vector<T> with block values specified for each direction and current blocknumber.
   */
 template<typename NumericType>
-const std::vector<NumericType> blockPartitionStructured_tmpl(const std::vector<NumericType> &blockSize,
-                                                             const NumericType &blockNum)
+const std::vector<NumericType> structured_block_partition(const std::vector<NumericType> &blockSize,
+                                                          const NumericType &blockNum)
 {
     std::vector<NumericType> distBlocks(blockSize.size());
     blockPartitionStructured_tmpl(blockSize.begin(), blockSize.end(), distBlocks.begin(), blockNum);
     return distBlocks;
 }
 
-
 /**
-  * Generates a vector<T> for structured data which contains block values for each dim partitioned for current blocknumber.
+  * @brief Generates a vector<T> for structured grid which contains block values for each dim partitioned for current blocknumber.
   *
   * @first: Begin pointer of block container.
   * @last: End pointer of block container.
@@ -101,12 +81,12 @@ const std::vector<NumericType> blockPartitionStructured_tmpl(const std::vector<N
   * @return: Vector<T> with block values specified for each direction and current blocknumber.
   */
 template<class ForwardIt, class NumericType>
-const std::vector<NumericType> blockPartitionStructured_tmpl(ForwardIt first, ForwardIt last,
-                                                             const NumericType &blockNum)
+const std::vector<NumericType> structured_block_partition(ForwardIt first, ForwardIt last, const NumericType &blockNum)
 {
     std::vector<NumericType> distBlocks(std::distance(first, last));
     blockPartitionStructured_tmpl(first, last, distBlocks.begin(), blockNum);
     return distBlocks;
 }
 
+} // namespace vistle
 #endif
