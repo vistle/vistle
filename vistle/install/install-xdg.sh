1b1bffecdeef3c934318b6a3ec6b5bc826979443
Martin Aumüller <aumuell@reserv.at>
2021-10-20 09:32:15 +0200

    make Science the primary category

--- vistle/install/install-xdg.sh
+++ vistle/install/install-xdg.sh
@@ -6,5 +6,5 @@ xdg-desktop-menu forceupdate
 xdg-mime default hlrs-vistle_opener.desktop x-scheme-handler/vistle
 
 xdg-icon-resource install --noupdate --size 48 hlrs-vistle.png
-xdg-icon-resource install --noupdate --context mimetypes --size 48 hlrs-vistle.png text-x-vsl
+xdg-icon-resource install --noupdate --context mimetypes --size 48 hlrs-vistle.png text/x-vsl
 xdg-icon-resource forceupdate
