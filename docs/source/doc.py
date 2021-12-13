7758bcb298e9d360d0c170c894de13743cbee845
mdjur <marko.djuric@gmx.de>
2021-12-13 12:29:27 +0100

    further optimization

--- docs/source/doc.py
+++ docs/source/doc.py
@@ -4,11 +4,8 @@ from collections import namedtuple
 
 Markdown = namedtuple("Markdown", "root filename")
 
-index_link_list = []
-
 # default
 docpy_path = os.path.dirname(os.path.realpath(__file__))
-# link_output_path = docpy_path + "/modules"
 indent = "   {}\n"
 md_extension_str = ".md"
 link_str = "_link"
@@ -23,25 +20,21 @@ def createLinks(markdown_list, root_link_output_path):
     if not os.path.exists(root_link_output_path):
         print("Directory {} does not exist.".format(root_link_output_path))
         return
-    for root, filename in markdown_list:
-        rel_dir_from_module_dir = os.path.relpath(root, root_link_output_path)
-        createLinkToMarkdownFile(root_link_output_path, rel_dir_from_module_dir, filename)
+    return [createLinkToMarkdownFile(root_link_output_path,
+                                     os.path.relpath(root, root_link_output_path),
+                                     filename)
+            for root, filename in markdown_list]
 
 
-def strInFile(file, s) -> bool:
-    with open(file, 'r') as readOnlyFile:
-        if s in readOnlyFile.read():
-            return True
-    return False
+def strInFile(openReadOnlyFile, s) -> bool:
+    if s in openReadOnlyFile.read():
+        return True
 
 
 def addLinkToRSTFile(rst_path, md_link_filename):
-    if strInFile(rst_path, md_link_filename):
-        return
-    writeableFile = open(rst_path, 'a')
-    writeableFile.write(indent.format(md_link_filename))
-    writeableFile.close()
-
+    with open(rst_path, 'a+') as arf:
+        if not strInFile(arf, md_link_filename):
+            arf.write(indent.format(md_link_filename))
 
 def createRSTHeaderNameFromRootPath(path, file_path = False):
     md_name = os.path.basename(path)
@@ -74,10 +67,10 @@ def createLinkToMarkdownFile(md_linkdir, md_root, md_filename):
         return
     with open(new_link_file_path, 'w') as f:
         f.write(myst_include_str % md_origin_path)
-    index_link_list.append(new_link_file_path.split('/')[-1])
+    return new_link_file_path.split('/')[-1]
 
 
-def searchFileInPath(path, output_list, predicate_func):
+def searchFilesInPath(path, output_list, predicate_func):
     for root, _, files in os.walk(path):
         for file in files:
             if predicate_func(file):
@@ -85,8 +78,8 @@ def searchFileInPath(path, output_list, predicate_func):
 
 
 def createIndexFile(name):
-    with open(name, 'w') as writeableFile:
-        writeableFile.write(createRSTHeaderNameFromRootPath(name, True))
+    with open(name, 'w') as wf:
+        wf.write(createRSTHeaderNameFromRootPath(name, True))
 
 
 def createIndexFileIfNotExisting(link_path):
@@ -102,15 +95,11 @@ def createIndexFileIfNotExisting(link_path):
 def run(root_path, search_dir_list, link_docs_output_relpath):
     markdown_files = []
     endswith = lambda file : file.endswith(md_extension_str)
-    for dir in search_dir_list:
-        rel_dir_path = root_path + '/' + dir
-        searchFileInPath(rel_dir_path, markdown_files, endswith)
-
+    [searchFilesInPath(root_path + '/' + dir, markdown_files, endswith) for dir in search_dir_list]
     root_link_output_path = docpy_path + '/' + link_docs_output_relpath
     link_output_path = createIndexFileIfNotExisting(root_link_output_path)
-    createLinks(markdown_files, root_link_output_path)
+    index_link_list = createLinks(markdown_files, root_link_output_path)
     [addLinkToRSTFile(link_output_path, link) for link in sorted(index_link_list)]
-    index_link_list.clear()
 
 
 if __name__ == "__main__":
