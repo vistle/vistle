92120a2837b6874a925b249324bce2d3d27b06ba
mdjur <marko.djuric@gmx.de>
2021-12-15 11:31:21 +0100

    add files method in dir + some comments

--- docs/source/clear.py
+++ docs/source/clear.py
@@ -1,12 +1,43 @@
 import os
-import shutil
+from shutil import rmtree
+from pathlib import Path
 
-def clearDir(path):
-    if os.path.exists(path):
-        shutil.rmtree(path)
+ERR_STR = "Error: {path} : {err_mss}"
+
+
+def deleteDir(path):
+    """Delete complete directory.
+
+    Keyword arguments:
+    path -- string to directory
+    """
+    try:
+        rmtree(path)
         print("Cleared " + path + "!")
-        return
-    print("Not existing path " + path)
+    except OSError as e:
+        print(ERR_STR.format(path=path, err_mss=e.strerror))
+
+
+def deleteFilesInDir(path, exclude=[], pattern='*'):
+    """
+    Delete all files which matching the pattern in path and exclude given filenames.
+
+    Keyword arguments:
+    path    -- string to directory
+    exclude -- list of files
+    pattern -- regex pattern
+    """
+    for file in Path(path).glob(pattern):
+        if not any(Path(file).name == eF for eF in exclude):
+            try:
+                file.unlink()
+            except OSError as e:
+                print(ERR_STR.format(path=path, err_mss=e.strerror))
+
 
 if __name__ == "__main__":
-    clearDir("/home/mdjur/program/vistle/docs/build")
+    # some tests
+    # deleteDir("/home/mdjur/program/vistle/docs/build")
+    # deleteFilesInDir("/home/mdjur/program/vistle/docs/source/lib", "message_link.md libsim_link.md".split(), "*.md")
+    # deleteFilesInDir("/home/mdjur/program/vistle/docs/source/lib", ["index.rst"])
+    deleteFilesInDir("/home/mdjur/program/vistle/docs/source/lib")
