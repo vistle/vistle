c6f78ba0b8be9d1498027ca393567cfc238431b7
Martin Aumüller <aumuell@reserv.at>
2022-02-02 22:11:07 +0100

    simplify serialization of structured grids

--- lib/vistle/core/uniformgrid_impl.h
+++ lib/vistle/core/uniformgrid_impl.h
@@ -13,21 +13,12 @@ namespace vistle {
 template<class Archive>
 void UniformGrid::Data::serialize(Archive &ar)
 {
-    ar &V_NAME(ar, "base_structuredGridBase", serialize_base<Base::Data>(ar, *this));
-    ar &V_NAME(ar, "numElements", numDivisions);
+    ar &V_NAME(ar, "unibase", serialize_base<Base::Data>(ar, *this));
     ar &V_NAME(ar, "min", min);
     ar &V_NAME(ar, "max", max);
-
-    for (int c = 0; c < 3; c++) {
-        std::string nvpTag = "ghostLayers" + std::to_string(c);
-        std::string tag0 = nvpTag + "0";
-        std::string tag1 = nvpTag + "1";
-        ar &V_NAME(ar, tag0.c_str(), ghostLayers[c][0]);
-        ar &V_NAME(ar, tag1.c_str(), ghostLayers[c][1]);
-
-        ar &V_NAME(ar, ("indexoffset" + std::to_string(c)).c_str(), indexOffset[c]);
-    }
-
+    ar &V_NAME(ar, "numdivisions", numDivisions);
+    ar &V_NAME(ar, "indexoffset", indexOffset);
+    ar &V_NAME(ar, "ghostlayers", ghostLayers);
     ar &V_NAME(ar, "normals", normals);
 }
 
