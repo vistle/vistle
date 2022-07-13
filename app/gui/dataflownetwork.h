d2ec41059ef77798b5d9566a721b71b0fce8d919
hpcdgrie <dennis.grieger@hlrs.de>
2022-07-13 15:48:49 +0200

    Fixed dataflow snapshot bounding bix

--- app/gui/dataflownetwork.h
+++ app/gui/dataflownetwork.h
@@ -41,7 +41,7 @@ public:
     Module *findModule(const boost::uuids::uuid &spawnUuid) const;
 
     QColor highlightColor() const;
-
+    QRect calculateBoundingBox() const;
 public slots:
     void addModule(int moduleId, const boost::uuids::uuid &spawnUuid, QString name);
     void deleteModule(int moduleId);
