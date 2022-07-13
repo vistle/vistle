f7719df1ff921f3ce79502177d27a8d5fa3c7b5c
hpcdgrie <dennis.grieger@hlrs.de>
2022-07-13 10:44:32 +0200

    Moved Documentaion macros in extra file

---
+++ cmake/Documentation.cmake
@@ -0,0 +1,67 @@
+option(VISTLE_GENERATE_DOCUMENTATION "turn this on after building to update documentation files" OFF)
+
+macro(genereate_documentation targetname)
+    set(CREATE_DOCUMENTATION_SCRIPT ${PROJECT_SOURCE_DIR}/doc/createDocumentation.sh)
+    if(WIN32)
+        set(CREATE_DOCUMENTATION_SCRIPT ${PROJECT_SOURCE_DIR}/doc/createDocumentation.bat)
+    endif()
+    set(VISTLE_FILE ${PROJECT_SOURCE_DIR}/doc/generateModuleInfo.vsl)
+    set(DOC_COMMAND ${CREATE_DOCUMENTATION_SCRIPT} ${targetname} ${CMAKE_CURRENT_SOURCE_DIR} ${VISTLE_FILE})
+    set(OUTPUT_FILE ${PROJECT_SOURCE_DIR}/doc/GenModInfo/moduleDescriptions/${targetname}.md)
+    set(INPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/${targetname}.md)
+    if(NOT EXISTS ${INPUT_FILE})
+        set(INPUT_FILE)
+    endif()
+
+    add_custom_command(
+        OUTPUT ${OUTPUT_FILE}
+        COMMAND ${DOC_COMMAND}
+        DEPENDS #build if changes in:
+                ${INPUT_FILE} #the custom documentation
+                ${targetname} #the module's source code
+                ${CREATE_DOCUMENTATION_SCRIPT} #the bridge script
+                ${VISTLE_FILE} #the file that gets loaded by vistle to generate the documentation
+                ${PROJECT_SOURCE_DIR}/doc/GenModInfo/genModInfo.py #dependency of VISTLE_FILE
+                ${DOCUMENTATION_DEPENDENCIES} #custom dependencies set by the calling module 
+        COMMENT "Generating documentation for " ${targetname})
+    set(ALWAYS_BUILD)
+    if(VISTLE_GENERATE_DOCUMENTATION)
+        set(ALWAYS_BUILD ALL)
+    endif()
+    add_custom_target(
+        ${targetname}_doc
+        ${ALWAYS_BUILD}
+        DEPENDS ${OUTPUT_FILE})
+    add_dependencies(vistle_doc ${targetname}_doc)
+endmacro()
+
+macro(generate_network_snapshot targetname network_file)
+    add_custom_command( #create a snapshot of the pipeline
+        OUTPUT  ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png
+        COMMAND vistle --snapshot ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vsl
+        DEPENDS  ${CMAKE_CURRENT_LIST_DIR}/${VISTLE_FILE}.vsl targetname
+        COMMENT "Generating network snapshot for " ${network_file}.vsl)
+
+    add_custom_target(
+        ${targetname}_${VISTLE_FILE}_workflow
+        ${ALWAYS_BUILD}
+        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${VISTLE_FILE}_workflow.png)
+    add_dependencies(${targetname}_doc ${targetname}_${VISTLE_FILE}_workflow)
+endmacro()
+
+macro(generate_snapshots targetname network_file)
+    if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vwp) #if we have a viewpoint file we can generate an result image, only first viewpoint is considered, only first cover is considered
+        add_custom_command(
+            OUTPUT  ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_result.png ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png
+            COMMAND ${CMAKE_COMMAND} -E env VISTLE_DOC_IMAGE_NAME=${network_file} VISTLE_DOC_SOURCE_DIR=${CMAKE_CURRENT_LIST_DIR} VISTLE_DOC_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR} vistle ${PROJECT_SOURCE_DIR}/doc/resultSnapShot.py
+            DEPENDS ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vsl ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vwp ${targetname}
+            COMMENT "Generating network and result snapshot for " ${network_file}.vsl)
+        add_custom_target(
+            ${targetname}_${network_file}_result
+            ${ALWAYS_BUILD}
+            DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_result.png ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png)
+        add_dependencies(${targetname}_doc ${targetname}_${network_file}_result)
+    else()
+        message(WARNING "can not generate snapshots for " ${targetname} " " ${network_file} ": missing viewpoint file, make sure a viewpoint file named \"" ${network_file} ".vwp\" exists!")
+    endif()
+endmacro()
