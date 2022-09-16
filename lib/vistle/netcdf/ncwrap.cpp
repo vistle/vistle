02fb67880373e678f787e8cd97be9b0fd9ec8634
Martin Aumüller <aumuell@reserv.at>
2022-09-16 15:47:39 +0200

    allow for reusing NetCDF wrapper

---
+++ lib/vistle/netcdf/ncwrap.cpp
@@ -0,0 +1,237 @@
+#include "ncwrap.h"
+
+namespace vistle {
+
+NcFile::NcFile(int id, const std::string &name, bool parallel): parallel(parallel), valid(true), name(name), id(id)
+{}
+NcFile::NcFile(const std::string &name, int err): error(err), valid(false), name(name)
+{}
+NcFile::NcFile(NcFile &&o): parallel(o.parallel), valid(std::exchange(o.valid, false)), name(o.name), id(o.id)
+{}
+NcFile::~NcFile()
+{
+    if (valid) {
+        nc_close(id);
+        valid = false;
+    }
+}
+NcFile::operator bool() const
+{
+    return error == NC_NOERR;
+}
+NcFile::operator int() const
+{
+    return id;
+}
+
+NcFile NcFile::open(const std::string &name)
+{
+    int ncid = -1;
+    int err = nc_open(name.c_str(), NC_NOWRITE, &ncid);
+    if (err != NC_NOERR) {
+        std::cerr << "error opening NetCDF file " << name << ": " << nc_strerror(err) << std::endl;
+        return NcFile(name, err);
+    }
+    return NcFile(ncid, name, false);
+}
+
+NcFile NcFile::open(const std::string &name, const MPI_Comm &comm)
+{
+#ifdef NETCDF_PARALLEL
+    int ncid = -1;
+    int err = nc_open_par(name.c_str(), NC_NOWRITE, comm, MPI_INFO_NULL, &ncid);
+    if (err != NC_NOERR) {
+        std::cerr << "error opening NetCDF file " << name << ": " << nc_strerror(err) << std::endl;
+        return NcFile(name, err);
+    }
+    return NcFile(ncid, name, true);
+#else
+    return open(name);
+#endif
+}
+
+//DIMENSION EXISTS
+//check if a given dimension exists in the NC-files
+bool hasDimension(int ncid, std::string findName)
+{
+    int dimid = -1;
+    int err = nc_inq_dimid(ncid, findName.c_str(), &dimid);
+    return err == NC_NOERR;
+}
+
+size_t getDimension(int ncid, std::string name)
+{
+    size_t dim = 0;
+    int dimid = -1;
+    int err = nc_inq_dimid(ncid, name.c_str(), &dimid);
+    if (err != NC_NOERR) {
+        return dim;
+    }
+    err = nc_inq_dimlen(ncid, dimid, &dim);
+    if (err != NC_NOERR) {
+        return dim;
+    }
+    return dim;
+}
+
+//VARIABLE EXISTS
+//check if a given variable exists in the NC-files
+bool hasVariable(int ncid, std::string findName)
+{
+    int varid = -1;
+    int err = nc_inq_varid(ncid, findName.c_str(), &varid);
+    return err == NC_NOERR;
+}
+
+template<typename S>
+struct NcFuncMapBase {
+    typedef std::function<int(int, int, S *)> get_var_func();
+    //static const get_var_func get_var;
+};
+template<typename S>
+struct NcFuncMap;
+
+#define NCFUNCS(Type, getvar, getvara) \
+    template<> \
+    struct NcFuncMap<Type> { \
+        typedef std::function<int(int, int, Type *)> get_var_func; \
+        static get_var_func get_var; \
+        typedef std::function<int(int, int, const size_t *, const size_t *, Type *)> get_vara_func; \
+        static get_vara_func get_vara; \
+    }; \
+    NcFuncMap<Type>::get_var_func NcFuncMap<Type>::get_var = getvar; \
+    NcFuncMap<Type>::get_vara_func NcFuncMap<Type>::get_vara = getvara;
+
+NCFUNCS(unsigned char, nc_get_var_uchar, nc_get_vara_uchar)
+NCFUNCS(unsigned, nc_get_var_uint, nc_get_vara_uint)
+NCFUNCS(unsigned long long, nc_get_var_ulonglong, nc_get_vara_ulonglong)
+NCFUNCS(float, nc_get_var_float, nc_get_vara_float)
+NCFUNCS(double, nc_get_var_double, nc_get_vara_double)
+
+template<typename T>
+std::vector<T> getVariable(int ncid, std::string name, std::vector<size_t> start, std::vector<size_t> count)
+{
+    std::vector<T> data;
+    int varid = -1;
+    int err = nc_inq_varid(ncid, name.c_str(), &varid);
+    if (err != NC_NOERR) {
+        std::cerr << "Nc: nc_inq_varid " << name << " error: " << nc_strerror(err) << std::endl;
+        return data;
+    }
+    int ndims = -1;
+    err = nc_inq_varndims(ncid, varid, &ndims);
+    if (err != NC_NOERR) {
+        std::cerr << "Nc: nc_inq_varndims " << name << " error: " << nc_strerror(err) << std::endl;
+        return data;
+    }
+    assert(start.size() == size_t(ndims));
+    assert(count.size() == size_t(ndims));
+    std::vector<int> dimids(ndims);
+    err = nc_inq_vardimid(ncid, varid, dimids.data());
+    if (err != NC_NOERR) {
+        std::cerr << "Nc: nc_inq_vardimd " << name << " error: " << nc_strerror(err) << std::endl;
+        return data;
+    }
+    std::vector<size_t> dims(ndims);
+    for (int i = 0; i < ndims; ++i) {
+        err = nc_inq_dimlen(ncid, dimids[i], &dims[i]);
+        if (err != NC_NOERR) {
+            std::cerr << "Nc: nc_inq_dimlen " << name << " error: " << nc_strerror(err) << std::endl;
+            return data;
+        }
+    }
+    size_t size = std::accumulate(count.begin(), count.end(), 1, [](size_t a, size_t b) { return a * b; });
+    data.resize(size);
+    size_t intmax = std::numeric_limits<int>::max();
+    size_t nreads = std::max(size_t(1), (size * sizeof(T) + intmax - 1) / intmax);
+    unsigned splitdim = 0;
+    while (splitdim < count.size() - 1 && count[splitdim] == 1)
+        ++splitdim;
+    size_t splitcount = (count[splitdim] + nreads - 1) / nreads;
+    std::vector blockcount(count);
+    blockcount[splitdim] = splitcount;
+    size_t nvalues = std::accumulate(blockcount.begin(), blockcount.end(), 1, [](size_t a, size_t b) { return a * b; });
+    for (size_t i = 0; i < nreads; ++i) {
+        std::vector<size_t> s(start), c(count);
+        s[splitdim] = start[splitdim] + i * splitcount;
+        c[splitdim] = std::min(splitcount, count[splitdim] - s[splitdim]);
+        if (s[splitdim] >= start[splitdim] + count[splitdim])
+            continue;
+        err = NcFuncMap<T>::get_vara(ncid, varid, s.data(), c.data(), data.data() + i * nvalues);
+        if (err != NC_NOERR) {
+            std::cerr << "Nc: get_vara " << name << " error: " << nc_strerror(err) << std::endl;
+            std::cerr << "i=" << i << ", start:";
+            for (auto v: s)
+                std::cerr << " " << v;
+            std::cerr << ", count:";
+            for (auto v: c)
+                std::cerr << " " << v;
+            std::cerr << std::endl;
+            data.clear();
+            return data;
+        }
+    }
+
+    return data;
+}
+
+template<typename T>
+std::vector<T> getVariable(int ncid, std::string name)
+{
+    std::vector<T> data;
+    int varid = -1;
+    int err = nc_inq_varid(ncid, name.c_str(), &varid);
+    if (err != NC_NOERR) {
+        std::cerr << "Nc: nc_inq_varid " << name << " error: " << nc_strerror(err) << std::endl;
+        return data;
+    }
+    int ndims = -1;
+    err = nc_inq_varndims(ncid, varid, &ndims);
+    if (err != NC_NOERR) {
+        std::cerr << "Nc: nc_inq_varndims " << name << " error: " << nc_strerror(err) << std::endl;
+        return data;
+    }
+    std::vector<int> dimids(ndims);
+    err = nc_inq_vardimid(ncid, varid, dimids.data());
+    if (err != NC_NOERR) {
+        std::cerr << "Nc: nc_inq_vardimd " << name << " error: " << nc_strerror(err) << std::endl;
+        return data;
+    }
+    std::vector<size_t> dims(ndims);
+    for (int i = 0; i < ndims; ++i) {
+        err = nc_inq_dimlen(ncid, dimids[i], &dims[i]);
+        if (err != NC_NOERR) {
+            std::cerr << "Nc: nc_inq_dimlen " << name << " error: " << nc_strerror(err) << std::endl;
+            return data;
+        }
+    }
+    size_t s = std::accumulate(dims.begin(), dims.end(), 1, [](size_t a, size_t b) { return a * b; });
+    data.resize(s);
+    err = NcFuncMap<T>::get_var(ncid, varid, data.data());
+    if (err != NC_NOERR) {
+        std::cerr << "Nc: get_var " << name << " error: " << nc_strerror(err) << std::endl;
+        data.clear();
+        return data;
+    }
+
+    return data;
+}
+
+template std::vector<unsigned char> getVariable(int ncid, std::string name, std::vector<size_t> start,
+                                                std::vector<size_t> count);
+template std::vector<unsigned> getVariable(int ncid, std::string name, std::vector<size_t> start,
+                                           std::vector<size_t> count);
+template std::vector<unsigned long long> getVariable(int ncid, std::string name, std::vector<size_t> start,
+                                                     std::vector<size_t> count);
+template std::vector<float> getVariable(int ncid, std::string name, std::vector<size_t> start,
+                                        std::vector<size_t> count);
+template std::vector<double> getVariable(int ncid, std::string name, std::vector<size_t> start,
+                                         std::vector<size_t> count);
+
+template std::vector<unsigned char> getVariable(int ncid, std::string name);
+template std::vector<unsigned> getVariable(int ncid, std::string name);
+template std::vector<unsigned long long> getVariable(int ncid, std::string name);
+template std::vector<float> getVariable(int ncid, std::string name);
+template std::vector<double> getVariable(int ncid, std::string name);
+
+} // namespace vistle
---
+++ lib/vistle/netcdf/ncwrap.h
@@ -0,0 +1,51 @@
+#ifndef VISTLE_NCWRAP_H
+#define VISTLE_NCWRAP_H
+
+#ifdef USE_NETCDF
+#include <netcdf.h>
+#ifdef NETCDF_PARALLEL
+#include <netcdf_par.h>
+#endif
+
+#include <boost/mpi.hpp>
+
+#include <string>
+
+namespace vistle {
+
+
+struct NcFile {
+    bool parallel = false;
+    int error = NC_NOERR;
+    bool valid = false;
+    std::string name;
+    int id = -1;
+    NcFile(int id, const std::string &name, bool parallel);
+    NcFile(const std::string &name, int err);
+    NcFile(NcFile &&o);
+    NcFile(const NcFile &o) = delete;
+    NcFile &operator=(const NcFile &o) = delete;
+    ~NcFile();
+    operator bool() const;
+    operator int() const;
+
+    static NcFile open(const std::string &name);
+    static NcFile open(const std::string &name, const MPI_Comm &comm);
+};
+
+//DIMENSION EXISTS
+//check if a given dimension exists in the NC-files
+bool hasDimension(int ncid, std::string findName);
+size_t getDimension(int ncid, std::string name);
+//VARIABLE EXISTS
+//check if a given variable exists in the NC-files
+bool hasVariable(int ncid, std::string findName);
+
+template<typename T>
+std::vector<T> getVariable(int ncid, std::string name, std::vector<size_t> start, std::vector<size_t> count);
+template<typename T>
+std::vector<T> getVariable(int ncid, std::string name);
+
+} // namespace vistle
+#endif
+#endif
