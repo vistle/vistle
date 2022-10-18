bde53543ed3591071e85b832e1e896ca84a834b1
Marko Djuric <marko.djuric@gmx.de>
2022-10-18 16:47:56 +0200

    typo

--- cmake/Documentation.cmake
+++ cmake/Documentation.cmake
@@ -1,11 +1,8 @@
 macro(add_module_doc_target targetname)
 
     set(VISTLE_DOCUMENTATION_WORKFLOW ${PROJECT_SOURCE_DIR}/doc/generateModuleInfo.vsl)
-    set(DOC_COMMAND ${CMAKE_COMMAND} -E env 
-        VISTLE_DOCUMENTATION_TARGET=${targetname}
-        VISTLE_DOCUMENTATION_DIR=${CMAKE_CURRENT_SOURCE_DIR}
-        VISTLE_DOCLUMENTATION_BIN_DIR=${CMAKE_CURRENT_BINARY_DIR}
-            vistle --batch ${VISTLE_DOCUMENTATION_WORKFLOW})
+    set(DOC_COMMAND ${CMAKE_COMMAND} -E env VISTLE_DOCUMENTATION_TARGET=${targetname} VISTLE_DOCUMENTATION_DIR=${CMAKE_CURRENT_SOURCE_DIR}
+                    VISTLE_DOCUMENTATION_BIN_DIR=${CMAKE_CURRENT_BINARY_DIR} vistle --batch ${VISTLE_DOCUMENTATION_WORKFLOW})
 
     set(OUTPUT_FILE ${PROJECT_SOURCE_DIR}/doc/GenModInfo/moduleDescriptions/${targetname}.md)
     set(INPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/${targetname}.md)
@@ -21,19 +18,20 @@ macro(add_module_doc_target targetname)
                 ${targetname} #the module's source code
                 ${VISTLE_DOCUMENTATION_WORKFLOW} #the file that gets loaded by vistle to generate the documentation
                 ${PROJECT_SOURCE_DIR}/doc/GenModInfo/genModInfo.py #dependency of VISTLE_DOCUMENTATION_WORKFLOW
-                ${DOCUMENTATION_DEPENDENCIES} #custom dependencies set by the calling module 
+                ${DOCUMENTATION_DEPENDENCIES} #custom dependencies set by the calling module
         COMMENT "Generating documentation for " ${targetname})
-    add_custom_target(
-        ${targetname}_doc
-        DEPENDS ${OUTPUT_FILE})
-    if (VISTLE_DOC_SKIP)
+    add_custom_target(${targetname}_doc DEPENDS ${OUTPUT_FILE})
+    if(VISTLE_DOC_SKIP)
         message("Skipping ${targetname}_doc: ${VISTLE_DOC_SKIP}")
         add_dependencies(vistle_doc_skip ${targetname}_doc)
     else()
         add_dependencies(vistle_doc ${targetname}_doc)
     endif()
 
-    file(GLOB WORKFLOWS LIST_DIRECTORIES FALSE ${CMAKE_CURRENT_SOURCE_DIR}/*.vsl)
+    file(
+        GLOB WORKFLOWS
+        LIST_DIRECTORIES FALSE
+        ${CMAKE_CURRENT_SOURCE_DIR}/*.vsl)
     foreach(file ${WORKFLOWS})
         get_filename_component(workflow ${file} NAME_WLE)
         message("Workflow: ${targetname} ${workflow} ${file}")
@@ -43,36 +41,40 @@ macro(add_module_doc_target targetname)
 endmacro()
 
 macro(generate_network_snapshot targetname network_file)
-    add_custom_command( #create a snapshot of the pipeline
-        OUTPUT  ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png
+    add_custom_command(
+        #create a snapshot of the pipeline
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png
         COMMAND vistle --snapshot ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vsl
-        DEPENDS  ${CMAKE_CURRENT_LIST_DIR}/${VISTLE_DOCUMENTATION_WORKFLOW}.vsl targetname
+        DEPENDS ${CMAKE_CURRENT_LIST_DIR}/${VISTLE_DOCUMENTATION_WORKFLOW}.vsl targetname
         COMMENT "Generating network snapshot for " ${network_file}.vsl)
 
-    add_custom_target(
-        ${targetname}_${network_file}_workflow
-        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png)
+    add_custom_target(${targetname}_${network_file}_workflow DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png)
     add_dependencies(${targetname}_doc ${targetname}_${network_file}_workflow)
 endmacro()
 
 macro(generate_snapshots targetname network_file)
-    if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vwp) #if we have a viewpoint file we can generate an result image, only first viewpoint is considered, only first cover is considered
+    if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vwp
+    )#if we have a viewpoint file we can generate an result image, only first viewpoint is considered, only first cover is considered
         add_custom_command(
-            OUTPUT  ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_result.png ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png
-            COMMAND ${CMAKE_COMMAND} -E env 
-                COCONFIG=${PROJECT_SOURCE_DIR}/doc/config.vistle.doc.xml 
-                VISTLE_DOC_IMAGE_NAME=${network_file}
-                VISTLE_DOC_SOURCE_DIR=${CMAKE_CURRENT_LIST_DIR}
-                VISTLE_DOC_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR}
-                     vistle ${PROJECT_SOURCE_DIR}/doc/resultSnapShot.py
+            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_result.png ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png
+            COMMAND
+                ${CMAKE_COMMAND} -E env COCONFIG=${PROJECT_SOURCE_DIR}/doc/config.vistle.doc.xml VISTLE_DOC_IMAGE_NAME=${network_file}
+                VISTLE_DOC_SOURCE_DIR=${CMAKE_CURRENT_LIST_DIR} VISTLE_DOC_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR} vistle
+                ${PROJECT_SOURCE_DIR}/doc/resultSnapShot.py
             DEPENDS ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vsl ${CMAKE_CURRENT_LIST_DIR}/${network_file}.vwp ${targetname}
-            ${PROJECT_SOURCE_DIR}/doc/resultSnapShot.py
+                    ${PROJECT_SOURCE_DIR}/doc/resultSnapShot.py
             COMMENT "Generating network and result snapshot for " ${network_file}.vsl)
-        add_custom_target(
-            ${targetname}_${network_file}_result
-            DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_result.png ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png)
+        add_custom_target(${targetname}_${network_file}_result DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_result.png
+                                                                       ${CMAKE_CURRENT_BINARY_DIR}/${network_file}_workflow.png)
         add_dependencies(${targetname}_doc ${targetname}_${network_file}_result)
     else()
-        message(WARNING "can not generate snapshots for " ${targetname} " " ${network_file} ": missing viewpoint file, make sure a viewpoint file named \"" ${network_file} ".vwp\" exists!")
+        message(
+            WARNING "can not generate snapshots for "
+                    ${targetname}
+                    " "
+                    ${network_file}
+                    ": missing viewpoint file, make sure a viewpoint file named \""
+                    ${network_file}
+                    ".vwp\" exists!")
     endif()
 endmacro()
