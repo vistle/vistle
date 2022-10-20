a4ac504b24eb892c5f09f5794dc767bb42f4d7dd
Martin Aumüller <aumuell@reserv.at>
2022-10-20 20:12:16 +0200

    more versatile stopwatch

--- lib/vistle/util/stopwatch.cpp
+++ lib/vistle/util/stopwatch.cpp
@@ -13,10 +13,30 @@ double Clock::time()
 
 StopWatch::StopWatch(const char *description)
 : m_description(description ? description : "")
-, m_start(clock_type::now())
-, m_timer(std::make_unique<boost::timer::auto_cpu_timer>(std::cerr, 3,
-                                                         m_description + ": %ws wall, %us user + %ss sys (%p%)\n"))
+, m_format(m_description + ": %ws wall, %us user + %ss sys (%p%)\n")
+, m_timer(std::make_unique<boost::timer::auto_cpu_timer>(std::cerr, 3, m_format))
 {}
+
+void StopWatch::stop()
+{
+    m_timer->stop();
+}
+
+void StopWatch::resume()
+{
+    m_timer->resume();
+}
+
+void StopWatch::report()
+{
+    m_timer->report();
+}
+
+void StopWatch::reset()
+{
+    m_timer = std::make_unique<boost::timer::auto_cpu_timer>(std::cerr, 3, m_format);
+}
+
 
 StopWatch::~StopWatch()
 {}
--- lib/vistle/util/stopwatch.h
+++ lib/vistle/util/stopwatch.h
@@ -31,10 +31,14 @@ public:
 
     StopWatch(const char *description);
     ~StopWatch();
+    void reset();
+    void stop();
+    void resume();
+    void report();
 
 private:
     std::string m_description;
-    clock_type::time_point m_start;
+    std::string m_format;
     std::unique_ptr<boost::timer::auto_cpu_timer> m_timer;
 };
 
