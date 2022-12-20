ecf3e8c566d9e91a84fd5d5090c5e41af64b68e6
Martin Aumüller <aumuell@reserv.at>
2022-12-21 00:33:20 +0100

    access to covconfig library from Vistle
    covconfig in 3rdparty, wrapped in vistle_config

---
+++ lib/vistle/config/access.h
@@ -0,0 +1,8 @@
+#ifndef VISTLE_CONFIG_ACCESS_H
+#define VISTLE_CONFIG_ACCESS_H
+#define CONFIG_NAMESPACE vistle
+
+#include <covconfig/access.h>
+
+#undef CONFIG_NAMESPACE
+#endif
---
+++ lib/vistle/config/array.h
@@ -0,0 +1,8 @@
+#ifndef VISTLE_CONFIG_ARRAY_H
+#define VISTLE_CONFIG_ARRAY_H
+#define CONFIG_NAMESPACE vistle
+
+#include <covconfig/array.h>
+
+#undef CONFIG_NAMESPACE
+#endif
