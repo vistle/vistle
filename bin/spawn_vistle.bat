33e4a7159ce899ea385cec601ef7ef693440325d
hpcdgrie <dennis.grieger@hlrs.de>
2023-01-26 15:52:29 +0100

    Spawn windows processes minimized

--- bin/spawn_vistle.bat
+++ bin/spawn_vistle.bat
@@ -4,5 +4,5 @@ echo MSMPI_BIN=%MSMPI_BIN%
 
 if not defined MPISIZE set MPISIZE=1
 
-start /WAIT "mpie" "%MSMPI_BIN%\mpiexec" -n %MPISIZE% %1 %2 %3 %4 %5 %6 %7 %8 %9
+start /min /WAIT "mpie" "%MSMPI_BIN%\mpiexec" -n %MPISIZE% %1 %2 %3 %4 %5 %6 %7 %8 %9
 REM start /WAIT "mpi" 
