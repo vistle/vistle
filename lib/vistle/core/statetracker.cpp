8cedab4509c98aa7283651cf491e9049a39b8d08
Martin Aumüller <aumuell@reserv.at>
2023-02-21 10:55:54 +0100

    don't crash vistle session when a module crashes during start up

--- lib/vistle/core/statetracker.cpp
+++ lib/vistle/core/statetracker.cpp
@@ -994,9 +994,20 @@ bool StateTracker::handlePriv(const message::Started &started)
     int moduleId = started.senderId();
     auto it = runningMap.find(moduleId);
     if (it == runningMap.end()) {
+        if (quitMap.find(moduleId) != quitMap.end()) {
+            CERR << "module " << moduleId << " crashed during startup?" << std::endl;
+            return true;
+        }
         CERR << "did not find " << moduleId << " in runningMap, contents are: ";
         for (auto it = runningMap.begin(); it != runningMap.end(); ++it) {
             if (it != runningMap.begin())
+                std::cerr << ", ";
+            std::cerr << it->first;
+        }
+        std::cerr << std::endl;
+        CERR << "did not find " << moduleId << " in quitMap, contents are: ";
+        for (auto it = quitMap.begin(); it != quitMap.end(); ++it) {
+            if (it != quitMap.begin())
                 std::cerr << ", ";
             std::cerr << it->first;
         }
