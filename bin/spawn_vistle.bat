358f628b02c023c8b836f856322c86997ab41174
Uwe Woessner <woessner@hlrs.de>
2023-03-22 14:40:21 +0100

    unique log file name

--- bin/spawn_vistle.bat
+++ bin/spawn_vistle.bat
@@ -3,7 +3,7 @@ echo spawn_vistle.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
 for %%F in ("%1") do set FILENAME=%%~nF
 if not defined MPISIZE set MPISIZE=1
 if not exist "%TEMP%\vistle" mkdir  "%TEMP%\vistle"
-if not "%5"=="" set FILENAME=%FILENAME%-%5.log
+if not "%5"=="" set FILENAME=%FILENAME%-%4-%5.log
 @REM start /min /WAIT "mpie" "%MSMPI_BIN%\mpiexec" -n %MPISIZE% %1 %2 %3 %4 %5 %6 %7 %8 %9
 "%MSMPI_BIN%\mpiexec" -n %MPISIZE% %1 %2 %3 %4 %5 %6 %7 %8 %9 1> %TEMP%\vistle\%FILENAME% 2>&1
 REM start /WAIT "mpi" 
