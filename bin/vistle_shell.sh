f5cd3ebb8cb2944ac35ce174878336758bc8730f
Martin Aumüller <aumuell@reserv.at>
2023-06-27 16:19:40 +0200

    handle more DYLD_* environment variables

--- bin/vistle_shell.sh
+++ bin/vistle_shell.sh
@@ -11,7 +11,10 @@ if [ -n "$2" ]; then
     port="$2"
 fi
 
-if [ -n "$VISTLE_DYLD_LIBRARY_PATH" ]; then
-    export DYLD_LIBRARY_PATH="$VISTLE_DYLD_LIBRARY_PATH"
-fi
+for v in DYLD_LIBRARY_PATH DYLD_FRAMEWORK_PATH DYLD_FALLBACK_LIBRARY_PATH DYLD_FALLBACK_FRAMEWORK_PATH DYLD_INSERT_LIBRARIES; do
+    eval val="\${VISTLE_${v}}"
+    if [ -n "${val}" ]; then
+        export $v="$val"
+    fi
+done
 exec ipython -i -c "import vistle; vistle._vistle.sessionConnect(None, \"$host\", $port); from vistle import *;" -- "$@"
