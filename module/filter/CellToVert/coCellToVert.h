cf31b7a091e22929eae5609cc280e0529802bfcb
Martin Aumüller <aumuell@reserv.at>
2023-10-14 01:16:13 +0200

    support for structured grids

--- module/filter/CellToVert/coCellToVert.h
+++ module/filter/CellToVert/coCellToVert.h
@@ -23,22 +23,6 @@ class coCellToVert {
 private:
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     //
-    // Original algorithm by Andreas Werner: + Assume data value related to center of elements.
-    //                                       + Calculate the average values weighted by the distance to the
-    //                                           surrounding center of elements.
-    //
-    //        - only implemented for unstructured grids
-    //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    template<typename S>
-    static bool weightedAlgo(Index num_elem, Index num_conn, Index num_point, const Index *elem_list,
-                             const Index *conn_list, const Byte *type_list, const Index *neighbour_cells,
-                             const Index *neighbour_idx, const Scalar *xcoord, const Scalar *ycoord,
-                             const Scalar *zcoord, Index numComp, const S *in_data[], S *out_data[]);
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    //
     //   Take the average value of all elements which the point includes
     //
     //   implemtented for POLYGN, LINES, UNSGRD
@@ -51,10 +35,8 @@ private:
                            S *out_data[]);
 
 public:
-    typedef enum { SQR_WEIGHT = 1, SIMPLE = 2 } Algorithm;
-
     // note: no attributes copied
-    DataBase::ptr interpolate(Object::const_ptr geo_in, DataBase::const_ptr data_in, Algorithm algo_option = SIMPLE);
+    DataBase::ptr interpolate(Object::const_ptr geo_in, DataBase::const_ptr data_in);
 
     //
     //  returns false in case of an error
@@ -62,8 +44,7 @@ public:
     template<typename S>
     static bool interpolate(bool unstructured, Index num_elem, Index num_conn, Index num_point, const Index *elem_list,
                             const Index *conn_list, const Byte *type_list, const Index *neighbour_cells,
-                            const Index *neighbour_idx, const Scalar *xcoord, const Scalar *ycoord,
-                            const Scalar *zcoord, Index numComp, Index dataSize, const S *in_data[], S *out_data[],
-                            Algorithm algo_option = SIMPLE);
+                            const Index *neighbour_idx, Index numComp, Index dataSize, const S *in_data[],
+                            S *out_data[]);
 };
 #endif
