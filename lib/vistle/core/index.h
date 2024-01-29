1413a7fd9d75cb45c3b4a138529fc19652cd8114
Martin Aumüller <aumuell@reserv.at>
2024-01-29 20:35:10 +0100

    ensure that static_cast<Integer>(InvalidIndex) is positive

--- lib/vistle/core/index.h
+++ lib/vistle/core/index.h
@@ -3,18 +3,21 @@
 
 #include <cstdlib>
 #include <cinttypes>
+#include <limits>
 
 namespace vistle {
 
 #ifdef VISTLE_INDEX_64BIT
 typedef uint64_t Index;
 typedef int64_t SIndex;
+const Index InvalidIndex =
+    std::numeric_limits<SIndex>::max(); // in order to keep static_cast<Integer>(InvalidIndex) positive
 #else
 typedef uint32_t Index;
 typedef int32_t SIndex;
+const Index InvalidIndex = ~(Index)(0);
 #endif
 
-const Index InvalidIndex = ~(Index)(0);
 
 } // namespace vistle
 
