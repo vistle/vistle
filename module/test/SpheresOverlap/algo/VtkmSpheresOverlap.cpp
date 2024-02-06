550e4fd4787ec90a2e17e6c2e69fdf914b39f2f3
DeVasconcelos <malheiros.susanne@gmail.com>
2024-02-06 18:11:16 +0100

    use vtkm's PointLocatorSearchGrid to create uniform search grid

--- module/test/SpheresOverlap/algo/VtkmSpheresOverlap.cpp
+++ module/test/SpheresOverlap/algo/VtkmSpheresOverlap.cpp
@@ -1,42 +1,90 @@
 /*
-    1.) convert vistle to vtk-m : Spheres to CellSet (of any type) where coordinates
-                                  are sphere coordinates and data field is radius
-    2.) create search grid: uniform grid, cell size = search radius, bounds =
-                            sphere bounds (probably best to make it search grid from 1.))
-    3.) create cell list: assign each sphere to cell containing it 
-    4.) use Point Neighborhood Worklet for checking overlap between 
-        spheres in this cell & neighbor cells
-    5.) convert result (lines) back to vistle structure
+    1.) create uniform search grid with cont::PointLocatorSearchGrid which gives us
+        --> PointIds: a list which stores all point ids sorted (by key) by the ids of 
+            the cells that contain them
+        --> LowerBounds: entry i stores the first index of the points in PointsId which
+                         lie in cell i
+        --> UpperBounds: entry i stores the last index of the points in PointsId which
+                         lie in cell i
+            
+    2.) write function which gets the indices of all neighboring cells 
+        (maybe look at FindInBox?)
 
-    For 1.) and 2.): ParticleDensityBase.h seems to be doing that
+    3.) write function for finding overlap similar to exec::PointLocatorSearchGrid::FindCell 
 
+    4.) think about data structure for saving Lines object (has to be convertible to vistle
+        to work with OpenCOVER and rest of modules!)
 */
 
 #include <vistle/core/spheres.h>
 #include <vistle/core/uniformgrid.h>
 
-#include <vtkm/cont/DataSetBuilderUniform.h>
 #include <vtkm/VectorAnalysis.h>
+
+#include <vtkm/cont/ArrayHandle.h>
+#include <vtkm/cont/ArrayHandleGroupVec.h>
+#include <vtkm/cont/CellLocatorUniformGrid.h>
+#include <vtkm/cont/CellSetSingleType.h>
+#include <vtkm/cont/DataSetBuilderUniform.h>
+#include <vtkm/cont/PointLocatorSparseGrid.h>
+
+#include <vtkm/exec/CellEdge.h>
+
+#include <vtkm/worklet/ScatterCounting.h>
+#include <vtkm/worklet/ScatterUniform.h>
 #include <vtkm/worklet/WorkletCellNeighborhood.h>
+#include <vtkm/worklet/WorkletMapField.h>
+#include <vtkm/worklet/WorkletMapTopology.h>
 
 #include "VtkmSpheresOverlap.h"
 
 using namespace vistle;
 
+// return the two indices needed to create a line
+
+// extracts all edges from a cell set
+class EdgeIndicesWorklet: public vtkm::worklet::WorkletVisitCellsWithPoints {
+public:
+    using ControlSignature = void(CellSetIn cellSet, FieldOut connectivity);
+    using ExecutionSignature = void(CellShape, PointIndices, _2, VisitIndex);
+
+    using ScatterType = vtkm::worklet::ScatterCounting;
+
+    template<typename CellShapeTag, typename PointIndexVecType>
+    VTKM_EXEC void operator()(CellShapeTag cellShape, const PointIndexVecType &globalPointIndicesForCell,
+                              vtkm::Id2 &connectivity, vtkm::IdComponent edgeIndex) const
+    {
+        vtkm::IdComponent numPointsInCell = globalPointIndicesForCell.GetNumberOfComponents();
+        vtkm::IdComponent pointInCellIndex0;
+        vtkm::exec::CellEdgeLocalIndex(numPointsInCell, 0, edgeIndex, cellShape, pointInCellIndex0);
+        vtkm::IdComponent pointInCellIndex1;
+        vtkm::exec::CellEdgeLocalIndex(numPointsInCell, 1, edgeIndex, cellShape, pointInCellIndex1);
+
+        connectivity[0] = globalPointIndicesForCell[pointInCellIndex0];
+        connectivity[1] = globalPointIndicesForCell[pointInCellIndex1];
+    }
+};
+
 class DetectOverlap: public vtkm::worklet::WorkletCellNeighborhood {
 public:
-    using ControlSignature = void(CellSetIn);
+    using ControlSignature = void(CellSetIn, FieldInNeighborhood coords, FieldInNeighborhood radii, ExecObject locator,
+                                  AtomicArrayInOut thicknesses);
 
-    using ExecutionSignature = void(_1);
+    using ExecutionSignature = void(Boundary, _2, _3, _4, _5);
 
-    template<typename Boundary>
-    VTKM_EXEC void operator()(Boundary boundary) const
+    template<typename Boundary, typename NeighborPoint, typename NeighborRadius, typename CellLocatorExecObj,
+             typename AtomicArray>
+    VTKM_EXEC void operator()(Boundary boundary, const NeighborPoint &neighborPoint,
+                              const NeighborRadius neighborRadius, const CellLocatorExecObj &locator,
+                              AtomicArray &thicknesses) const
     {
-        //auto thisPosition = neighborPosition.Get(0, 0, 0);
-        //auto thisRadius = neighborRadius.Get(0, 0, 0);
+        auto currentPosition = neighborPosition.Get(0, 0, 0);
+        auto currentRadius = neighborRadius.Get(0, 0, 0);
 
         auto minIndices = boundary.MinNeighborIndices(1);
         auto maxIndices = boundary.MaxNeighborIndices(1);
+
+        vtkm::Id cellId{};
 
         // go through all neighbor cells
         for (int k = minIndices[2]; k <= maxIndices[2]; k++) {
@@ -52,30 +100,45 @@ public:
     }
 };
 
-vtkm::cont::DataSet CreateSearchGrid(Spheres::const_ptr spheres, Scalar searchRadius)
+
+// This should return Lines dataset with certain thickness
+VTKM_CONT vtkm::cont::DataSet VtkmSpheresOverlap::DoExecute(const vtkm::cont::DataSet &inputSpheres)
 {
-    // calculate grid size using bounds of spheres
-    auto bounds = spheres->getBounds();
-    auto minBounds = bounds.first;
-    auto maxBounds = bounds.second;
+    auto coords = inputSpheres.GetCoordinateSystem();
+    auto radii = inputSpheres.GetPointField("radius");
 
-    Index dim = maxBounds.size();
-    // we expect getBounds() to return a pair of Vector3s
-    assert(dim == 3);
+    vtkm::cont::ArrayHandle<Scalar> thickness; // output
 
-    Index gridSize[dim];
+    // determine number of lines and connectivity array
+    /*auto nrOverlaps = 0;
+    vtkm::worklet::ScatterUniform<2> scatter;
 
-    // divide bounding box into cubes with side length = searchRadius
-    for (Index i = 0; i < dim; i++) {
-        gridSize[i] = ceil(maxBounds[i] / searchRadius);
+    auto inputCells = inputSpheres.GetCellSet();
+    vtkm::cont::ArrayHandle<vtkm::Id> linesConnectivity;
 
-        // if gridSize = 1, vistle can't create cells (since #cells = gridSize - 1),
-        // To make algorithm work anyways, add second cell that does not contain any spheres
-        gridSize[i] = gridSize[i] == 1 ? 2 : gridSize[i];
-    }
+    // need to use ArrayHandleGroupVec as CellSetSingleType expects a flat array
+    this->Invoke(EdgeIndicesWorklet{}, scatter, inputCells, vtkm::cont::make_ArrayHandleGroupVec<2>(linesConnectivity));
 
-    vtkm::cont::DataSetBuilderUniform datasetBuilder;
-    return datasetBuilder.Create(vtkm::Id3(gridSize[0], gridSize[1], gridSize[2]),
-                                 vtkm::Vec3f(minBounds[0], minBounds[1], minBounds[2]),
-                                 vtkm::Vec3f(searchRadius, searchRadius, searchRadius));
+    vtkm::cont::CellSetSingleType<> overlapLines;
+    overlapLines.Fill(nrOverlaps, vtkm::CELL_SHAPE_LINE, 2, linesConnectivity);
+    */
+
+    // create search grid
+    vtkm::cont::PointLocatorSparseGrid pointLocator;
+    pointLocator.SetCoordinates(inputSpheres.GetCoordinateSystem());
+
+    // make search radius at least twice as large as maximum sphere radius
+    auto searchRadius = 2.1 * radii.GetRange().ReadPortal().Get(0).Max;
+    auto bounds = coords.GetBounds();
+    vtkm::Id3 binSizes{vtkm::Ceil((bounds.X.Max - bounds.X.Min) / searchRadius),
+                       vtkm::Ceil((bounds.Y.Max - bounds.Y.Min) / searchRadius),
+                       vtkm::Ceil((bounds.Z.Max - bounds.Z.Min) / searchRadius)};
+    pointLocator.SetNumberOfBins(binSizes);
+
+    pointLocator.SetRange({vtkm::Range(bounds.X.Min, bounds.X.Min + binSizes[0] * searchRadius),
+                           vtkm::Range(bounds.Y.Min, bounds.Y.Min + binSizes[1] * searchRadius),
+                           vtkm::Range(bounds.Z.Min, bounds.Z.Min + binSizes[2] * searchRadius)});
+
+    // build search grid in parallel
+    pointLocator.Update();
 }
