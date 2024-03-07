fbdd7ba065fc3e8aa08da17ebe5957f814442013
MDjur <marko.djuric@gmx.de>
2024-03-07 19:01:52 +0100

    fix pre-commit

--- module/read/ReadDuisburg/ReadDuisburg.h
+++ module/read/ReadDuisburg/ReadDuisburg.h
@@ -24,16 +24,15 @@ private:
     bool read(Token &token, int timestep = -1, int block = -1) override;
     bool examine(const vistle::Parameter *param) override;
     bool finishRead() override;
-   
-    bool cellIsWater(const std::vector<double> &h, int i, int j,int dimX, int dimY) const;
-    bool getDimensions(const PnetCDF::NcmpiFile &ncFile, int &dimX, int &dimY)const ;
+
+    bool cellIsWater(const std::vector<double> &h, int i, int j, int dimX, int dimY) const;
+    bool getDimensions(const PnetCDF::NcmpiFile &ncFile, int &dimX, int &dimY) const;
 
     Object::ptr generateTriangleGrid(const PnetCDF::NcmpiFile &ncFile, int timestep, int block) const;
     // Object::ptr generateLayerGrid(const PnetCDF::NcmpiFile &ncFile, int timestep, int block) const;
 
     Port *m_gridOut = nullptr;
     StringParameter *m_gridFile;
-
 };
 
 #endif
