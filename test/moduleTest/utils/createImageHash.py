3b5408c7145719f792d5c51d41ff340003e9988d
MDjur <marko.djuric@gmx.de>
2024-05-16 13:17:51 +0200

    refactoring createImageHash

--- test/moduleTest/utils/createImageHash.py
+++ test/moduleTest/utils/createImageHash.py
@@ -1,69 +1,78 @@
+# needs python 3.8
+from typing import TypedDict
 from PIL import Image
 import imagehash
 import argparse
 import json
 
+
+class Option(TypedDict):
+    name: str
+    metavar: str
+    help: str
+    nargs: str | int
+    action: argparse.Action
+    default: str = None
+
+
+Options = [
+    Option(name="-name", metavar="name", help="Vsl network filename.", nargs=1),
+    Option(name="-sdir", metavar="srcDir", help="Image path.", nargs=1),
+    Option(name="-jdir", metavar="jsondir", help="Path to json.", nargs=1),
+    Option(
+        name="-cmp",
+        metavar="compare",
+        default=False,
+        help="Compare with hash in json file <vistle_root>/test/moduleTest/utils/refImageHash.json.",
+        action=argparse.BooleanOptionalAction,
+    ),
+    Option(
+        name="-refHashString",
+        metavar="referenceHashString",
+        help="Reference hash as string literal to compare with new created imagehashes. Add -cmp additionally",
+        nargs="?",
+    ),
+]
+
+
 def createImageHash(imagePath):
+    print("Creating imagehash for: " + imagePath)
     hash = imagehash.average_hash(Image.open(imagePath))
+    print(hash)
     return hash
 
-def compareHash(hash1, hash2):
+def compareHash(hash1, hash2) -> bool:
     return hash1 == hash2
 
-def addJson(name:str, jsonDir:str, hash):
+def addModuleImagehashToJson(pathToModuleRoot: str, jsonDir: str, hash) -> None:
     with open(jsonDir, "r+") as file:
         data = json.load(file)
 
-    data[name] = str(hash)
+    data[pathToModuleRoot] = str(hash)
 
     with open(jsonDir, "w") as file:
         file.write(json.dumps(data, indent=4))
-        
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Create imagehash."
-    )
-    parser.add_argument(
-        "-name",
-        nargs=1,
-        metavar=("name"),
-        default="module",
-        help="Vsl name.",
-    )
-    parser.add_argument(
-        "-sdir",
-        nargs=1,
-        metavar=("srcDir"),
-        default="",
-        help="Image path.",
-    )
-    parser.add_argument(
-        "-jdir",
-        nargs=1,
-        metavar=("jdir"),
-        default=None,
-        help="Path to json.",
-    )
-    parser.add_argument(
-        "-cmp",
-        action=argparse.BooleanOptionalAction,
-        metavar=("cmp"),
-        default=False,
-        help="if true compares with hash in json",
-    )
 
-    parser.add_argument(
-        "-refHashString",
-        nargs=1,
-        metavar=("refHashString"),
-        default=None,
-        help="hash to compare",
-    )
-    args = parser.parse_args()
+
+def initParser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Create imagehash.")
+    for option in Options:
+        name = option.pop("name")
+        parser.add_argument(name, **option)
+    return parser
+
+
+def getArgs():
+    parser = initParser()
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = getArgs()
 
     if args.name != None:
         name = args.name[0]
-    
+
     if args.sdir != None:
         srcDir = args.sdir[0]
 
@@ -71,13 +80,21 @@ if __name__ == "__main__":
         cmp = args.cmp
 
     if args.refHashString != None:
-        refHashString = args.refHashString[0]
+        refHashString = args.refHashString
+        cmp = True
 
     hash = createImageHash(srcDir)
     if args.jdir != None:
         jdir = args.jdir[0]
-        addJson(name, jdir, hash)
+        addModuleImagehashToJson(name, jdir, hash)
 
     if cmp:
         if not compareHash(hash, imagehash.hex_to_hash(refHashString)):
-            raise Exception("Hashes are not the same for " + name + ": " + str(hash) + " != " + refHashString)
+            raise Exception(
+                "Hashes are not the same for "
+                + name
+                + ": "
+                + str(hash)
+                + " != "
+                + refHashString
+            )
