420973db7374bf12b5b75781b2500aa3102e25c1
DeVasconcelos <malheiros.susanne@gmail.com>
2024-05-23 12:49:31 +0200

    move worklet into its own header + src file

--- lib/vistle/vtkm/convert.cpp
+++ lib/vistle/vtkm/convert.cpp
@@ -25,7 +25,7 @@
 #include <vistle/core/shm_array_impl.h>
 
 #include "convert.h"
-
+#include "convert_worklets.h"
 
 namespace vistle {
 
@@ -275,51 +275,6 @@ VtkmTransformStatus vtkmAddField(vtkm::cont::DataSet &vtkmDataSet, const vistle:
         return VtkmTransformStatus::SUCCESS;
 
     return VtkmTransformStatus::UNSUPPORTED_FIELD_TYPE;
-}
-
-struct ShapeToDimensionWorklet : vtkm::worklet::WorkletMapField
-{
-    using ControlSignature = void(FieldIn shapes, FieldOut dimensions);
-    using ExecutionSignature = void(_1, _2);
-    using InputDomain = _1;
-
-    template <typename InputType, typename OutputType>
-    VTKM_EXEC void operator()(const InputType &shape, OutputType &dim) const
-    {
-        dim = shape;
-        switch (shape)
-        {
-        case vtkm::CELL_SHAPE_VERTEX:
-            dim = 0;
-            break;
-        case vtkm::CELL_SHAPE_LINE:
-        case vtkm::CELL_SHAPE_POLY_LINE:
-            dim = 1;
-            break;
-        case vtkm::CELL_SHAPE_TRIANGLE:
-        case vtkm::CELL_SHAPE_QUAD:
-        case vtkm::CELL_SHAPE_POLYGON:
-            dim = 2;
-            break;
-        case vtkm::CELL_SHAPE_TETRA:
-        case vtkm::CELL_SHAPE_HEXAHEDRON:
-        case vtkm::CELL_SHAPE_WEDGE:
-        case vtkm::CELL_SHAPE_PYRAMID:
-            dim = 3;
-            break;
-        default:
-            dim = -1;
-            break;
-        }
-    }
-};
-
-std::tuple<int, int> getMinMaxDims(vtkm::cont::ArrayHandle<vtkm::UInt8>& shapes)
-{
-    vtkm::cont::Invoker invoke;
-    vtkm::cont::ArrayHandle<vtkm::Int8> dims;
-    invoke(ShapeToDimensionWorklet{}, shapes, dims);
-    return {vtkm::cont::Algorithm::Reduce(dims, 10, vtkm::Minimum()), vtkm::cont::Algorithm::Reduce(dims, -1, vtkm::Maximum())};
 }
 
 Object::ptr vtkmGetGeometry(vtkm::cont::DataSet &dataset)
