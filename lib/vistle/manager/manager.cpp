ea8159260c98c594e05d41ffaa409b74f5694d43
Martin Aumüller <aumuell@reserv.at>
2024-05-24 15:40:07 +0200

    don't confuse boost::program_options with duplicated options

--- lib/vistle/manager/manager.cpp
+++ lib/vistle/manager/manager.cpp
@@ -128,8 +128,11 @@ bool VistleManager::run(int argc, char *argv[])
     po::options_description desc("usage");
     // clang-format off
     desc.add_options()
+    #if 0
+        // this are added by hub as well
         ("help,h", "show this message")
         ("version,v", "print version")
+    #endif
         ("from-vistle", "invoked by Vistle hub")
         ;
     // clang-format on
