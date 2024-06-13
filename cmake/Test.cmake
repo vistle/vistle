6e40a24a01a6b31c725f8d28cbdc009d1c7a0b90
DeVasconcelos <malheiros.susanne@gmail.com>
2024-06-13 15:18:07 +0200

    remove obsolete CREATE_IMAGE_HASH variable

--- cmake/Test.cmake
+++ cmake/Test.cmake
@@ -1,19 +1,18 @@
 macro(add_image_hash targetname)
     add_custom_target(${targetname}_hash)
 
-    if(MODULE_CAN_CREATE_HASH)
-        add_dependencies(vistle_ref_hash ${targetname}_hash)
-    else()
-        message("Skipping ${targetname}_hash")
-    endif()
+    add_dependencies(vistle_ref_hash ${targetname}_hash)
 
+    # loop over all .vsl files inside the folder of the current module
     file(
         GLOB WORKFLOWS
         LIST_DIRECTORIES FALSE
         ${CMAKE_CURRENT_SOURCE_DIR}/*.vsl)
+        
     foreach(file ${WORKFLOWS})
+        # remove directory and extension from .vsl file
         get_filename_component(workflow ${file} NAME_WLE)
-        # message("Workflow: ${targetname} ${workflow} ${file}")
+        
         generate_cover_snapshot(${targetname} ${workflow} ${CMAKE_CURRENT_SOURCE_DIR})
         create_image_hash(${targetname} ${workflow})
         # TODO: uncomment later
