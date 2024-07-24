df59367b67d79af8b4c840bf8fc0bae2a8abd898
DeVasconcelos <malheiros.susanne@gmail.com>
2024-07-24 13:14:41 +0200

    for now simply setting gridSize dimension to 3

--- module/geometry/SpheresOverlap/algo/CellListsAlgorithm.cpp
+++ module/geometry/SpheresOverlap/algo/CellListsAlgorithm.cpp
@@ -13,7 +13,10 @@ UniformGrid::ptr CreateSearchGrid(Points::const_ptr spheres, Scalar searchRadius
     // we expect getBounds() to return a pair of Vector3s
     assert(dim == 3);
 
-    Index gridSize[dim];
+    // FIXME: Check if this is supported by all compilers once eigen v3.5 is released
+    //        (currently it's not supported by MSVC-) 
+    //Index gridSize[dim];
+    Index gridSize[3];
 
     // divide bounding box into cubes with side length = searchRadius
     for (Index i = 0; i < dim; i++) {
