15c784fb22b4dc5f1959ac9eb284534329a2067e
hpcdgrie <dennis.grieger@hlrs.de>
2024-08-27 14:23:39 +0200

    Removed special treatment for SENSEI

--- lib/vistle/vtk/export.h
+++ lib/vistle/vtk/export.h
@@ -2,18 +2,11 @@
 #define VISTLE_VTK_TO_VISTLE_EXPORT_H
 
 #include <vistle/util/export.h>
-#ifdef SENSEI
-#if defined(vistle_sensei_vtk_EXPORTS)
-#define V_VTK_EXPORT V_EXPORT
-#else
-#define V_VTK_EXPORT V_IMPORT
-#endif
-#else
+
 #if defined(vistle_vtk_EXPORTS)
 #define V_VTK_EXPORT V_EXPORT
 #else
 #define V_VTK_EXPORT V_IMPORT
 #endif
-#endif
+#endif // VISTLE_VTK_TO_VISTLE_EXPORT_H
 
-#endif
