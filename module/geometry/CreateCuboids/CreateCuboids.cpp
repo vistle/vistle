49acfc32ac469faf82f4147601c2692c88af1a51
DeVasconcelos <75034916+DeVasconcelos@users.noreply.github.com>
2024-10-16 14:11:29 +0200

    Add CreateCuboids module (#191)
    This geometry module creates a hexahedral grid of cuboids given an input grid where the point coordinates are the cuboid centers and its mapped data are the edge lengths in x, y, z.

---
+++ module/geometry/CreateCuboids/CreateCuboids.cpp
@@ -0,0 +1,111 @@
+#include <vistle/alg/objalg.h>
+#include <vistle/core/unstr.h>
+
+#include "CreateCuboids.h"
+
+MODULE_MAIN(CreateCuboids)
+
+using namespace vistle;
+
+CreateCuboids::CreateCuboids(const std::string &name, int moduleID, mpi::communicator comm)
+: Module(name, moduleID, comm)
+{
+    createInputPort("grid_in", "grid containing cuboid definitions");
+    createOutputPort("grid_out", "grid containing cuboids");
+}
+
+CreateCuboids::~CreateCuboids()
+{}
+
+// returns an unstructured hexahedral grid containing the cuboids defined by `cuboidCenters` and `edgeLengths`
+UnstructuredGrid::ptr createCuboidGrid(Coords::const_ptr cuboidCenters, Vec<Scalar, 3>::const_ptr edgeLengths)
+{
+    // clang-format off
+    /*
+        This function creates cuboids from the given centers and edge lengths following vistle's
+        hexahedron definition (see lib/vistle/core/unstr.h):
+                    
+                   7 -------- 6
+                  /|         /|
+                 / |        / |
+                4 -------- 5  |        
+                |  3-------|--2
+                | /        | /
+                |/         |/
+                0----------1
+        
+        If cP = (cX | cY | cZ) is one of the given cuboid centers and lX, lY, lZ are the corresponding edge lengths 
+        along the x-, y- and z-axis, the corners are calculated as follows:
+        corner 0 = ( cX + 0.5 * lX | cY - 0.5 * lY | cZ - 0.5 * lZ), 
+        corner 1 = ( cX + 0.5 * lX | cY + 0.5 * lY | cZ - 0.5 * lZ), ...
+
+        The following is the complete list of signs in front of the edge lengths for each corner:
+    */
+    std::array<int, 24> signs = {  1, -1, -1, // corner 0
+                                   1,  1, -1, // corner 1
+                                  -1,  1, -1, // ...
+                                  -1, -1, -1, 
+                                   1, -1,  1, 
+                                   1,  1,  1, 
+                                  -1,  1,  1, 
+                                  -1, -1,  1  
+                                };
+    // clang-format on
+
+    auto numCenters = cuboidCenters->getNumCoords();
+    auto cornersPerCuboid = 8;
+
+    UnstructuredGrid::ptr cuboids(
+        new UnstructuredGrid(numCenters, cornersPerCuboid * numCenters, cornersPerCuboid * numCenters));
+
+    for (Index i = 0; i < cuboidCenters->getNumCoords(); i++) {
+        auto x = cuboidCenters->x()[i];
+        auto y = cuboidCenters->y()[i];
+        auto z = cuboidCenters->z()[i];
+
+        auto lengthX = 0.5 * edgeLengths->x()[i];
+        auto lengthY = 0.5 * edgeLengths->y()[i];
+        auto lengthZ = 0.5 * edgeLengths->z()[i];
+
+        for (auto j = 0; j < cornersPerCuboid; j++) {
+            cuboids->x()[cornersPerCuboid * i + j] = x + lengthX * signs[3 * j];
+            cuboids->y()[cornersPerCuboid * i + j] = y + lengthY * signs[3 * j + 1];
+            cuboids->z()[cornersPerCuboid * i + j] = z + lengthZ * signs[3 * j + 2];
+
+            cuboids->cl()[cornersPerCuboid * i + j] = cornersPerCuboid * i + j;
+        }
+
+        cuboids->tl()[i] = UnstructuredGrid::HEXAHEDRON;
+        cuboids->el()[i] = cornersPerCuboid * i;
+    }
+    cuboids->el()[numCenters] = cornersPerCuboid * numCenters;
+
+    return cuboids;
+}
+
+bool CreateCuboids::compute(const std::shared_ptr<vistle::BlockTask> &task) const
+{
+    auto definition = task->expect<Vec<Scalar, 3>>("grid_in");
+    if (!definition) {
+        sendError("The mapped data, i.e., the edge lengths (in x, y, z) for each cuboid, must be three-dimensional!");
+        return true;
+    }
+
+    auto container = splitContainerObject(definition);
+    auto geo = container.geometry;
+    if (!Coords::as(geo)) {
+        sendError("The input grid does not contain point coordinates (i.e., cuboid centers), cannot create cuboids!");
+        return true;
+    }
+    auto centers = Coords::as(geo);
+    auto lengths = Vec<Scalar, 3>::as(container.mapped);
+
+    auto cuboids = createCuboidGrid(centers, lengths);
+
+    cuboids->copyAttributes(geo);
+    updateMeta(cuboids);
+
+    task->addObject("grid_out", cuboids);
+
+    return true;
+}
