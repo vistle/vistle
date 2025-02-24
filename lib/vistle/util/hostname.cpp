6bf5c61caa11407c00b8c5cb87ae8f2209648c4d
Martin Aumüller <aumuell@reserv.at>
2025-02-24 17:05:07 +0100

    remove old code

--- lib/vistle/util/hostname.cpp
+++ lib/vistle/util/hostname.cpp
@@ -22,28 +22,21 @@ std::string clustername()
 std::string hostname()
 {
     static std::string hname;
-
-    if (hname.empty()) {
-#if 0
-      const size_t HOSTNAMESIZE = 256;
-
-      char hostname[HOSTNAMESIZE];
-      gethostname(hostname, HOSTNAMESIZE-1);
-      hostname[HOSTNAMESIZE-1] = '\0';
-      hname = hostname;
-#else
-        const char *hn = getenv("VISTLE_HOSTNAME");
-        if (hn) {
-            hname = hn;
-        } else {
-            boost::system::error_code ec;
-            hname = boost::asio::ip::host_name(ec);
-            if (ec) {
-                std::cerr << "failed to get hostname: " << ec.message() << std::endl;
-            }
-        }
-#endif
+    if (!hname.empty()) {
+        return hname;
     }
+
+    const char *hn = getenv("VISTLE_HOSTNAME");
+    if (hn) {
+        hname = hn;
+    } else {
+        boost::system::error_code ec;
+        hname = boost::asio::ip::host_name(ec);
+        if (ec) {
+            std::cerr << "failed to get hostname: " << ec.message() << std::endl;
+        }
+    }
+
     return hname;
 }
 
