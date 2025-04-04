74bb7d7edd6c2f24bf9c16ae060d725f3295228d
DeVasconcelos <malheiros.susanne@gmail.com>
2025-04-04 16:37:02 +0200

    build and include BigWhoop in Vistle

---
+++ lib/vistle/core/archives_compress_bigwhoop.cpp
@@ -0,0 +1,8 @@
+#include <bwc.h>
+
+#include "archives_compress_bigwhoop.h"
+
+void compressBigWhoop()
+{
+    bwc_codec *decoder = bwc_alloc_decoder();
+}
---
+++ lib/vistle/core/archives_compress_bigwhoop.h
@@ -0,0 +1,7 @@
+#ifndef VISTLE_CORE_ARCHIVES_COMPRESS_BIGWHOOP_H
+#define VISTLE_CORE_ARCHIVES_COMPRESS_BIGWHOOP_H
+
+// just for testing...
+void compressBigWhoop();
+
+#endif
