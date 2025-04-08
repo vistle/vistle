fef3ad86852cd6e553a287c36136a2366e431c59
DeVasconcelos <malheiros.susanne@gmail.com>
2025-04-08 16:08:17 +0200

    write first version of the compress functionality

--- lib/vistle/core/archives_compress_bigwhoop.cpp
+++ lib/vistle/core/archives_compress_bigwhoop.cpp
@@ -1,8 +1,45 @@
 #include <bwc.h>
 
+#include "index.h"
 #include "archives_compress_bigwhoop.h"
 
-void compressBigWhoop()
+namespace vistle {
+namespace detail {
+
+// BigWhoop seems to only support floating point compression
+template<typename T>
+size_t compressBigWhoop(T *toCompress, const Index dim[3], T *compressed, const BigWhoopParameters &parameters)
 {
-    bwc_codec *decoder = bwc_alloc_decoder();
+    return 0;
 }
+
+template<>
+size_t compressBigWhoop(float *toCompress, const Index dim[3], float *compressed, const BigWhoopParameters &parameters)
+{
+    bwc_codec *coder = bwc_alloc_coder(dim[0], dim[1], dim[2], 1, parameters.nPar, bwc_precision_single);
+    bwc_stream *stream = bwc_init_stream(toCompress, compressed, comp);
+
+    bwc_create_compression(coder, stream, parameters.rate);
+    size_t compressed_size = bwc_compress(coder, stream);
+
+    bwc_free_codec(coder);
+
+    return compressed_size;
+}
+
+template<>
+size_t compressBigWhoop(double *toCompress, const Index dim[3], double *compressed,
+                        const BigWhoopParameters &parameters)
+{
+    bwc_codec *coder = bwc_alloc_coder(dim[0], dim[1], dim[2], 1, parameters.nPar, bwc_precision_double);
+    bwc_stream *stream = bwc_init_stream(toCompress, compressed, comp);
+
+    bwc_create_compression(coder, stream, parameters.rate);
+    size_t compressed_size = bwc_compress(coder, stream);
+
+    bwc_free_codec(coder);
+
+    return compressed_size;
+}
+} // namespace detail
+} // namespace vistle
--- lib/vistle/core/archives_compress_bigwhoop.h
+++ lib/vistle/core/archives_compress_bigwhoop.h
@@ -1,7 +1,26 @@
 #ifndef VISTLE_CORE_ARCHIVES_COMPRESS_BIGWHOOP_H
 #define VISTLE_CORE_ARCHIVES_COMPRESS_BIGWHOOP_H
 
-// just for testing...
-void compressBigWhoop();
+namespace vistle {
+namespace detail {
+
+struct BigWhoopParameters {
+    uint8_t nPar;
+    char *rate;
+};
+
+template<typename T>
+size_t compressBigWhoop(T *toCompress, const Index dim[3], T *compressed, const BigWhoopParameters &parameters);
+
+template<>
+size_t compressBigWhoop(float *toCompress, const Index dim[3], float *compressed, const BigWhoopParameters &parameters);
+
+template<>
+size_t compressBigWhoop(double *toCompress, const Index dim[3], double *compressed,
+                        const BigWhoopParameters &parameters);
+
+} // namespace detail
+} // namespace vistle
+
 
 #endif
