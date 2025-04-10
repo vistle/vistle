bd01d95768a65202b5d8d719b64a7ff5be77bab2
DeVasconcelos <malheiros.susanne@gmail.com>
2025-04-10 16:26:48 +0200

    move primary template to header and remove explicit instantiation in order to support all data types (Seems like only floating point values can be (de)compressed. Providing fallback implementation for the rest.)

--- lib/vistle/core/archives_compress_bigwhoop.cpp
+++ lib/vistle/core/archives_compress_bigwhoop.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include <bwc.h>
 
 #include "index.h"
@@ -9,13 +7,6 @@
 
 namespace vistle {
 namespace detail {
-
-template<typename T>
-size_t compressBigWhoop(T *toCompress, const Index dim[3], T *compressed, const CompressionSettings &config)
-{
-    std::cerr << "BigWhoop only supports floating point compression" << std::endl;
-    return 0;
-}
 
 template<>
 size_t compressBigWhoop<float>(float *toCompress, const Index dim[3], float *compressed,
@@ -47,14 +38,8 @@ size_t compressBigWhoop<double>(double *toCompress, const Index dim[3], double *
     return compressed_size;
 }
 
-template<typename T, typename std::enable_if<!std::is_floating_point<T>::value, int>::type>
-void decompressBigWhoop(T *toDecompress, const Index dim[3], T *decompressed, uint8_t layer)
-{
-    std::cerr << "BigWhoop only supports floating point decompression" << std::endl;
-}
-
-template<typename T, typename std::enable_if<std::is_floating_point<T>::value, int>::type>
-void decompressBigWhoop(T *toDecompress, const Index dim[3], T *decompressed, uint8_t layer)
+template<>
+void decompressBigWhoop<float>(float *toDecompress, const Index dim[3], float *decompressed, uint8_t layer)
 {
     bwc_codec *decoder = bwc_alloc_decoder();
     bwc_stream *stream = bwc_init_stream(toDecompress, decompressed, decomp);
@@ -64,28 +49,16 @@ void decompressBigWhoop(T *toDecompress, const Index dim[3], T *decompressed, ui
     bwc_free_codec(decoder);
 }
 
-template size_t compressBigWhoop<void>(void *toCompress, const Index dim[3], void *compressed,
-                                       const CompressionSettings &config);
-template size_t compressBigWhoop<float>(float *toCompress, const Index dim[3], float *compressed,
-                                        const CompressionSettings &config);
-template size_t compressBigWhoop<double>(double *toCompress, const Index dim[3], double *compressed,
-                                         const CompressionSettings &config);
-template size_t compressBigWhoop<int32_t>(int32_t *toCompress, const Index dim[3], int32_t *compressed,
-                                          const CompressionSettings &config);
-template size_t compressBigWhoop<int64_t>(int64_t *toCompress, const Index dim[3], int64_t *compressed,
-                                          const CompressionSettings &config);
+template<>
+void decompressBigWhoop<double>(double *toDecompress, const Index dim[3], double *decompressed, uint8_t layer)
+{
+    bwc_codec *decoder = bwc_alloc_decoder();
+    bwc_stream *stream = bwc_init_stream(toDecompress, decompressed, decomp);
 
-template void vistle::detail::decompressBigWhoop<void>(void *toDecompress, const Index dim[3], void *decompressed,
-                                                       uint8_t layer);
-template void vistle::detail::decompressBigWhoop<float>(float *toDecompress, const Index dim[3], float *decompressed,
-                                                        uint8_t layer);
-template void vistle::detail::decompressBigWhoop<double>(double *toDecompress, const Index dim[3], double *decompressed,
-                                                         uint8_t layer);
-template void vistle::detail::decompressBigWhoop<int32_t>(int32_t *toDecompress, const Index dim[3],
-                                                          int32_t *decompressed, uint8_t layer);
-template void vistle::detail::decompressBigWhoop<int64_t>(int64_t *toDecompress, const Index dim[3],
-                                                          int64_t *decompressed, uint8_t layer);
-
+    bwc_create_decompression(decoder, stream, layer);
+    bwc_decompress(decoder, stream);
+    bwc_free_codec(decoder);
+}
 
 } // namespace detail
 } // namespace vistle
--- lib/vistle/core/archives_compress_bigwhoop.h
+++ lib/vistle/core/archives_compress_bigwhoop.h
@@ -1,6 +1,8 @@
 #ifndef VISTLE_CORE_ARCHIVES_COMPRESS_BIGWHOOP_H
 #define VISTLE_CORE_ARCHIVES_COMPRESS_BIGWHOOP_H
 
+#include <iostream>
+
 #include <type_traits>
 
 #include "archives_compression_settings.h"
@@ -9,8 +11,12 @@ namespace vistle {
 namespace detail {
 
 template<typename T>
-size_t V_COREEXPORT compressBigWhoop(T *toCompress, const Index dim[3], T *compressed,
-                                     const CompressionSettings &config);
+size_t compressBigWhoop(T *toCompress, const Index dim[3], T *compressed, const CompressionSettings &config)
+{
+    std::cerr << "Big Whoop only supports floating point compression!" << std::endl;
+    return 0;
+}
+
 template<>
 size_t V_COREEXPORT compressBigWhoop<float>(float *toCompress, const Index dim[3], float *compressed,
                                             const CompressionSettings &config);
@@ -19,11 +25,19 @@ size_t V_COREEXPORT compressBigWhoop<double>(double *toCompress, const Index dim
                                              const CompressionSettings &config);
 
 
-template<typename T, typename std::enable_if<!std::is_floating_point<T>::value, int>::type = 0>
-void V_COREEXPORT decompressBigWhoop(T *toDecompress, const Index dim[3], T *decompressed, uint8_t layer);
+template<typename T>
+void decompressBigWhoop(T *toDecompress, const Index dim[3], T *decompressed, uint8_t layer)
+{
+    std::cerr << "Big Whoop only supports floating point decompression!" << std::endl;
+}
 
-template<typename T, typename std::enable_if<std::is_floating_point<T>::value, int>::type = 0>
-void V_COREEXPORT decompressBigWhoop(T *toDecompress, const Index dim[3], T *decompressed, uint8_t layer);
+template<>
+void V_COREEXPORT decompressBigWhoop<float>(float *toDecompress, const Index dim[3], float *decompressed,
+                                            uint8_t layer);
+
+template<>
+void V_COREEXPORT decompressBigWhoop<double>(double *toDecompress, const Index dim[3], double *decompressed,
+                                             uint8_t layer);
 
 } // namespace detail
 } // namespace vistle
