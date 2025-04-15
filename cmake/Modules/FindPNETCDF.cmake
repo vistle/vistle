f2f876201739bb5bfe47329df11563a871c2cc46
Martin Aumüller <aumuell@reserv.at>
2025-04-15 11:35:53 +0200

    if not using MPI, don't use pnetcdf linked against an arbitrary MPI implementation

--- cmake/Modules/FindPNETCDF.cmake
+++ cmake/Modules/FindPNETCDF.cmake
@@ -5,26 +5,32 @@
 # PNETCDF_LIBRARIES - libraries needed for PNetCDF
 # PNETCDF_DEFINITIONS - Compiler switches for using PNetCDF
 
-find_package(PkgConfig)
-pkg_check_modules(PC_PNETCDF QUIET pnetcdf)
-set(PNETCDF_DEFINITIONS ${PC_PNETCDF_CFLAGS_OTHER})
+if(NOT VISTLE_USE_MPI)
+    set(PNETCDF_FOUND FALSE)
+else()
 
-find_path(
-    PNETCDF_INCLUDE_DIR pnetcdf pnetcdf.h
-    HINTS ${PC_PNETCDF_INCLUDEDIR} ${PC_PNETCDF_INCLUDE_DIRS}
-    PATH_SUFFIXES pnetcdf)
+    find_package(PkgConfig)
+    pkg_check_modules(PC_PNETCDF QUIET pnetcdf)
+    set(PNETCDF_DEFINITIONS ${PC_PNETCDF_CFLAGS_OTHER})
 
-find_library(
-    PNETCDF_LIBRARY
-    NAMES pnetcdf libpnetcdf
-    HINTS ${PC_PNETCDF_LIBDIR} ${PC_PNETCDF_LIBRARY_DIRS})
+    find_path(
+        PNETCDF_INCLUDE_DIR pnetcdf pnetcdf.h
+        HINTS ${PC_PNETCDF_INCLUDEDIR} ${PC_PNETCDF_INCLUDE_DIRS}
+        PATH_SUFFIXES pnetcdf)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PNETCDF DEFAULT_MSG PNETCDF_LIBRARY PNETCDF_INCLUDE_DIR)
+    find_library(
+        PNETCDF_LIBRARY
+        NAMES pnetcdf libpnetcdf
+        HINTS ${PC_PNETCDF_LIBDIR} ${PC_PNETCDF_LIBRARY_DIRS})
 
-mark_as_advanced(PNETCDF_INCLUDE_DIR PNETCDF_LIBRARY)
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(PNETCDF DEFAULT_MSG PNETCDF_LIBRARY PNETCDF_INCLUDE_DIR)
 
-if(PNETCDF_FOUND)
-    set(PNETCDF_LIBRARIES ${PNETCDF_LIBRARY})
-    set(PNETCDF_INCLUDE_DIRS ${PNETCDF_INCLUDE_DIR})
+    mark_as_advanced(PNETCDF_INCLUDE_DIR PNETCDF_LIBRARY)
+
+    if(PNETCDF_FOUND)
+        set(PNETCDF_LIBRARIES ${PNETCDF_LIBRARY})
+        set(PNETCDF_INCLUDE_DIRS ${PNETCDF_INCLUDE_DIR})
+    endif()
+
 endif()
