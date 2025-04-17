f17570fe0d2a1aa5b0baf42c49d1ea505208e4a4
Martin Aumüller <aumuell@reserv.at>
2025-04-18 01:35:37 +0200

    output coordinates and data as CSV

---
+++ module/write/WritePointsCsv/WritePointsCsv.h
@@ -0,0 +1,33 @@
+#ifndef VISTLE_WRITEPOINTSCSV_WRITEPOINTSCSV_H
+#define VISTLE_WRITEPOINTSCSV_WRITEPOINTSCSV_H
+
+#include <vistle/module/module.h>
+
+#include <ostream>
+#include <fstream>
+
+class WritePointsCsv: public vistle::Module {
+public:
+    WritePointsCsv(const std::string &name, int moduleID, mpi::communicator comm);
+    ~WritePointsCsv();
+
+    static const int NumPorts = 3;
+    std::array<vistle::Port *, NumPorts> m_port;
+    vistle::StringParameter *m_filename = nullptr;
+
+private:
+    bool compute() override;
+    bool prepare() override;
+    bool reduce(int t) override;
+
+    vistle::IntParameter *m_writeHeader = nullptr;
+    vistle::IntParameter *m_writeCoordinates = nullptr;
+    vistle::IntParameter *m_writeData = nullptr;
+    vistle::IntParameter *m_writeTime = nullptr;
+    vistle::IntParameter *m_writeBlock = nullptr;
+    vistle::IntParameter *m_writeIndex = nullptr;
+
+    std::ofstream m_csv;
+};
+
+#endif
