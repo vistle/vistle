d93d58f1bdc04ae364cdc4219898fa8b09a7bb4d
Martin Aumüller <aumuell@reserv.at>
2025-04-30 10:14:01 +0200

    don't exagerate zooming

--- app/gui/dataflowview.cpp
+++ app/gui/dataflowview.cpp
@@ -492,7 +492,7 @@ void DataFlowView::zoomAll()
     }
     fitInView(bounds, Qt::KeepAspectRatio);
     auto t = transform();
-    if (t.m11() > 4 || t.m22() > 4) {
+    if (t.m11() > 2.5 || t.m22() > 2.5) {
         t.setMatrix(4., t.m12(), t.m13(), t.m21(), 4., t.m23(), t.m31(), t.m32(), t.m33());
         setTransform(t);
     }
