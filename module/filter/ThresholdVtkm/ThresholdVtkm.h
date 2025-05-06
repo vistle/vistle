a761d8ecb32349d2ad28a7c0c0ebc4e928b4ea77
DeVasconcelos <75034916+DeVasconcelos@users.noreply.github.com>
2025-05-06 12:30:09 +0200

    Create VTK-m version of the Threshold module (#278)
    * started implementing ThresholdVtkm module
    
    * enable VTK-m's clean_grid and entitiy_extraction modules (required by Threshold filter)
    
    * add module parameters (from CPU version)
    
    * remove Contains operation (cannot be calculated with Threshold filter)
    
    * allow empty results
    
    * VtkmModule was moved to vistle namespace in previous commit

---
+++ module/filter/ThresholdVtkm/ThresholdVtkm.h
@@ -0,0 +1,28 @@
+#ifndef VISTLE_THRESHOLDVTKM_THRESHOLDVTKM_H
+#define VISTLE_THRESHOLDVTKM_THRESHOLDVTKM_H
+
+#include <vistle/vtkm/vtkm_module.h>
+
+class ThresholdVtkm: public vistle::VtkmModule {
+public:
+    ThresholdVtkm(const std::string &name, int moduleID, mpi::communicator comm);
+    ~ThresholdVtkm();
+
+private:
+    vistle::IntParameter *m_invert = nullptr;
+    vistle::IntParameter *m_operation = nullptr;
+    vistle::FloatParameter *m_threshold = nullptr;
+
+    std::unique_ptr<vtkm::filter::Filter> setUpFilter() const override;
+
+    vistle::Object::ptr prepareOutputGrid(const vtkm::cont::DataSet &dataset,
+                                          const vistle::Object::const_ptr &inputGrid) const override;
+
+    vistle::DataBase::ptr prepareOutputField(const vtkm::cont::DataSet &dataset,
+                                             const vistle::Object::const_ptr &inputGrid,
+                                             const vistle::DataBase::const_ptr &inputField,
+                                             const std::string &fieldName,
+                                             const vistle::Object::ptr &outputGrid) const override;
+};
+
+#endif
