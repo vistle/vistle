873fefca8bff4a94ee918cd2cc455f2d12dc1fd9
hpcdgrie <grieger@hlrs.de>
2025-08-20 15:55:42 +0200

    Added OpenCOVER plugin for Transform module

---
+++ module/render/COVER/plugin/Transform/TransformInteraction.h
@@ -0,0 +1,38 @@
+#ifndef VISTLE_COVER_PLUGIN_TRANSFORM_TRANSFORMINTERACTION_H
+#define VISTLE_COVER_PLUGIN_TRANSFORM_TRANSFORMINTERACTION_H
+
+#include <memory>
+#include <PluginUtil/ModuleInteraction.h>
+
+namespace opencover {
+class coInteractor;
+class coVR3DTransformInteractor;
+class RenderObject;
+class TransformPlugin;
+namespace ui {
+class Action;
+}
+
+class TransformInteraction: public ModuleInteraction {
+private:
+    std::unique_ptr<coVR3DTransformInteractor> m_interactor;
+    TransformPlugin *m_plugin;
+    opencover::ui::Action *m_resetScaleButton;
+
+public:
+    TransformInteraction(const RenderObject *container, coInteractor *inter, const char *pluginName,
+                         TransformPlugin *plugin);
+    virtual ~TransformInteraction();
+
+    void preFrame() override;
+    void update(const RenderObject *container, coInteractor *inter) override;
+    void updatePickInteractors(bool show) override;
+    void updateDirectInteractors(bool show) override;
+
+private:
+    void updateInteractorTransform();
+    void sendTransformToModule();
+};
+
+} // namespace opencover
+#endif
