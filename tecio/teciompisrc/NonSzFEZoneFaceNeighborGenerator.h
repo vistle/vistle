b70274446bd5c159ded1a29b98f93b453204e42f
rosBa <rosina.baumann@hlrs.de>
2025-09-02 13:48:59 +0200

    comment out prints and adding partition handle to improve performance

--- tecio/teciompisrc/NonSzFEZoneFaceNeighborGenerator.h
+++
@@ -1,3 +0,0 @@
- #pragma once
-#include "FaceNeighborGeneratorAbstract.h"
-namespace tecplot { namespace ___3931 { class NonSzFEZoneFaceNeighborGenerator : public FaceNeighborGeneratorAbstract { public: explicit NonSzFEZoneFaceNeighborGenerator(class ___37& ___36); virtual ___372 gatherUserFaceNeighbors(std::vector<int32_t>& userFaceNeighbors, ___4633 zone) const; }; }}
--- tecio/teciosrc/DataWriteStatisticsInterface.h
+++
@@ -1,3 +0,0 @@
- #pragma once
-#include <string>
-namespace tecplot { namespace ___3931 { class ___934 { public: virtual ~___934() {} virtual void addWriteTime(uint64_t milliseconds) = 0; virtual void ___7(uint64_t bytes) = 0; virtual void setMemoryInfoString(std::string const& str) = 0; }; }}
