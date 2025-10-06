cf284a6d41a5fe7913940eb8b65aac2fbe6d8dbe
Martin Aumüller <aumuell@reserv.at>
2025-10-06 12:47:33 +0200

    direct data connection between ranks
    Try to establish a direct connection between individual data manager ranks. If this has succeeded, transfer data objects directly from source to destination data manager, without letting hubs forward these messages.
    
    Disabled for now via setting num_direct_connections to 0.

--- lib/vistle/manager/datamanager.h
+++ lib/vistle/manager/datamanager.h
@@ -15,6 +15,7 @@
 #include <boost/asio/executor_work_guard.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/deadline_timer.hpp>
 
 #include <boost/mpi/request.hpp>
 #include <boost/mpi/communicator.hpp>
@@ -26,11 +27,18 @@ class StateTracker;
 class Object;
 
 class DataManager {
+    typedef boost::asio::ip::tcp::acceptor acceptor;
+    typedef boost::asio::ip::address address;
+    typedef boost::asio::io_context io_context;
+
 public:
+    typedef boost::asio::ip::tcp::socket tcp_socket;
+
     DataManager(boost::mpi::communicator &comm, unsigned short baseport);
     ~DataManager();
     unsigned short port() const;
-    bool handle(const message::Message &msg, buffer *payload);
+
+    bool handle(std::shared_ptr<tcp_socket> &sock, const message::Message &msg, buffer *payload);
     //! request a remote object for servicing an AddObject request
     bool requestObject(const message::AddObject &add, const std::string &objId, const ObjectCompletionHandler &handler);
     //! request a remote object for resolving a reference to a sub-object
@@ -46,6 +54,8 @@ public:
 
     void trace(message::Type type);
 
+    bool send(std::shared_ptr<tcp_socket> &sock, const message::Message &message,
+              std::shared_ptr<buffer> payload = nullptr);
     bool send(const message::Message &message, std::shared_ptr<buffer> payload = nullptr);
 
     struct Msg {
@@ -58,9 +68,21 @@ public:
     void removeHub(const message::RemoveHub &hub);
 
 private:
+    io_context &io();
+    void startThread();
+    void closeDirect(int hub);
+    void closeAllDirect();
+
+    void startAccept(acceptor &a);
+    void handleAccept(acceptor &a, const boost::system::error_code &error, std::shared_ptr<tcp_socket> sock);
+    void handleConnect(std::shared_ptr<tcp_socket> sock0, std::shared_ptr<tcp_socket> sock1,
+                       const boost::system::error_code &error);
+    void shutdownSocket(std::shared_ptr<tcp_socket> sock, const std::string &reason);
+
     bool handlePriv(const message::RequestObject &req);
     bool handlePriv(const message::SendObject &snd, buffer *payload);
     bool handlePriv(const message::AddObjectCompleted &complete);
+    bool serveDirectSocket(std::shared_ptr<tcp_socket> sock);
     void updateStatus();
 
     std::mutex m_recvMutex;
@@ -77,10 +99,37 @@ private:
     boost::mpi::request m_req;
     int m_msgSize;
 
+    unsigned short m_port;
     boost::asio::io_context m_ioContext;
-    boost::asio::ip::tcp::acceptor m_acceptor_v4;
-    boost::asio::ip::tcp::acceptor m_acceptor_v6;
-    boost::asio::ip::tcp::socket m_dataSocket;
+    boost::asio::ip::tcp::acceptor m_acceptorv4;
+    boost::asio::ip::tcp::acceptor m_acceptorv6;
+    std::shared_ptr<boost::asio::ip::tcp::socket> m_dataSocket;
+    std::mutex m_directSocketsMutex;
+    struct SocketList {
+        SocketList(boost::asio::io_context &io): connectionTimer(io) {}
+        SocketList(SocketList &&other): connectionTimer(other.connectionTimer.get_executor())
+        {
+            std::lock_guard<std::mutex> guard(other.mutex);
+            connecting = std::move(other.connecting);
+            sockets = std::move(other.sockets);
+            current = other.current;
+        }
+        std::mutex mutex;
+        boost::asio::deadline_timer connectionTimer;
+        std::set<std::shared_ptr<boost::asio::ip::tcp::socket>> connecting;
+        std::vector<std::shared_ptr<boost::asio::ip::tcp::socket>> sockets;
+        size_t current = 0; //!< round-robin counter for next socket to use
+        std::shared_ptr<boost::asio::ip::tcp::socket> get()
+        {
+            std::lock_guard<std::mutex> guard(mutex);
+            if (sockets.empty())
+                return nullptr;
+            if (current >= sockets.size())
+                current = 0;
+            return sockets[current++];
+        }
+    };
+    std::map<int, std::vector<SocketList>> m_directSockets; // for every hub and rank, a vector of sockets
 
     std::set<message::AddObject>
         m_inTransitObjects; //!< objects for which AddObject messages have been sent to remote hubs -- cannot be deleted yet
@@ -112,11 +161,17 @@ private:
     std::deque<std::future<bool>> m_sendTasks;
 
     boost::asio::executor_work_guard<boost::asio::io_context::executor_type> m_workGuard;
-    std::thread m_ioThread;
+    std::mutex m_threadsMutex;
+    std::vector<std::thread> m_ioThreads;
     std::thread m_listenThread;
     std::thread m_recvThread;
     std::thread m_cleanThread;
     message::Type m_traceMessages = message::INVALID;
+
+    double m_connectionTimeout = 10; // seconds
+    size_t m_numConnections = 1; // number of connections per rank (and direction)
+    bool m_asyncSend = true; // don't wait for message sends to complete
+    bool m_useDirectComm = true; // use direct connections between hubs if possible
 };
 
 } // namespace vistle
