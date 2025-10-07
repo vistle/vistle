50045bbb5cdb6e4f648e66f53c00853cfdbf28bc
karthikeya-ms <writetokarthikeya99@gmail.com>
2025-10-07 15:31:38 +0200

    VecToScalarVtkm: works on all kinds of grids and ports to GPU

--- module/filter/VecToScalarVtkm/VecToScalarVtkm.h
+++ module/filter/VecToScalarVtkm/VecToScalarVtkm.h
@@ -10,8 +10,23 @@ public:
     ~VecToScalarVtkm();
 
 private:
+    // vistle::IntParameter *m_caseParam;
+    ModuleStatusPtr prepareInputField(const vistle::Port *port, const vistle::Object::const_ptr &grid,
+                                      const vistle::DataBase::const_ptr &field, std::string &fieldName,
+                                      viskores::cont::DataSet &dataset) const override;
+
+                                   
 
     std::unique_ptr<viskores::filter::Filter> setUpFilter() const override;
+
+    vistle::Object::const_ptr prepareOutputGrid(const viskores::cont::DataSet &dataset,
+                                                const vistle::Object::const_ptr &inputGrid) const override;
+
+    vistle::DataBase::ptr prepareOutputField(const viskores::cont::DataSet &dataset,
+                                             const vistle::Object::const_ptr &inputGrid,
+                                             const vistle::DataBase::const_ptr &inputField,
+                                             const std::string &fieldName,
+                                             const vistle::Object::const_ptr &outputGrid) const override;
 };
 
 #endif
