c636495f949c4fd6dd8b80f1fd124c129ca2a221
karthikeya-ms <writetokarthikeya99@gmail.com>
2025-10-07 18:01:45 +0200

    convert vector field to scalars: X/Y/Z/Magnitude

--- module/filter/VecToScalarVtkm/VecToScalarVtkm.cpp
+++ module/filter/VecToScalarVtkm/VecToScalarVtkm.cpp
@@ -3,6 +3,10 @@
 
 #include "VecToScalarVtkm.h"
 
+#include <viskores/cont/UnknownArrayHandle.h>
+#include <viskores/filter/vector_analysis/VectorMagnitude.h>
+
+
 MODULE_MAIN(VecToScalarVtkm)
 
 using namespace vistle;
@@ -12,18 +16,51 @@ using namespace vistle;
 VecToScalarVtkm::VecToScalarVtkm(const std::string &name, int moduleID, mpi::communicator comm)
     : VtkmModule(name, moduleID, comm, 1, MappedDataHandling::Use)
 {
+    m_caseParam = addIntParameter("choose_scalar_value", "Choose Scalar Value", 3, Parameter::Choice);
+    setParameterChoices(m_caseParam, std::vector<std::string>{"X","Y","Z","Magnitude"});
+
 }
 
 VecToScalarVtkm::~VecToScalarVtkm()
 {
 }
 
-std::unique_ptr<viskores::filter::Filter> VecToScalarVtkm::setUpFilter() const
-{
+class ExtractComponentFilter : public viskores::filter::Filter {
+public:
+  void SetComponentIndex(int c) { comp_ = c; }
+private:
+  int comp_ = 0;
 
-    return std::make_unique<viskores::filter::vector_analysis::VectorMagnitude>();
+  viskores::cont::DataSet DoExecute(const viskores::cont::DataSet &in) override {
+    auto inField = this->GetFieldFromDataSet(in);
+    auto assoc   = inField.GetAssociation();
+    auto name    = inField.GetName();
 
+    using BaseT = vistle::Scalar; // <- key line
+    auto ua = inField.GetData();
+    auto compAH = ua.ExtractComponent<BaseT>(comp_);  // <-- now compiles
+    viskores::cont::DataSet out = in;
+    out.AddField(viskores::cont::Field(name, assoc, compAH));
+    return out;
+  }
+};
+
+
+
+std::unique_ptr<viskores::filter::Filter> VecToScalarVtkm::setUpFilter() const {
+  const int choice = m_caseParam->getValue(); // 0=X,1=Y,2=Z,3=Mag
+  if (choice == 3) {
+    auto f = std::make_unique<viskores::filter::vector_analysis::VectorMagnitude>();
+    filter_ = f.get();
+    return f;
+  } else {
+    auto f = std::make_unique<ExtractComponentFilter>();
+    f->SetComponentIndex(choice);
+    filter_ = f.get();
+    return f;
+  }
 }
+
 
 
 ModuleStatusPtr VecToScalarVtkm::prepareInputField(const Port *port, const Object::const_ptr &grid,
