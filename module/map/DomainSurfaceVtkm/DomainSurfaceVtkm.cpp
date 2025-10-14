2b49585c2eac72b43ec80dc3700ccc3defee62f8
DeVasconcelos <malheiros.susanne@gmail.com>
2025-10-14 18:22:25 +0200

    port DomainSurface to Viskores
    - create VTK-m version of DomainSurface, with optional mapped dat, without module parameters for now
    - also add input check: only supporting structured and unstructured grids

---
+++ module/map/DomainSurfaceVtkm/DomainSurfaceVtkm.cpp
@@ -0,0 +1,30 @@
+#include <viskores/filter/entity_extraction/ExternalFaces.h>
+
+#include <vistle/core/structuredgrid.h>
+#include <vistle/core/unstr.h>
+
+#include "DomainSurfaceVtkm.h"
+
+MODULE_MAIN(DomainSurfaceVtkm)
+
+using namespace vistle;
+
+DomainSurfaceVtkm::DomainSurfaceVtkm(const std::string &name, int moduleID, mpi::communicator comm)
+: VtkmModule(name, moduleID, comm, 1, MappedDataHandling::Use)
+{}
+
+DomainSurfaceVtkm::~DomainSurfaceVtkm()
+{}
+
+ModuleStatusPtr DomainSurfaceVtkm::prepareInputGrid(const vistle::Object::const_ptr &grid,
+                                                    viskores::cont::DataSet &dataset) const
+{
+    if (!StructuredGridBase::as(grid) && !UnstructuredGrid::as(grid))
+        return Error("only structured and unstructured grids supported");
+    return VtkmModule::prepareInputGrid(grid, dataset);
+}
+
+std::unique_ptr<viskores::filter::Filter> DomainSurfaceVtkm::setUpFilter() const
+{
+    return std::make_unique<viskores::filter::entity_extraction::ExternalFaces>();
+}
