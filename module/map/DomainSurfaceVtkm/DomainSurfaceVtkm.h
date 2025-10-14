f9149068da5783e471f44c92fcb36dbc77f443f3
DeVasconcelos <malheiros.susanne@gmail.com>
2025-10-14 18:22:25 +0200

    port DomainSurface to Viskores
    - create VTK-m version of DomainSurface, with optional mapped dat, without module parameters for now
    - also add input check: only supporting structured and unstructured grids

---
+++ module/map/DomainSurfaceVtkm/DomainSurfaceVtkm.h
@@ -0,0 +1,17 @@
+#ifndef VISTLE_DOMAINSURFACEVTKM_DOMAINSURFACEVTKM_H
+#define VISTLE_DOMAINSURFACEVTKM_DOMAINSURFACEVTKM_H
+
+#include <vistle/vtkm/vtkm_module.h>
+
+class DomainSurfaceVtkm: public vistle::VtkmModule {
+public:
+    DomainSurfaceVtkm(const std::string &name, int moduleID, mpi::communicator comm);
+    ~DomainSurfaceVtkm();
+
+private:
+    ModuleStatusPtr prepareInputGrid(const vistle::Object::const_ptr &grid,
+                                     viskores::cont::DataSet &dataset) const override;
+    std::unique_ptr<viskores::filter::Filter> setUpFilter() const override;
+};
+
+#endif
