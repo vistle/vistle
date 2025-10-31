9e0907d5b17ed0dda0d2649fd4f7dd7f75ee7e90
DeVasconcelos <malheiros.susanne@gmail.com>
2025-10-31 11:02:31 +0100

    avoid divergent branches in the EXEC environment (moved switch cases to DoExecute() by introducing a component mask and one worklet per operation)

--- module/filter/DisplaceVtkm/filter/DisplaceFilter.cpp
+++ module/filter/DisplaceVtkm/filter/DisplaceFilter.cpp
@@ -18,7 +18,41 @@ VISKORES_CONT viskores::cont::DataSet DisplaceFilter::DoExecute(const viskores::
         this->CastAndCallScalarField(inputScalar.GetData(), [&](const auto &scalars) {
             viskores::cont::ArrayHandle<CoordType> result;
 
-            this->Invoke(vistle::DisplaceWorklet{m_component, m_operation, m_scale}, scalars, coords, result);
+            viskores::Vec<viskores::FloatDefault, 3> mask{0.0f, 0.0f, 0.0f};
+            switch (m_component) {
+            case vistle::DisplaceComponent::X:
+                mask[0] = 1.0f;
+                break;
+            case vistle::DisplaceComponent::Y:
+                mask[1] = 1.0f;
+                break;
+            case vistle::DisplaceComponent::Z:
+                mask[2] = 1.0f;
+                break;
+            case vistle::DisplaceComponent::All:
+                mask[0] = 1.0f;
+                mask[1] = 1.0f;
+                mask[2] = 1.0f;
+                break;
+            default:
+                throw viskores::cont::ErrorBadValue(
+                    "Error in DisplaceFilter: Encountered unknown DisplaceComponent value!");
+            }
+
+            switch (m_operation) {
+            case vistle::DisplaceOperation::Set:
+                this->Invoke(SetDisplaceWorklet{m_scale, mask}, scalars, coords, result);
+                break;
+            case vistle::DisplaceOperation::Add:
+                this->Invoke(AddDisplaceWorklet{m_scale, mask}, scalars, coords, result);
+                break;
+            case vistle::DisplaceOperation::Multiply:
+                this->Invoke(MultiplyDisplaceWorklet{m_scale, mask}, scalars, coords, result);
+                break;
+            default:
+                throw viskores::cont::ErrorBadValue(
+                    "Error in DisplaceFilter: Encountered unknown DisplaceOperation value!");
+            }
 
             outputCoords = result;
         });
--- module/filter/DisplaceVtkm/filter/DisplaceWorklet.h
+++ module/filter/DisplaceVtkm/filter/DisplaceWorklet.h
@@ -10,34 +10,24 @@ namespace vistle {
 DEFINE_ENUM_WITH_STRING_CONVERSIONS(DisplaceComponent, (X)(Y)(Z)(All))
 DEFINE_ENUM_WITH_STRING_CONVERSIONS(DisplaceOperation, (Set)(Add)(Multiply))
 
-template<typename T, typename S>
-VISKORES_EXEC T applyOperation(const T &value, const S &displacement, DisplaceOperation operation)
-{
-    switch (operation) {
-    case Set:
-        return displacement;
-    case Add:
-        return value + displacement;
-    case Multiply:
-        return value * displacement;
-    default:
-        std::cerr << "DisplaceWorklet: Unknown DisplaceOperation " << static_cast<int>(operation) << std::endl;
-        return value;
-    }
-}
+} // namespace vistle
 
-struct DisplaceWorklet: viskores::worklet::WorkletMapField {
-    DisplaceComponent m_component;
-    DisplaceOperation m_operation;
+struct BaseDisplaceWorklet {
     viskores::FloatDefault m_scale;
+    viskores::Vec<viskores::FloatDefault, 3> m_mask;
 
-    VISKORES_CONT DisplaceWorklet()
-    : m_component(DisplaceComponent::X), m_operation(DisplaceOperation::Add), m_scale(1.0f)
+    VISKORES_CONT BaseDisplaceWorklet(): m_scale{1.0f}, m_mask{1.0f, 1.0f, 1.0f} {};
+
+    VISKORES_CONT BaseDisplaceWorklet(viskores::FloatDefault scale, viskores::Vec<viskores::FloatDefault, 3> mask)
+    : m_scale{scale}, m_mask{mask}
     {}
+};
 
-    VISKORES_CONT DisplaceWorklet(DisplaceComponent component, DisplaceOperation operation,
-                                  viskores::FloatDefault scale)
-    : m_component(component), m_operation(operation), m_scale(scale)
+struct SetDisplaceWorklet: BaseDisplaceWorklet, viskores::worklet::WorkletMapField {
+    VISKORES_CONT SetDisplaceWorklet(): BaseDisplaceWorklet(){};
+
+    VISKORES_CONT SetDisplaceWorklet(viskores::FloatDefault scale, viskores::Vec<viskores::FloatDefault, 3> mask)
+    : BaseDisplaceWorklet(scale, mask)
     {}
 
     using ControlSignature = void(FieldIn scalarField, FieldIn coords, FieldOut result);
@@ -48,26 +38,49 @@ struct DisplaceWorklet: viskores::worklet::WorkletMapField {
     VISKORES_EXEC void operator()(const T &scalar, const viskores::Vec<S, 3> &coord,
                                   viskores::Vec<S, 3> &displacedCoord) const
     {
-        displacedCoord = coord;
-        switch (m_component) {
-        case X:
-            displacedCoord[0] = applyOperation(coord[0], scalar * m_scale, m_operation);
-            break;
-        case Y:
-            displacedCoord[1] = applyOperation(coord[1], scalar * m_scale, m_operation);
-            break;
-        case Z:
-            displacedCoord[2] = applyOperation(coord[2], scalar * m_scale, m_operation);
-            break;
-        case All:
-            for (int c = 0; c < 3; ++c) {
-                displacedCoord[c] = applyOperation(coord[c], scalar * m_scale, m_operation);
-            }
-            break;
-        }
+        for (auto c = 0; c < 3; c++)
+            displacedCoord[c] = m_mask[c] * (scalar * m_scale) + (1 - m_mask[c]) * coord[c];
     }
 };
 
-} // namespace vistle
+struct AddDisplaceWorklet: BaseDisplaceWorklet, viskores::worklet::WorkletMapField {
+    VISKORES_CONT AddDisplaceWorklet(): BaseDisplaceWorklet(){};
+
+    VISKORES_CONT AddDisplaceWorklet(viskores::FloatDefault scale, viskores::Vec<viskores::FloatDefault, 3> mask)
+    : BaseDisplaceWorklet(scale, mask)
+    {}
+
+    using ControlSignature = void(FieldIn scalarField, FieldIn coords, FieldOut result);
+    using ExecutionSignature = void(_1, _2, _3);
+    using InputDomain = _1;
+
+    template<typename T, typename S>
+    VISKORES_EXEC void operator()(const T &scalar, const viskores::Vec<S, 3> &coord,
+                                  viskores::Vec<S, 3> &displacedCoord) const
+    {
+        for (auto c = 0; c < 3; c++)
+            displacedCoord[c] = m_mask[c] * (coord[c] + scalar * m_scale) + (1 - m_mask[c]) * coord[c];
+    }
+};
+
+struct MultiplyDisplaceWorklet: BaseDisplaceWorklet, viskores::worklet::WorkletMapField {
+    VISKORES_CONT MultiplyDisplaceWorklet(): BaseDisplaceWorklet(){};
+
+    VISKORES_CONT MultiplyDisplaceWorklet(viskores::FloatDefault scale, viskores::Vec<viskores::FloatDefault, 3> mask)
+    : BaseDisplaceWorklet(scale, mask)
+    {}
+
+    using ControlSignature = void(FieldIn scalarField, FieldIn coords, FieldOut result);
+    using ExecutionSignature = void(_1, _2, _3);
+    using InputDomain = _1;
+
+    template<typename T, typename S>
+    VISKORES_EXEC void operator()(const T &scalar, const viskores::Vec<S, 3> &coord,
+                                  viskores::Vec<S, 3> &displacedCoord) const
+    {
+        for (auto c = 0; c < 3; c++)
+            displacedCoord[c] = m_mask[c] * (coord[c] * scalar * m_scale) + (1 - m_mask[c]) * coord[c];
+    }
+};
 
 #endif
