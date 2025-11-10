7b68b77e1d90f198f231b155f3ed40ec6eeac2ea
Martin Aumüller <aumuell@reserv.at>
2025-11-10 09:44:23 +0100

    more generic default values

--- module/read/ReadWrfChem/ReadWRFChem.cpp
+++ module/read/ReadWrfChem/ReadWRFChem.cpp
@@ -33,9 +33,7 @@ ReadWRFChem::ReadWRFChem(const std::string &name, int moduleID, mpi::communicato
 : vistle::Reader(name, moduleID, comm)
 {
     m_gridOut = createOutputPort("grid_out", "grid");
-    m_filedir =
-        addStringParameter("file_dir", "NC files directory", "/mnt/raid/home/hpcleker/Desktop/test_files/NC/test_time",
-                           Parameter::ExistingDirectory);
+    m_filedir = addStringParameter("file_dir", "NC files directory", "/data", Parameter::ExistingDirectory);
 
     m_numPartitionsLat = addIntParameter("num_partitions_lat", "number of partitions in lateral", 1);
     m_numPartitionsVer = addIntParameter("num_partitions_ver", "number of partitions in vertical", 1);
