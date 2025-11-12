a9574e13919cd5f86b5672de33fc974de04fa3f0
Martin Aumüller <aumuell@reserv.at>
2025-11-12 20:39:14 +0100

    remove OsgRenderer and simplify

--- module/render/COVER/VistleGeometryGenerator.h
+++ module/render/COVER/VistleGeometryGenerator.h
@@ -14,9 +14,7 @@
 #include <osg/Uniform>
 #include <osg/ref_ptr>
 
-#ifdef COVER_PLUGIN
 #include <cover/coVRShader.h>
-#endif
 
 namespace osg {
 class Geode;
@@ -28,14 +26,14 @@ struct OsgColorMap {
     void setName(const std::string &species);
     void setRange(float min, float max);
     void setBlendWithMaterial(bool enable);
-#ifdef COVER_PLUGIN
+
     std::shared_ptr<opencover::coVRShader> shader;
     std::shared_ptr<opencover::coVRShader> shaderUnlit;
     std::shared_ptr<opencover::coVRShader> shaderHeightMap;
     std::shared_ptr<opencover::coVRShader> shaderSpheres;
     std::shared_ptr<opencover::coVRShader> shaderSpheresCorrectDepth;
     std::vector<std::shared_ptr<opencover::coVRShader>> allShaders;
-#endif
+
     std::string species;
     bool blendWithMaterial = false;
     float rangeMin = 0.f, rangeMax = 1.f;
