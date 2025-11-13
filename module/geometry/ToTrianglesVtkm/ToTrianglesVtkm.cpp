d78a8b652161280a2741106665cfd1c990312da4
DeVasconcelos <malheiros.susanne@gmail.com>
2025-11-13 17:13:54 +0100

    pass through unsupported input grids

--- module/geometry/ToTrianglesVtkm/ToTrianglesVtkm.cpp
+++ module/geometry/ToTrianglesVtkm/ToTrianglesVtkm.cpp
@@ -1,5 +1,11 @@
 #include <viskores/filter/geometry_refinement/Triangulate.h>
 
+#include <vistle/core/polygons.h>
+#include <vistle/core/quads.h>
+#include <vistle/core/unstr.h>
+
+#include <vistle/vtkm/convert_worklets.h>
+
 #include "ToTrianglesVtkm.h"
 
 MODULE_MAIN(ToTrianglesVtkm)
@@ -10,7 +16,65 @@ ToTrianglesVtkm::ToTrianglesVtkm(const std::string &name, int moduleID, mpi::com
 : VtkmModule(name, moduleID, comm, 1, MappedDataHandling::Use)
 {}
 
+template<typename ArrayHandle>
+bool cellsArePolygons(const ArrayHandle &cellShapes)
+{
+    auto [minDim, maxDim] = getMinMaxDims(cellShapes);
+    return minDim == 2 && maxDim == 2;
+}
+
+ModuleStatusPtr ToTrianglesVtkm::prepareInputGrid(const vistle::Object::const_ptr &grid,
+                                                  viskores::cont::DataSet &dataset) const
+{
+    applyFilter = false;
+
+    if (Polygons::as(grid) || Quads::as(grid)) {
+        applyFilter = true;
+        return VtkmModule::prepareInputGrid(grid, dataset);
+
+    } else if (auto unstr = UnstructuredGrid::as(grid)) {
+        if (cellsArePolygons(unstr->tl().handle())) {
+            applyFilter = true;
+            return VtkmModule::prepareInputGrid(grid, dataset);
+        }
+    }
+
+    return Warning("Input grid is not polygonal, skipping triangulation!");
+}
+
+
 std::unique_ptr<viskores::filter::Filter> ToTrianglesVtkm::setUpFilter() const
 {
+    if (!applyFilter)
+        return nullptr;
     return std::make_unique<viskores::filter::geometry_refinement::Triangulate>();
+}
+
+vistle::Object::const_ptr ToTrianglesVtkm::prepareOutputGrid(const viskores::cont::DataSet &dataset,
+                                                             const vistle::Object::const_ptr &inputGrid) const
+{
+    if (!applyFilter) {
+        auto outputGrid = inputGrid->clone();
+        updateMeta(outputGrid);
+        outputGrid->copyAttributes(inputGrid);
+        return outputGrid;
+    }
+    return VtkmModule::prepareOutputGrid(dataset, inputGrid);
+}
+
+vistle::DataBase::ptr ToTrianglesVtkm::prepareOutputField(const viskores::cont::DataSet &dataset,
+                                                          const vistle::Object::const_ptr &inputGrid,
+                                                          const vistle::DataBase::const_ptr &inputField,
+                                                          const std::string &fieldName,
+                                                          const vistle::Object::const_ptr &outputGrid) const
+{
+    if (!applyFilter) {
+        auto outputField = inputField->clone();
+        updateMeta(outputField);
+        outputField->copyAttributes(inputField);
+        if (outputGrid)
+            outputField->setGrid(outputGrid);
+        return outputField;
+    }
+    return VtkmModule::prepareOutputField(dataset, inputGrid, inputField, fieldName, outputGrid);
 }
--- module/geometry/ToTrianglesVtkm/ToTrianglesVtkm.h
+++ module/geometry/ToTrianglesVtkm/ToTrianglesVtkm.h
@@ -3,13 +3,26 @@
 
 #include <vistle/vtkm/vtkm_module.h>
 
-// TODO: make sure input grid are polyhedra only
 class ToTrianglesVtkm: public vistle::VtkmModule {
 public:
     ToTrianglesVtkm(const std::string &name, int moduleID, mpi::communicator comm);
 
 private:
+    mutable bool applyFilter;
+
+    ModuleStatusPtr prepareInputGrid(const vistle::Object::const_ptr &grid,
+                                     viskores::cont::DataSet &dataset) const override;
+
     std::unique_ptr<viskores::filter::Filter> setUpFilter() const override;
+
+    vistle::Object::const_ptr prepareOutputGrid(const viskores::cont::DataSet &dataset,
+                                                const vistle::Object::const_ptr &inputGrid) const override;
+
+    vistle::DataBase::ptr prepareOutputField(const viskores::cont::DataSet &dataset,
+                                             const vistle::Object::const_ptr &inputGrid,
+                                             const vistle::DataBase::const_ptr &inputField,
+                                             const std::string &fieldName,
+                                             const vistle::Object::const_ptr &outputGrid) const override;
 };
 
 #endif // VISTLE_TOTRIANGLESVTKM_TOTRIANGLESVTKM_H
