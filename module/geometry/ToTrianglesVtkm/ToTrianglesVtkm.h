02fb40351eeab6bc7496d676cbe912467e844a34
DeVasconcelos <malheiros.susanne@gmail.com>
2025-11-14 15:29:03 +0100

    don't mimick ToTriangles behavior: unsupported grid types are not passed through anymore, instead the filter throws an error which will be handled by vtkmModule

--- module/geometry/ToTrianglesVtkm/ToTrianglesVtkm.h
+++ module/geometry/ToTrianglesVtkm/ToTrianglesVtkm.h
@@ -8,21 +8,7 @@ public:
     ToTrianglesVtkm(const std::string &name, int moduleID, mpi::communicator comm);
 
 private:
-    mutable bool applyFilter;
-
-    ModuleStatusPtr prepareInputGrid(const vistle::Object::const_ptr &grid,
-                                     viskores::cont::DataSet &dataset) const override;
-
     std::unique_ptr<viskores::filter::Filter> setUpFilter() const override;
-
-    vistle::Object::const_ptr prepareOutputGrid(const viskores::cont::DataSet &dataset,
-                                                const vistle::Object::const_ptr &inputGrid) const override;
-
-    vistle::DataBase::ptr prepareOutputField(const viskores::cont::DataSet &dataset,
-                                             const vistle::Object::const_ptr &inputGrid,
-                                             const vistle::DataBase::const_ptr &inputField,
-                                             const std::string &fieldName,
-                                             const vistle::Object::const_ptr &outputGrid) const override;
 };
 
 #endif // VISTLE_TOTRIANGLESVTKM_TOTRIANGLESVTKM_H
