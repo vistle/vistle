79f1e29f8583261da213345794ceda0542561121
Martin Aumüller <aumuell@reserv.at>
2026-01-13 12:49:06 +0100

    also extract components from 2D vectors

--- module/filter/VecToScalar/VecToScalar.cpp
+++ module/filter/VecToScalar/VecToScalar.cpp
@@ -27,57 +27,17 @@ VecToScalar::VecToScalar(const std::string &name, int moduleID, mpi::communicato
     V_ENUM_SET_CHOICES(m_caseParam, Choices);
 }
 
-bool VecToScalar::compute()
-{
-    auto data_in = expect<Vec<Scalar, 3>>("data_in");
-    if (!data_in) {
-        std::cerr << "VecToScalar: ERROR: Input is not vector data\n";
-        return true;
-    }
-    std::string spec = data_in->getAttribute(attribute::Species);
-    if (!spec.empty())
-        spec += "_";
-
-    Vec<Scalar>::ptr out;
-    switch (m_caseParam->getValue()) {
-    case X:
-        spec += "x";
-        out = extract(data_in, 0);
-        break;
-    case Y:
-        spec += "y";
-        out = extract(data_in, 1);
-        break;
-    case Z:
-        spec += "z";
-        out = extract(data_in, 2);
-        break;
-    case AbsoluteValue:
-        spec += "magnitude";
-        out = calculateAbsolute(data_in);
-        break;
-    }
-
-    if (out) {
-        out->copyAttributes(data_in);
-        out->describe(spec, id());
-        out->setGrid(data_in->grid());
-        updateMeta(out);
-        addObject("data_out", out);
-    }
-
-    return true;
-}
-
-vistle::Vec<vistle::Scalar>::ptr VecToScalar::extract(vistle::Vec<vistle::Scalar, 3>::const_ptr &data,
-                                                      const vistle::Index &coord)
+template<int d>
+vistle::Vec<vistle::Scalar>::ptr extract(typename vistle::Vec<vistle::Scalar, d>::const_ptr &data,
+                                         const vistle::Index &coord)
 {
     auto dataOut = std::make_shared<Vec<Scalar>>(Object::Initialized);
     dataOut->d()->x[0] = data->d()->x[coord];
     return dataOut;
 }
 
-vistle::Vec<vistle::Scalar>::ptr VecToScalar::calculateAbsolute(vistle::Vec<vistle::Scalar, 3>::const_ptr &data)
+template<int d>
+vistle::Vec<vistle::Scalar>::ptr calculateAbsolute(typename vistle::Vec<vistle::Scalar, d>::const_ptr &data)
 {
 #ifdef VECTOSCALARVTKM
     // Create a Viskores dataset to hold the input mesh and fields
@@ -117,13 +77,109 @@ vistle::Vec<vistle::Scalar>::ptr VecToScalar::calculateAbsolute(vistle::Vec<vist
     return std::dynamic_pointer_cast<vistle::Vec<vistle::Scalar>>(outField);
 #else
     const auto n = data->getSize();
-    const auto *x = data->x().data();
-    const auto *y = data->y().data();
-    const auto *z = data->z().data();
+    const Scalar *x = data->x().data();
+    const Scalar *y = nullptr;
+    if (d > 1)
+        y = data->y().data();
+    const Scalar *z = nullptr;
+    if (d > 2)
+        z = data->z().data();
     auto out = std::make_shared<vistle::Vec<vistle::Scalar>>(n);
     auto *o = out->x().data();
-    for (vistle::Index i = 0; i < n; ++i)
-        o[i] = std::sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]);
+    if (d == 1) {
+        for (vistle::Index i = 0; i < n; ++i)
+            o[i] = std::abs(x[i]);
+        return out;
+    } else if (d == 2) {
+        for (vistle::Index i = 0; i < n; ++i)
+            o[i] = std::sqrt(x[i] * x[i] + y[i] * y[i]);
+        return out;
+    } else if (d == 3) {
+        for (vistle::Index i = 0; i < n; ++i)
+            o[i] = std::sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]);
+    }
     return out;
 #endif
+}
+
+bool VecToScalar::compute()
+{
+    auto data_in = expect<DataBase>("data_in");
+    if (!data_in) {
+        std::cerr << "VecToScalar: ERROR: Input is not vector data\n";
+        return true;
+    }
+    std::string spec = data_in->getAttribute(attribute::Species);
+    if (!spec.empty())
+        spec += "_";
+
+    Vec<Scalar>::ptr out;
+    auto data_in1 = Vec<Scalar, 1>::as(data_in);
+    auto data_in2 = Vec<Scalar, 2>::as(data_in);
+    auto data_in3 = Vec<Scalar, 3>::as(data_in);
+
+    if (data_in1) {
+        out = extract<1>(data_in1, 0);
+        switch (m_caseParam->getValue()) {
+        case X:
+            spec += "x";
+            out = extract<1>(data_in1, 0);
+            break;
+        case AbsoluteValue:
+            spec += "magnitude";
+            out = calculateAbsolute<1>(data_in1);
+            break;
+        case Y:
+        case Z:
+            sendError("input data has only 1 component, cannot extract y nor z component");
+            return true;
+        }
+    } else if (data_in2) {
+        switch (m_caseParam->getValue()) {
+        case X:
+            spec += "x";
+            out = extract<2>(data_in2, 0);
+            break;
+        case Y:
+            spec += "y";
+            out = extract<2>(data_in2, 1);
+            break;
+        case AbsoluteValue:
+            spec += "magnitude";
+            out = calculateAbsolute<2>(data_in2);
+            break;
+        case Z:
+            sendError("input data has only 2 components, cannot extract z component");
+            return true;
+        }
+    } else if (data_in3) {
+        switch (m_caseParam->getValue()) {
+        case X:
+            spec += "x";
+            out = extract<3>(data_in3, 0);
+            break;
+        case Y:
+            spec += "y";
+            out = extract<3>(data_in3, 1);
+            break;
+        case Z:
+            spec += "z";
+            out = extract<3>(data_in3, 2);
+            break;
+        case AbsoluteValue:
+            spec += "magnitude";
+            out = calculateAbsolute<3>(data_in3);
+            break;
+        }
+    }
+
+    if (out) {
+        out->copyAttributes(data_in);
+        out->describe(spec, id());
+        out->setGrid(data_in->grid());
+        updateMeta(out);
+        addObject("data_out", out);
+    }
+
+    return true;
 }
