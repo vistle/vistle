fc2c743dc340ac628d8bfe8d6592e7933217ab09
Martin Aumüller <aumuell@reserv.at>
2026-01-29 23:16:52 +0100

    if run as root, register vistle system wide

--- install/install-xdg.sh
+++ install/install-xdg.sh
@@ -1,10 +1,17 @@
-xdg-mime install hlrs-vsl.desktop
+#! /bin/bash
 
-xdg-desktop-menu install --noupdate hlrs-vistle.desktop
-xdg-desktop-menu install --noupdate hlrs-vistle_opener.desktop
-xdg-desktop-menu forceupdate
+mode=user
+if [ "$EUID" -eq 0 ]; then
+    mode=system
+fi
+
+xdg-mime install --mode $mode hlrs-vsl.desktop
+
+xdg-desktop-menu install --mode $mode --noupdate hlrs-vistle.desktop
+xdg-desktop-menu install --mode $mode --noupdate hlrs-vistle_opener.desktop
+xdg-desktop-menu forceupdate --mode $mode
 xdg-mime default hlrs-vistle_opener.desktop x-scheme-handler/vistle
 
-xdg-icon-resource install --noupdate --size 48 hlrs-vistle.png
-xdg-icon-resource install --noupdate --context mimetypes --size 48 hlrs-vistle.png text/x-vsl
-xdg-icon-resource forceupdate
+xdg-icon-resource install --mode $mode --noupdate --size 48 hlrs-vistle.png
+xdg-icon-resource install --mode $mode --noupdate --context mimetypes --size 48 hlrs-vistle.png text/x-vsl
+xdg-icon-resource forceupdate --mode $mode
