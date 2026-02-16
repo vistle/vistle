5d96a36c8f6f9e6e31f6b840fb712383d5e3ce35
Martin Aumüller <aumuell@reserv.at>
2026-02-16 10:40:56 +0100

    enforce thtat examine is reimplemented

--- lib/vistle/module/reader.h
+++ lib/vistle/module/reader.h
@@ -80,8 +80,9 @@ public:
     *  After constructing the class, the method is called after all parameters have been set to their initial value with param==nullptr.
     *  Otherwise, the changed parameter is passed.
     *  Call @ref setTimesteps and @ref setPartitions according to the data to be read.
+    *  @return return true iff the module is ready to read.
     */
-    virtual bool examine(const Parameter *param = nullptr);
+    virtual bool examine(const Parameter *param = nullptr) = 0;
 
     /// called for every unit of work to be read
     /** Called for every unit of work to be read.
