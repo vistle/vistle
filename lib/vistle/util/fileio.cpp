067171c9ce09b6c6137a3fe1517fbcede7415559
hpcdgrie <grieger@hlrs.de>
2026-05-12 12:01:55 +0200

    Added C style file I/O to ensure cross platform functionality with large files. Normal Windows functions use 32 bit offsets, so replaced them with their 64 bit äquivalent. .

--- lib/vistle/util/fileio.cpp
+++ lib/vistle/util/fileio.cpp
@@ -1,1 +1,38 @@
 #include "fileio.h"
+
+
+ssize_t file::tell(const int fd)
+{
+#ifdef WIN32
+    return _lseeki64(fd, 0, SEEK_CUR);
+#else
+    return lseek(fd, 0, SEEK_CUR);
+#endif
+}
+
+ssize_t file::tell(FILE *file)
+{
+#ifdef WIN32
+    return _ftelli64(file);
+#else
+    return ftell(file);
+#endif
+}
+
+ssize_t file::seek(int fd, ssize_t off, int whence)
+{
+#ifdef WIN32
+    return _lseeki64(fd, off, whence);
+#else
+    return lseek(fd, off, whence);
+#endif
+}
+
+ssize_t file::seek(FILE *file, ssize_t off, int whence)
+{
+#ifdef WIN32
+    return _fseeki64(file, off, whence);
+#else
+    return fseek(file, off, whence);
+#endif
+}
