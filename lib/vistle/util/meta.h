b35a0810201d14f036cbe66c84025231a4ce0785
hpcdgrie <grieger@hlrs.de>
2026-05-21 14:00:28 +0200

    Added dispatch_by_type: call a function template with the type of an object if it is in a tuple of types. Will be used in VistleGeometryGenerator.

--- lib/vistle/util/meta.h
+++ lib/vistle/util/meta.h
@@ -4,6 +4,8 @@
 #include <type_traits>
 #include <cstdlib>
 #include <utility>
+#include <typeindex>
+#include <typeinfo>
 
 namespace vistle {
 namespace meta {
@@ -39,6 +41,29 @@ void _for(F func)
 {
     _for<E - B>(detail::_shift<B>(func));
 }
+
+namespace detail {
+template<typename Tuple, typename T, typename F, std::size_t... I>
+bool dispatch_by_type_impl(const T &obj, F &&f, std::index_sequence<I...>)
+{
+    const std::type_index actual(typeid(obj));
+
+    return ((actual == std::type_index(typeid(std::tuple_element_t<I, Tuple>)) &&
+             (std::forward<F>(f).template operator()<std::tuple_element_t<I, Tuple>>(), true)) ||
+            ...);
+}
+} // namespace detail
+
+// call f.template operator()<T>() where T is the type of obj and is in Tuple
+// f can be a lamda: [&]<typename T>() { ... }
+// returns true if a match was found, false otherwise
+template<typename Tuple, typename T, typename F>
+bool dispatch_by_type(const T &obj, F &&f)
+{
+    return detail::dispatch_by_type_impl<Tuple>(obj, std::forward<F>(f),
+                                                std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+}
+
 
 } // namespace meta
 } // namespace vistle
