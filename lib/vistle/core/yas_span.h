10c0012d45e8f68fbd40bb82588b8f8e3941676b
Martin Aumüller <aumuell@reserv.at>
2026-06-17 14:20:03 +0200

    restore compatibility with Boost 1.75
    C++ 20 provides std::span, so no need to depend on Boost 1.78 for boost::span

--- lib/vistle/core/yas_span.h
+++ lib/vistle/core/yas_span.h
@@ -7,21 +7,21 @@
 
 #include <yas/types/concepts/array.hpp>
 
-#include <boost/core/span.hpp>
+#include <span>
 
 namespace yas {
 namespace detail {
 
 template<std::size_t F, typename T, std::size_t E>
-struct serializer<type_prop::not_a_fundamental, ser_case::use_internal_serializer, F, boost::span<T, E>> {
+struct serializer<type_prop::not_a_fundamental, ser_case::use_internal_serializer, F, std::span<T, E>> {
     template<typename Archive>
-    static Archive &save(Archive &ar, const boost::span<T, E> &span)
+    static Archive &save(Archive &ar, const std::span<T, E> &span)
     {
         return concepts::array::save<F>(ar, span);
     }
 
     template<typename Archive>
-    static Archive &load(Archive &ar, boost::span<T, E> &span)
+    static Archive &load(Archive &ar, std::span<T, E> &span)
     {
         return concepts::array::load<F>(ar, span);
     }
